af26b370636a0ab3964f19b36a72be591a913aea
Djothi Grondin <djothi.grondin@live.fr>
2018-02-22 12:09:25 +0400

    feat(maintenance): maintenance API to send a message

---
+++ maintenance/handler.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+const doGetMaintenance = async () => {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  try {
+    const selector = {
+      active: true,
+    };
+
+    return await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
+      .findOne(selector);
+  } finally {
+    client.close();
+  }
+};
+
+export const handleGetMaintenance = async (event, context, callback) => {
+  try {
+    const msg = await doGetMaintenance();
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(msg),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      message: e.message,
+    };
+    callback(null, response);
+  }
+};
