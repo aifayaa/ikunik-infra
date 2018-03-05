39494384055eaeda6ff81e87f9f727c90ebec20f
Djothi Grondin <djothi.grondin@live.fr>
2018-03-05 16:15:38 +0400

    feat(purchases): API to get purchases by projects for a user

--- users/handler.js
+++ users/handler.js
@@ -69,6 +69,76 @@ const doGetBalances = async (userId) => {
   }
 };
 
+const doGetPurchases = async (userId) => {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  try {
+    const record = await client.db(process.env.DB_NAME).collection('profil')
+      .aggregate([
+        { $match: { UserId: userId } },
+        {
+          $lookup: {
+            from: 'Project',
+            localField: '_id',
+            foreignField: 'profil_ID',
+            as: 'projects',
+          },
+        },
+        {
+          $unwind: {
+            path: '$projects',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: 'purchases',
+            localField: 'projects._id',
+            foreignField: 'purchase.project_ID',
+            as: 'purchases',
+          },
+        },
+        {
+          $unwind: {
+            path: '$purchases',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $project: {
+            projects: 1,
+            purchases: { $ifNull: ['$purchases', { purchase: { project_ID: '$projects._id' } }] },
+          },
+        },
+        {
+          $group: {
+            _id: '$purchases.purchase.project_ID',
+            projectName: { $first: '$projects.projectName' },
+            purchases: { $sum: '$purchases.purchase.price' },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            total: { $sum: '$purchases' },
+            projects: { $push: { _id: '$_id', projectName: '$projectName', purchases: '$purchases' } },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            total: 1,
+            projects: 1,
+            unity: { $literal: 'credits' },
+            symbol: { $literal: 'credits' },
+          },
+        },
+      ]).toArray();
+    return record[0];
+  } finally {
+    client.close();
+  }
+};
+
 export const handleGetBalances = async (event, context, callback) => {
   const userId = event.requestContext.authorizer.principalId;
   const urlId = event.pathParameters.id;
@@ -76,16 +146,43 @@ export const handleGetBalances = async (event, context, callback) => {
     const response = {
       statusCode: 403,
       body: 'Forbidden',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
     };
     callback(null, response);
     return;
   }
   try {
     const results = await doGetBalances(userId);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(results),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      message: e.message,
+    };
+    callback(null, response);
+  }
+};
+
+export const handleGetPurchases = async (event, context, callback) => {
+  const userId = event.requestContext.authorizer.principalId;
+  const urlId = event.pathParameters.id;
+  if (userId !== urlId) {
+    const response = {
+      statusCode: 403,
+      body: 'Forbidden',
+    };
+    callback(null, response);
+    return;
+  }
+  try {
+    const results = await doGetPurchases(userId);
     const response = {
       statusCode: 200,
       body: JSON.stringify(results),
