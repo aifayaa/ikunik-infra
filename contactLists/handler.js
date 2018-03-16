8e2e89a85b34da794188607c635d1e546d7af696
Djothi Grondin <djothi.grondin@live.fr>
2018-03-16 09:47:59 +0400

    fix(contactslist): use profile instead of userid

--- contactLists/handler.js
+++ contactLists/handler.js
@@ -3,19 +3,34 @@ import { MongoClient } from 'mongodb';
 const doGetContactList = async (userId, contactListId) => {
   const client = await MongoClient.connect(process.env.MONGO_URL);
   try {
-    const contactList = await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
+    const contactList = await client.db(process.env.DB_NAME).collection('profil')
       .aggregate([
-        { $match: { _id: contactListId, user_ID: userId } },
+        { $match: { UserId: userId } },
+        {
+          $lookup: {
+            from: process.env.COLL_NAME,
+            localField: '_id',
+            foreignField: 'profil_ID',
+            as: 'list',
+          },
+        },
         {
           $unwind: {
-            path: '$contactIDs',
+            path: '$list',
+            preserveNullAndEmptyArrays: false,
+          },
+        },
+        { $match: { 'list._id': contactListId } },
+        {
+          $unwind: {
+            path: '$list.contactIDs',
             preserveNullAndEmptyArrays: false,
           },
         },
         {
           $lookup: {
             from: 'contacts',
-            localField: 'contactIDs',
+            localField: 'list.contactIDs',
             foreignField: '_id',
             as: 'contact',
           },
@@ -28,13 +43,13 @@ const doGetContactList = async (userId, contactListId) => {
         },
         {
           $group: {
-            _id: '$_id',
+            _id: '$list._id',
             contacts: { $addToSet: '$contact' },
-            contactListName: { $first: '$contactListName' },
-            date: { $first: '$date' },
-            profil_ID: { $first: '$profil_ID' },
-            type: { $first: '$type' },
-            user_ID: { $first: '$user_ID' },
+            contactListName: { $first: '$list.contactListName' },
+            date: { $first: '$list.date' },
+            profil_ID: { $first: '$list.profil_ID' },
+            type: { $first: '$list.type' },
+            user_ID: { $first: '$list.user_ID' },
           },
         },
       ]).toArray();
