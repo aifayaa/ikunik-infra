659e17d583db1382f62d7a44e655d7d0698cf2fb
Djothi Grondin <djothi.grondin@live.fr>
2018-04-25 17:19:42 +0400

    feat(notif): push notif 15 minutes before

--- lineup/handler.js
+++ lineup/handler.js
@@ -1,4 +1,17 @@
 import { MongoClient } from 'mongodb';
+import CloudWatchEvents from 'aws-sdk/clients/cloudwatchevents';
+import Lambda from 'aws-sdk/clients/lambda';
+import uuidv4 from 'uuid/v4';
+
+const THRESHOLD = 15; // minutes
+
+const cloudwatchevents = new CloudWatchEvents({
+  region: process.env.REGION,
+});
+
+const lambda = new Lambda({
+  region: process.env.REGION,
+});
 
 const doGetLineup = async (someId, type) => {
   const client = await MongoClient.connect(process.env.MONGO_URL);
@@ -57,6 +70,167 @@ const doGetLineup = async (someId, type) => {
   }
 };
 
+const doPostLineup = async (festivalId, sceneId, artistId, startDate, endDate, ticketingURL) => {
+  const lineupId = uuidv4();
+  const wDate = new Date(new Date(startDate).valueOf() - (THRESHOLD * 60000));
+  const min = wDate.getMinutes();
+  const hours = wDate.getHours();
+  const day = wDate.getDate();
+  const month = wDate.getMonth() + 1;
+  const jobId = `CronJobLineup-${lineupId}`;
+  const paramsRule = {
+    Name: jobId,
+    Description: `Cron job for ${startDate}/${ticketingURL}`,
+    ScheduleExpression: `cron(${min} ${hours} ${day} ${month} ? *)`,
+  };
+  const paramsTarget = {
+    Rule: jobId,
+    Targets: [
+      {
+        Arn: `arn:aws:lambda:us-east-1:630176884077:function:lineup-${process.env.STAGE}-notifyLineup`,
+        Id: 'CronLineupTarget',
+        Input: JSON.stringify({ lineupId }),
+      },
+    ],
+  };
+  await cloudwatchevents.putRule(paramsRule).promise();
+  await cloudwatchevents.putTargets(paramsTarget).promise();
+
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  try {
+    await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
+      .insertOne({
+        _id: lineupId,
+        festivalId,
+        sceneId,
+        artistId,
+        startDate,
+        endDate,
+        ticketingURL,
+      });
+    return true;
+  } finally {
+    client.close();
+  }
+};
+
+const doNotifyLineup = async (lineupId) => {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  try {
+    const toNotify = await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
+      .aggregate([
+        { $match: { _id: lineupId } },
+        {
+          $lookup: {
+            from: 'artistsFav',
+            localField: 'artistId',
+            foreignField: 'artistId',
+            as: 'artistsFav',
+          },
+        },
+        {
+          $unwind: {
+            path: '$artistsFav', preserveNullAndEmptyArrays: true,
+          },
+        },
+        { $match: { 'artistsFav.isFavorite': true } },
+        {
+          $lookup: {
+            from: 'pushNotifications',
+            localField: 'artistsFav.userId',
+            foreignField: 'userId',
+            as: 'endpoint',
+          },
+        },
+        {
+          $unwind: {
+            path: '$endpoint', preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'artistsFav.userId',
+            foreignField: '_id',
+            as: 'users',
+          },
+        },
+        {
+          $unwind: {
+            path: '$users', preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: 'artists',
+            localField: 'artistId',
+            foreignField: '_id',
+            as: 'artists',
+          },
+        },
+        {
+          $unwind: {
+            path: '$artists', preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: 'scenes',
+            localField: 'sceneId',
+            foreignField: '_id',
+            as: 'scenes',
+          },
+        },
+        {
+          $unwind: {
+            path: '$scenes', preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $group: {
+            _id: '$users.locale',
+            endpoints: {
+              $addToSet: {
+                Platform: '$endpoint.Platform',
+                EndpointArn: '$endpoint.EndpointArn',
+              },
+            },
+            phone: { $addToSet: '$users.profile.phone' },
+            artistName: { $first: '$artists.artistName' },
+            sceneName: { $first: '$scenes.name' },
+          },
+        },
+      ]).toArray();
+    if (toNotify.length === 0) return true;
+    toNotify.forEach(async (locale) => {
+      console.log('======', locale._id.split('-')[0]);
+      const { artistName, sceneName } = locale;
+      const paramsNotif = {
+        FunctionName: process.env.BLAST_NOTIF,
+        Payload: JSON.stringify({
+          artistName,
+          endpoints: locale.endpoints,
+          message: `Votre concert va bientôt commencer sur la scène du ${sceneName} !!!`,
+        }),
+      };
+      const paramsText = {
+        FunctionName: process.env.BLAST_TEXT,
+        Payload: JSON.stringify({
+          phones: locale.phone,
+          message: `${artistName} : Votre concert va bientôt commencer sur la scène du ${sceneName} !!!`
+        }),
+      };
+      console.log('>>>>>>>', paramsNotif, paramsText);
+      await lambda.invoke(paramsNotif).promise();
+      await lambda.invoke(paramsText).promise();
+    });
+    // await cloudwatchevents.deleteRule({ Name: `CronJobLineup-${lineupId}` });*/
+    return true;
+  } finally {
+    client.close();
+  }
+};
+
 export const handleGetLineup = async (event, context, callback) => {
   try {
     const someId = event.pathParameters.id;
@@ -75,6 +249,70 @@ export const handleGetLineup = async (event, context, callback) => {
         type = undefined;
     }
     const results = await doGetLineup(someId, type);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(results),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ message: e.message }),
+    };
+    callback(null, response);
+  }
+};
+
+export const handlePostLineup = async (event, context, callback) => {
+  try {
+    const {
+      festivalId,
+      sceneId,
+      artistId,
+      startDate,
+      endDate,
+      ticketingURL,
+    } = JSON.parse(event.body);
+
+    if (!festivalId || !sceneId || !artistId || !startDate || !endDate || !ticketingURL) {
+      throw new Error('Bad arguments');
+    }
+
+    const results = await doPostLineup(
+      festivalId,
+      sceneId,
+      artistId,
+      startDate,
+      endDate,
+      ticketingURL,
+    );
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(results),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ message: e.message }),
+    };
+    callback(null, response);
+  }
+};
+
+export const handlerNotifyLineup = async (event, context, callback) => {
+  try {
+    console.log('>>>>>>>>>>>>=========================', event);
+    const { lineupId } = event;
+    const results = await doNotifyLineup(lineupId);
     const response = {
       statusCode: 200,
       body: JSON.stringify(results),
