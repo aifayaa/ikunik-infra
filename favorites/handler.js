ab2d4d3eaeb1022ac70b42d588e30cd275d89f4b
Mathieu <mathieu.chaussinand@epitech.eu>
2018-04-26 19:27:22 +0400

    fix(favoriteMS): fix 'lint', fix 'one handler'

--- favorites/handler.js
+++ favorites/handler.js
@@ -1,42 +1,5 @@
 import { MongoClient } from 'mongodb';
 
-export const doAddFavorite = async (userId, artistId) => {
-  const client = await MongoClient.connect(process.env.MONGO_URL);
-  const favoriteData = {
-    userId,
-    artistId,
-    isFavorite: true,
-    date: new Date(),
-  };
-
-  try {
-    await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
-      .update({ userId, artistId }, favoriteData, { upsert: true });
-    return true;
-  } finally {
-    client.close();
-  }
-};
-
-export const doUnfavorite = async (userId, artistId) => {
-  const client = await MongoClient.connect(process.env.MONGO_URL);
-
-  try {
-    await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
-      .updateOne(
-        { userId, artistId },
-        {
-          $set: { isFavorite: false, date: new Date() },
-        }, {
-          upsert: true,
-        },
-      );
-    return true;
-  } finally {
-    client.close();
-  }
-};
-
 export const doGetFavorite = async (userId, artistId) => {
   const client = await MongoClient.connect(process.env.MONGO_URL);
 
@@ -49,60 +12,43 @@ export const doGetFavorite = async (userId, artistId) => {
   }
 };
 
-export const handleGetFavorite = async (event, context, callback) => {
-  const userId = event.requestContext.authorizer.principalId;
-  const { id } = event.pathParameters;
+export const doToggleFavorite = async (userId, artistId, isFavorite) => {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  const favoriteData = {
+    userId,
+    artistId,
+    isFavorite,
+    date: new Date(),
+  };
 
   try {
-    const result = await doGetFavorite(userId, id);
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials:': true,
-      },
-    };
-    callback(null, response);
-  } catch (e) {
-    const response = {
-      statusCode: 500,
-      body: JSON.stringify({ message: e.message }),
-    };
-    callback(null, response);
+    await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
+      .update({ userId, artistId }, favoriteData, { upsert: true });
+    return true;
+  } finally {
+    client.close();
   }
 };
 
-export const handleAddFavorite = async (event, context, callback) => {
+export const handleFavorite = async (event, context, callback) => {
   const userId = event.requestContext.authorizer.principalId;
   const { id } = event.pathParameters;
+  let result;
 
   try {
-    const result = await doAddFavorite(userId, id);
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-    };
-    callback(null, response);
-  } catch (e) {
-    const response = {
-      statusCode: 500,
-      body: JSON.stringify({ message: e.message }),
-    };
-    callback(null, response);
-  }
-};
-
-export const handleUnfavorite = async (event, context, callback) => {
-  const userId = event.requestContext.authorizer.principalId;
-  const { id } = event.pathParameters;
-
-  try {
-    const result = await doUnfavorite(userId, id);
+    switch (event.httpMethod) {
+      case 'GET':
+        result = await doGetFavorite(userId, id);
+        break;
+      case 'POST':
+        result = await doToggleFavorite(userId, id, true);
+        break;
+      case 'DELETE':
+        result = await doToggleFavorite(userId, id, false);
+        break;
+      default:
+        result = await doGetFavorite(userId, id);
+    }
     const response = {
       statusCode: 200,
       body: JSON.stringify(result),
