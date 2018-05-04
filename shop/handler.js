add524e8fcccc3c190f0a445e20b6ce01a09e084
Djothi Grondin <djothi.grondin@live.fr>
2018-05-04 17:12:42 +0400

    feat(order): implement printful order

--- shop/handler.js
+++ shop/handler.js
@@ -1,4 +1,12 @@
 import { MongoClient } from 'mongodb';
+import findIndex from 'lodash/findIndex';
+import validator from 'validator';
+import Lambda from 'aws-sdk/clients/lambda';
+import request from 'request';
+
+const lambda = new Lambda({
+  region: process.env.REGION,
+});
 
 const doGetShopItems = async () => {
   let client;
@@ -24,36 +32,66 @@ const doGetShopItem = async (itemId) => {
 };
 
 const doShopOrder = async (itemId, variantId, qte, adr, userId) => {
-  let client;
+  if (!validator.isInt(qte, { min: 0, allow_leading_zeroes: false, max: 100 })) {
+    throw new Error('Wrong quantity');
+  }
+  if (!adr.name || !adr.address1 || !adr.city || !adr.country_code || !adr.zip) {
+    throw new Error('address malformed');
+  }
+  // let client;
   try {
-    const { price, sizes } = doGetShopItem(itemId);
-    if (!price) throw new Error('Shop item not found');
-    // Check in this item, if variantID exists
+    const item = await doGetShopItem(itemId);
+    if (!item) throw new Error('Shop item not found');
+    const { price, sizes } = item;
+    if (findIndex(sizes, { variantId }) === -1) throw new Error('Shop variant not find');
 
-    // TODO get credits and compare to price
     const params = {
       FunctionName: `credits-${process.env.STAGE}-getCredits`,
+      Payload: JSON.stringify({ requestContext: { authorizer: { principalId: userId } } }),
     };
-    const res = await lambda.invoke(params).promise();
-    console.log('++++++++', res);
+    const { Payload } = await lambda.invoke(params).promise();
+    const { statusCode, body } = JSON.parse(Payload);
+    if (statusCode !== 200) throw new Error(`get credits failed: ${statusCode}`);
+    const { credits } = JSON.parse(body);
+    if (!credits) throw new Error('unable to get credits from service response');
+    if (credits < price) throw new Error('insufficient credits on user account');
+    const order = {
+      recipient: adr,
+      items: [{
+        variant_id: variantId,
+        quantity: qte,
+      }],
+    };
 
-    // Open order
-    // get back order id and put it in shopOrders collection
-    // consume crédits
-    // send email to say order is done
-    // webhook to listen to order detail event state
-    /*const { Payload } = await lambda.invoke(params).promise();
-    const res = JSON.parse(Payload);
-    if (res.statusCode !== 200) {
-      throw new Error(`addCredits handler failed: ${res.body}`);
-    }*/
+    const httpOptions = {
+      method: 'POST',
+      url: process.env.PRINTFUL_URL,
+      headers: {
+        Authorization: `Basic ${process.env.PRINTFUL_KEY}`,
+      },
+      json: true,
+      body: order,
+    };
+    const httpReq = new Promise((resolve, reject) => {
+      request(httpOptions, (err, res, httpBody) => {
+        const resStatusCode = res.statusCode;
+        const { code, result } = httpBody;
+        if (err) {
+          reject(err);
+          return;
+        }
+        if (resStatusCode !== 200 || code !== 200) {
+          reject(new Error(`Bad response code: ${resStatusCode}`));
+          return;
+        }
+        resolve(result);
+      });
+    });
 
-    /*client = await MongoClient.connect(process.env.MONGO_URL);
-    return await client.db(process.env.DB_NAME).collection('shopItems')
-      .findOne({ _id: itemId });*/
-    return true;
+    const prinfulOrder = await httpReq;
+    return prinfulOrder;
   } finally {
-    client.close();
+    // client.close();
   }
 };
 
@@ -104,7 +142,7 @@ export const handlePostShopOrder = async (event, context, callback) => {
   try {
     const userId = event.requestContext.authorizer.principalId;
     const { itemId, variantId, qte, adr } = JSON.parse(event.body);
-    if (!itemId, !variantId || !qte || !adr) {
+    if (!itemId || !variantId || !qte || !adr) {
       throw new Error('mal formed request');
     }
     const results = await doShopOrder(itemId, variantId, qte, adr, userId);
