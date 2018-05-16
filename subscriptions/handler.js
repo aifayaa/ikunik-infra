7a39cb9266949c3a58607622d9ad0b9ad43cabcf
Djothi Grondin <djothi.grondin@live.fr>
2018-05-16 16:13:17 +0400

    feat(subscriptions): get user'subscriptions

--- subscriptions/handler.js
+++ subscriptions/handler.js
@@ -20,12 +20,45 @@ const doGetSubscription = async (subId) => {
   }
 };
 
+const doGetUserSubscriptions = async (userId) => {
+  let client;
+  try {
+    client = await MongoClient.connect(process.env.MONGO_URL);
+    const res = await client.db(process.env.DB_NAME).collection('userSubscriptions')
+      .aggregate([
+        { $match: { userId } },
+        {
+          $lookup: {
+            from: 'subscriptions',
+            localField: 'subscriptionId',
+            foreignField: '_id',
+            as: 'subscription',
+          },
+        },
+        {
+          $unwind: {
+            path: '$subscriptions', preserveNullAndEmptyArrays: true,
+          },
+        },
+      ]).toArray();
+    return { subscriptions: res };
+  } catch (e) {
+    throw e;
+  } finally {
+    client.close();
+  }
+};
+
 const doSubscribe = async (userId, subId) => {
   const sub = await doGetSubscription(subId);
   if (!sub) throw new Error('Subscription not found');
   const { price, duration } = sub;
   let client;
   try {
+    client = await MongoClient.connect(process.env.MONGO_URL);
+    const userSub = await client.db(process.env.DB_NAME).collection('userSubscriptions')
+      .findOne({ userId, subscriptionId: subId });
+    if (userSub) throw new Error('User already subscribed');
     const params = {
       FunctionName: `credits-${process.env.STAGE}-getCredits`,
       Payload: JSON.stringify({ requestContext: { authorizer: { principalId: userId } } }),
@@ -45,8 +78,7 @@ const doSubscribe = async (userId, subId) => {
       createdAt: new Date(),
       amount: price,
     };
-    client = await MongoClient.connect(process.env.MONGO_URL);
-    const { insertedId } = await client.db(process.env.DB_NAME).collection('userSubcriptions')
+    const { insertedId } = await client.db(process.env.DB_NAME).collection('userSubscriptions')
       .insertOne(subscription);
     const credParams = {
       FunctionName: `credits-${process.env.STAGE}-removeCredits`,
@@ -69,6 +101,37 @@ export const handleGetSubscription = async (event, context, callback) => {
   try {
     const subId = event.pathParameters.id;
     const results = await doGetSubscription(subId);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(results),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ message: e.message }),
+    };
+    callback(null, response);
+  }
+};
+
+export const handleGetUserSubscriptions = async (event, context, callback) => {
+  const userId = event.requestContext.authorizer.principalId;
+  const urlId = event.pathParameters.id;
+  if (userId !== urlId) {
+    const response = {
+      statusCode: 403,
+      body: JSON.stringify({ message: 'Forbidden' }),
+    };
+    callback(null, response);
+    return;
+  }
+  try {
+    const results = await doGetUserSubscriptions(userId);
     const response = {
       statusCode: 200,
       body: JSON.stringify(results),
