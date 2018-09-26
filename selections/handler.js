4acfac235cafb5b9c396682524b50651f3eae788
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2018-09-26 12:22:25 +0000

    Ludovic/feat/channel subscription

--- selections/handler.js
+++ selections/handler.js
@@ -1,6 +1,9 @@
 import { MongoClient, ObjectId } from 'mongodb';
 import Lambda from 'aws-sdk/clients/lambda';
 import winston from 'winston';
+import doGetSelectionSubscriptions from './libs/doGetSelectionSubscriptions';
+import { doLinkMediaToSelection, doUnlinkMediaFromSelection } from './libs/mediaSelections';
+import { doDeleteUserSelectionTree } from './libs/doDeleteUserSelection';
 
 const selectionFields = [
   'banners',
@@ -95,28 +98,75 @@ const doGetSelection = async (selectionId, userId) => {
 
     const [isAudioSelection, isVideoSelection] = [selectionCollection.includes('audio'), selectionCollection.includes('video')];
     const selectionFindQuery = JSON.parse(selection.selectionFindQuery);
-    selectionFindQuery.isPublished = true;
+    if (selectionFindQuery) selectionFindQuery.isPublished = true;
 
     const sort = JSON.parse(selection.selectionOptionQuery).sort || {};
     const trackSort = {};
     Object.keys(sort).forEach((key) => { trackSort[`track.${key}`] = sort[key]; });
 
-    const audioPromise = isAudioSelection ?
-      client.db(process.env.DB_NAME)
-        .collection('audio')
-        .find(
-          selectionFindQuery,
-          JSON.parse(selection.selectionOptionQuery),
-        ).toArray() : [];
-    const videoPromise = isVideoSelection ?
-      client.db(process.env.DB_NAME)
-        .collection('video')
-        .find(
-          selectionFindQuery,
-          JSON.parse(selection.selectionOptionQuery),
-        ).toArray() : [];
-    const [audioTracks, videoTracks] = await Promise.all([audioPromise, videoPromise]);
-    const rawTracks = audioTracks.concat(videoTracks);
+    const audioPromise = selection.selectionFindQuery ? client.db(process.env.DB_NAME)
+      .collection('audio')
+      .find(
+        selectionFindQuery,
+        JSON.parse(selection.selectionOptionQuery),
+      ).toArray() : [];
+    const videoPromise = selection.selectionFindQuery ? client.db(process.env.DB_NAME)
+      .collection('video')
+      .find(
+        selectionFindQuery,
+        JSON.parse(selection.selectionOptionQuery),
+      ).toArray() : [];
+
+    const mediaChannelPromise = !selection.selectionFindQuery ? client.db(process.env.DB_NAME)
+      .collection('mediumSelectionLinks')
+      .aggregate([
+        { $match: { selectionId } },
+        {
+          $lookup: {
+            from: 'audio',
+            localField: 'mediumId',
+            foreignField: '_id',
+            as: 'audio',
+          },
+        },
+        {
+          $lookup: {
+            from: 'video',
+            localField: 'mediumId',
+            foreignField: '_id',
+            as: 'video',
+          },
+        },
+        {
+          $unwind: {
+            path: '$audio',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $unwind: {
+            path: '$video',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $project: {
+            medium: { $ifNull: ['$audio', '$video'] },
+          },
+        },
+        {
+          $unwind: {
+            path: '$medium',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        { $replaceRoot: { newRoot: '$medium' } },
+      ]).toArray() : [];
+
+    const [audioTracks, videoTracks, mediaChannelTracks] = await Promise.all([
+      audioPromise, videoPromise, mediaChannelPromise,
+    ]);
+    const rawTracks = audioTracks.concat(videoTracks, mediaChannelTracks);
     const projectIds = [...new Set(rawTracks.map(track => track.project_ID))];
     let projects;
 
@@ -311,19 +361,7 @@ const doGetUserRootSelections = async (userId) => {
   }
 };
 
-const doDeleteUserSelection = async (selectionId, userId) => {
-  const client = await MongoClient.connect(process.env.MONGO_URL);
-  try {
-    await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
-      .deleteOne({ _id: selectionId, userId });
-
-    return true;
-  } finally {
-    client.close();
-  }
-};
-
-const doPatchUserSelection = async (selectionId, userId, contentIds, selectionIds, action = 'replace') => {
+const generatePatchUserSelection = async (selectionId, userId, contentIds, selectionIds, action = 'replace') => {
   const client = await MongoClient.connect(process.env.MONGO_URL);
   try {
     if (selectionIds && selectionIds.length > 0) {
@@ -348,8 +386,9 @@ const doPatchUserSelection = async (selectionId, userId, contentIds, selectionId
       await client.db(process.env.DB_NAME)
         .collection(process.env.COLL_NAME)
         .findOne({ _id: selectionId, userId }) : null;
-    const selectionFindQuery = (selection && JSON.parse(selection.selectionFindQuery)) ||
-      { selectionFindQuery: { _id: { $in: [] } } };
+    const selectionFindQuery = (selection && selection.selectionFindQuery &&
+      JSON.parse(selection.selectionFindQuery)) ||
+      (selectionIds && { selectionFindQuery: { _id: { $in: [] } } });
     if (!selectionFindQuery._id) selectionFindQuery._id = { $in: [] };
     if (!selectionFindQuery._id.$in) selectionFindQuery._id.$in = [];
     delete selectionFindQuery._id.$exists;
@@ -397,11 +436,37 @@ const doPatchUserSelection = async (selectionId, userId, contentIds, selectionId
         modifier = { $set: modifier };
       }
     }
+    return modifier;
+  } finally {
+    client.close();
+  }
+};
 
+const doPatchUserSelection = async (selectionId, userId, patch, noCheck) => {
+  if (!noCheck) {
+    const allowedOperations = ['$set'];
+    const allowedFields = ['selectionName', 'isWebPublished', 'isMobilePublished'];
+    Object.keys(patch).forEach(((key) => {
+      if (!allowedOperations.includes(key)) {
+        throw new Error('operation not allowed');
+      }
+      Object.keys(patch[key]).forEach((fKey) => {
+        if (!allowedFields.includes(fKey)) {
+          throw new Error('operation not allowed');
+        }
+      });
+    }));
+  }
+
+  // modify selectionDisplayName with selectionName
+  if (patch.$set.selectionName) patch.$set.selectionDisplayName = patch.$set.selectionName;
+
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  try {
     await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
-      .updateOne({ _id: selectionId, userId }, modifier);
-
-    return true;
+      .updateOne({ _id: selectionId, userId }, patch);
+    return await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
+      .findOne({ _id: selectionId, userId });
   } finally {
     client.close();
   }
@@ -410,30 +475,51 @@ const doPatchUserSelection = async (selectionId, userId, contentIds, selectionId
 const doCreateUserSelection = async (name, userId, parent) => {
   const client = await MongoClient.connect(process.env.MONGO_URL);
   try {
+    const subscriptions = parent ? await doGetSelectionSubscriptions(parent, userId) : [{
+      _id: ObjectId().toString(),
+      userId,
+      createAt: new Date(),
+      price: null,
+      name: null,
+      duration: null,
+      desc: null,
+      banners: null,
+    }];
     const selection = {
       _id: ObjectId().toString(),
       createAt: new Date(),
       date: 'Anytime',
-      isPublished: true,
-      isWebPublished: true,
+      isPublished: false,
+      isWebPublished: false,
+      isMobilePublished: false,
       limit: 10,
       selectionCollection: [
         'audio',
         'video',
       ],
       selectionDisplayName: name,
-      selectionFindQuery: '{"_id": {"$exists": false}}',
       selectionName: name,
       selectionOptionQuery: '{}',
       userId,
+      subscriptionIds: subscriptions.map(item => item._id),
     };
-
-    const { insertedId } = await client.db(process.env.DB_NAME).collection(process.env.COLL_NAME)
-      .insertOne(selection);
     if (parent) {
-      await doPatchUserSelection(parent, userId, undefined, [insertedId], 'add');
+      const parentSelection = await client.db(process.env.DB_NAME)
+        .collection(process.env.COLL_NAME)
+        .findOne({ _id: parent }, { rootSelection: 1, subscriptionIds: 1, userId: 1 });
+      if (!parentSelection) throw new Error('parent selection not exists');
+      if (parentSelection.userId !== userId) throw new Error('parent selection is owned by an other user');
+      selection.rootSelectionId = parentSelection.rootSelectionId || parent;
     }
-    return true;
+
+    const [patch] = await Promise.all([
+      parent ?
+        generatePatchUserSelection(parent, userId, undefined, [selection._id], 'add')
+        : client.db(process.env.DB_NAME).collection('subscriptions').insertMany(subscriptions),
+      client.db(process.env.DB_NAME).collection(process.env.COLL_NAME).insertOne(selection),
+    ]);
+    if (parent) { await doPatchUserSelection(parent, userId, patch, true); }
+    return selection;
   } finally {
     client.close();
   }
@@ -576,10 +662,10 @@ export const handleDeleteUserSelection = async (event, context, callback) => {
   }
   try {
     const { selectionId } = event.pathParameters;
-    const results = await doDeleteUserSelection(selectionId, userId);
+    await doDeleteUserSelectionTree(userId, selectionId);
     const response = {
       statusCode: 200,
-      body: JSON.stringify(results),
+      body: JSON.stringify(true),
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
@@ -608,17 +694,35 @@ export const handlePatchUserSelection = async (event, context, callback) => {
   }
   try {
     const { selectionId } = event.pathParameters;
-    const { contentIds, selectionIds, action } = JSON.parse(event.body);
-    if ((!contentIds && !selectionIds) || ![undefined, 'replace', 'remove', 'add'].includes(action)) {
-      throw new Error('malformed request');
-    }
-    const results = await doPatchUserSelection(
-      selectionId,
-      userId,
-      contentIds,
+    const { contentIds,
       selectionIds,
       action,
-    );
+      patch } = JSON.parse(event.body);
+    if ((!contentIds && !selectionIds && !patch) || ![undefined, 'replace', 'remove', 'add', 'patch'].includes(action)) {
+      throw new Error('malformed request');
+    }
+    let results;
+    if (action === 'patch') {
+      results = await doPatchUserSelection(
+        selectionId,
+        userId,
+        patch,
+      );
+    } else {
+      const modifier = await generatePatchUserSelection(
+        selectionId,
+        userId,
+        contentIds,
+        selectionIds,
+        action,
+      );
+      results = await doPatchUserSelection(
+        selectionId,
+        userId,
+        modifier,
+        true,
+      );
+    }
     const response = {
       statusCode: 200,
       body: JSON.stringify(results),
@@ -635,6 +739,83 @@ export const handlePatchUserSelection = async (event, context, callback) => {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
       },
+      body: JSON.stringify({ message: e.message }),
+    };
+    callback(null, response);
+  }
+};
+
+export const handleGetSelectionSubscriptions = async (event, context, callback) => {
+  const userId = event.requestContext.authorizer.principalId;
+  const selectionId = event.pathParameters.id;
+  try {
+    const subscriptions = await doGetSelectionSubscriptions(selectionId, userId);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ subscriptions }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const statusCode = 500;
+    winston.error(e);
+    const response = {
+      statusCode,
+      body: JSON.stringify({ message: e.message }),
+    };
+    callback(null, response);
+  }
+};
+
+export const handleLinkMediaToSelection = async (event, context, callback) => {
+  const userId = event.requestContext.authorizer.principalId;
+  const selectionId = event.pathParameters.id;
+  try {
+    const { mediaIds } = JSON.parse(event.body);
+    const subscriptions = await doLinkMediaToSelection(userId, selectionId, mediaIds);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ subscriptions }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const statusCode = 500;
+    winston.error(e);
+    const response = {
+      statusCode,
+      body: JSON.stringify({ message: e.message }),
+    };
+    callback(null, response);
+  }
+};
+
+export const handleUnlinkMediaFromSelection = async (event, context, callback) => {
+  const userId = event.requestContext.authorizer.principalId;
+  const selectionId = event.pathParameters.id;
+  try {
+    const { mediaIds } = JSON.parse(event.body);
+    await doUnlinkMediaFromSelection(userId, selectionId, mediaIds);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(true),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const statusCode = 500;
+    winston.error(e);
+    const response = {
+      statusCode,
       body: JSON.stringify({ message: e.message }),
     };
     callback(null, response);
