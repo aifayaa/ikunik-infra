9aeb7c2a098a1384c69f362ff8bf35d7ff2c668e
Djothi Grondin <djothi.grondin@live.fr>
2018-11-12 16:21:22 +0400

    fix(ticketcat): lookup on meta info

--- tickets/lib/getTicketCategory.js
+++ tickets/lib/getTicketCategory.js
@@ -4,9 +4,40 @@ export default async (categoryId) => {
   let client;
   try {
     client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
-    return await client.db(process.env.DB_NAME)
+    const ticketCat = await client.db(process.env.DB_NAME)
       .collection('ticketCategories')
-      .findOne({ _id: categoryId });
+      .aggregate([
+        { $match: { _id: categoryId } },
+        {
+          $lookup: {
+            from: 'lineup',
+            localField: 'lineupId',
+            foreignField: '_id',
+            as: 'lineup',
+          },
+        },
+        {
+          $unwind: {
+            path: '$lineup',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+        {
+          $lookup: {
+            from: 'stages',
+            localField: 'lineup.stageId',
+            foreignField: '_id',
+            as: 'stage',
+          },
+        },
+        {
+          $unwind: {
+            path: '$stage',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+      ]).toArray();
+    return ticketCat[0] || null;
   } finally {
     client.close();
   }
