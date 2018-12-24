c9965dc96c029d727f3f18ac6a53e9858160283a
Djothi Grondin <djothi.grondin@live.fr>
2018-12-24 15:54:15 +0400

    feat(auth): new auth for public MS

---
+++ account/lib/authorizeUser.js
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb';
+
+export default async (hashedToken) => {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  try {
+    const user = await client.db('crowdaaDev').collection('users').findOne(
+      { 'services.resume.loginTokens': { $elemMatch: { hashedToken } } },
+      { projection: { _id: 1 } },
+    );
+    return user;
+  } finally {
+    client.close();
+  }
+};
