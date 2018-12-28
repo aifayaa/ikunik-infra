c9baec1f7eeb0b25bcf1f15ac1c6f5fd60d292c2
Paul SEBASTIEN <paul.sebastien974@gmail.com>
2018-12-28 09:07:33 +0400

    fix(account): authorizePublic deny wrong tokens

--- account/handlers/authorizePublic.js
+++ account/handlers/authorizePublic.js
@@ -16,8 +16,8 @@ export default async ({ headers, methodArn }, context, callback) => {
       winston.info('allow', authorizationToken, user._id);
       return callback(null, generatePolicy('allow', methodArn, user._id));
     }
-    winston.info('allow', authorizationToken);
-    return callback(null, generatePolicy('allow', methodArn, null));
+    winston.info('deny', authorizationToken);
+    return callback(null, generatePolicy('deny', methodArn, null));
   } catch (e) {
     winston.info('allow (error)', e);
     return callback(null, generatePolicy('allow', methodArn, null));
