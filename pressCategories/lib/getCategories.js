d442f3a401c8aacbde8b0b4778471d283f7d6cf3
Djothi Grondin <djothi.grondin@live.fr>
2019-01-21 10:52:26 +0400

    fix(cat): result

--- pressCategories/lib/getCategories.js
+++ pressCategories/lib/getCategories.js
@@ -4,9 +4,10 @@ export default async () => {
   let client;
   try {
     client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
-    return await client.db(process.env.DB_NAME)
+    const categories = await client.db(process.env.DB_NAME)
       .collection(process.env.COLL_NAME)
       .find({}, { sort: { name: -1 } }).toArray();
+    return { categories };
   } finally {
     client.close();
   }
