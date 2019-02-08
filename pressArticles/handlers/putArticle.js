f76e5a4b9ccbf1bbfc8239fc8c9b59089717e107
Djothi Grondin <djothi.grondin@live.fr>
2019-02-08 16:25:13 +0400

    feat(article): api

---
+++ pressArticles/handlers/putArticle.js
@@ -0,0 +1,34 @@
+import putArticle from '../lib/putArticle';
+
+export default async (event, context, callback) => {
+  try {
+    if (!event.body) {
+      throw new Error('mal_formed_request');
+    }
+    const { articleId, categoryId, title, summary, html, md } = JSON.parse(event.body);
+    if (!articleId || !categoryId || !title || !summary || !html || !md) {
+      throw new Error('mal_formed_request');
+    }
+    const userId = event.requestContext.authorizer.principalId;
+    const results = await putArticle(userId, articleId, categoryId, title, summary, html, md);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(results),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ message: e.message }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  }
+};
---
+++ pressArticles/lib/publishArticle.js
@@ -0,0 +1,31 @@
+import { MongoClient } from 'mongodb';
+
+export default async (userId, articleId, draftId) => {
+  const client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+
+  try {
+    const draft = await client.db(process.env.DB_NAME).collection('pressDrafts')
+      .findOne({ articleId, _id: draftId });
+    if (!draft) {
+      throw new Error('Not found');
+    }
+
+    const { title, summary, text, md, _id } = draft;
+    await client.db(process.env.DB_NAME).collection('pressDrafts')
+      .updateOne({ _id: articleId }, {
+        title,
+        summary,
+        text,
+        md,
+        draftId: _id,
+        isPublished: true,
+        publishedBy: userId,
+      });
+
+    return { articleId, draftId };
+  } catch (error) {
+    throw error;
+  } finally {
+    client.close();
+  }
+};
