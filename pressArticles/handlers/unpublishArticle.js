8e344fffbaf6bde189742078a4846b1bb0dcb5bc
Damien ETHEVE <damien@crowdaa.com>
2019-03-27 04:46:46 +0000

    feat(article): add unpublish method

---
+++ pressArticles/handlers/unpublishArticle.js
@@ -0,0 +1,28 @@
+import unpublishArticle from '../lib/unpublishArticle';
+
+export default async (event, context, callback) => {
+  try {
+    const userId = event.requestContext.authorizer.principalId;
+    const articleId = event.pathParameters.id;
+    const results = await unpublishArticle(userId, articleId);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(results),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ message: e.message }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    callback(null, response);
+  }
+};
