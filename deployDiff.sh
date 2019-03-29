39362ad7a9a0a5fa0ca45c50066282a8bd921454
Damien ETHEVE <damien@crowdaa.com>
2019-03-29 13:02:48 +0000

    fix(libs): update ci spec for libs

--- deployDiff.sh
+++ deployDiff.sh
@@ -5,8 +5,10 @@ fichier="folderList"
 for ligne in $(<$fichier)
 do
    echo "___________Deploy de $ligne ___________"
-   cd $ligne
-   npm i
-   node --max-old-space-size=2000 node_modules/serverless/bin/serverless deploy --stage $1 --region $2
-   cd ..
+   if [ $ligne != "libs" ]; then
+      cd $ligne
+      npm i
+      node --max-old-space-size=2000 ../node_modules/serverless/bin/serverless deploy --stage $1 --region $2
+      cd ..
+   fi
 done
