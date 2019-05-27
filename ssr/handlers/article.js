46840fcb814925e4f7adb8c8059df7b3a2ab10ba
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2019-05-27 10:23:46 +0400

    feat(ssr): use appName + add crawler list

--- ssr/handlers/article.js
+++ ssr/handlers/article.js
@@ -1,20 +1,23 @@
 import buildResponse from '../../libs/httpResponses/response';
+import getAppId from '../lib/getAppId';
 import getArticle from '../../pressArticles/lib/getArticle';
-import prepareNotifString from '../../pressArticles/lib/prepareNotifString';
 import meta from '../lib/meta';
+import prepareNotifString from '../../pressArticles/lib/prepareNotifString';
 import redirect from '../lib/redirect';
 
 export default async (event, context, callback) => {
   try {
     const redirectUrl = (event.queryStringParameters || {}).redirect_url;
+    const { appName } = (event.queryStringParameters || {});
     const userAgent = event.headers['User-Agent'];
     const redirectResponse = redirect(userAgent, redirectUrl);
     if (redirectResponse) {
       callback(null, redirectResponse);
       return;
     }
+    const appId = await getAppId(appName);
     const articleId = event.pathParameters.id;
-    const article = await getArticle(articleId, { getPictures: true, isServer: true });
+    const article = await getArticle(articleId, appId, { getPictures: true, isServer: true });
     const body = meta(
       article.title,
       prepareNotifString(article.plainText, 120),
