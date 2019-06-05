fe667b44317136c3511853fa3d75ca0099c42816
Olivier Leclercq <olivier@crowdaa.com>
2019-06-05 08:47:10 +0400

    eslint compliance

--- pressCategories/handlers/postCategory.js
+++ pressCategories/handlers/postCategory.js
@@ -2,33 +2,32 @@ import postCategory from '../lib/postCategory';
 import response from '../../libs/httpResponses/response';
 
 export default async (event, context, callback) => {
-    const catId = event.pathParameters.id;
-    const { appId } = event.requestContext.authorizer;
-    try {
+  const catId = event.pathParameters.id;
+  const { appId } = event.requestContext.authorizer;
+  try {
+    const {
+      name,
+      pathName,
+      color,
+    } = JSON.parse(event.body);
 
-        const {
-            name,
-            pathName,
-            color,
-        } = JSON.parse(event.body);
-
-        if (!name || !pathName || !color) {
-            throw new Error('Missing arguments');
-        }
-        [
-            name,
-            pathName,
-            color,
-        ].forEach((item) => {
-            if (item && typeof item !== 'string') {
-                throw new Error('Wrong argument type');
-            }
-        });
-
-        const results = await postCategory(appId, catId, name, pathName, color);
-        callback(null, response({ code: 200, body: results }));
-    } catch (e) {
-        callback(null, response({ code: 500, message: e.message }));
+    if (!name || !pathName || !color) {
+      throw new Error('Missing arguments');
     }
+    [
+      name,
+      pathName,
+      color,
+    ].forEach((item) => {
+      if (item && typeof item !== 'string') {
+        throw new Error('Wrong argument type');
+      }
+    });
+
+    const results = await postCategory(appId, catId, name, pathName, color);
+    callback(null, response({ code: 200, body: results }));
+  } catch (e) {
+    callback(null, response({ code: 500, message: e.message }));
+  }
 };
 
--- pressCategories/lib/postCategory.js
+++ pressCategories/lib/postCategory.js
@@ -1,56 +1,60 @@
 import { MongoClient, ObjectID } from 'mongodb';
 
+const {
+  MONGO_URL,
+  DB_NAME,
+  COLL_PRESS_CATEGORIES,
+} = process.env;
+
 export default async (userId, appId, name, pathName, color) => {
+  /* Check for parameters */
+  if (
+    typeof name !== 'string'
+    || typeof pathName !== 'string'
+    || typeof color !== 'string'
+  ) {
+    throw new Error('bad arguments');
+  }
 
-    /* Check for parameters */
-    if (
-        typeof name !== 'string'
-        || typeof pathName !== 'string'
-        || typeof color !== 'string'
-    ) {
-        throw new Error('bad arguments');
-    }
+  /* Mongo client */
+  const client = await MongoClient.connect(MONGO_URL, { useNewUrlParser: true });
 
-    /* Mongo client */
-    const client = await MongoClient.connect(process.env.MONGO_URL, { useNewUrlParser: true });
+  try {
+    /* Request for categories having the same appId */
+    const categories = await client.db(DB_NAME)
+      .collection(COLL_PRESS_CATEGORIES)
+      .find({
+        appIds: { $elemMatch: { $eq: appId } },
+      }, { sort: { name: -1 } })
+      .toArray();
 
-    try {
+    /* Check if those categories already have the same pathName and throw an error if so */
+    const errors = [];
 
-        /* Request for categories having the same appId */
-        const categories = await client.db(process.env.DB_NAME)
-            .collection(process.env.COLL_PRESS_CATEGORIES)
-            .find({
-                appIds: { $elemMatch: { $eq: appId } },
-            }, { sort: { name: -1 } })
-            .toArray();
-        
-        /* Check if those categories already have the same pathName and throw an error if so */
-        let errors = [];
-        for(var k in categories) {
-            if(categories[k].pathName == pathName) {
-                errors.push("Pathname " + pathName + " already exists for appId " + appId);
-            }
-        }
+    categories.forEach((category) => {
+      if (category.pathName === pathName) {
+        errors.push(`Pathname ${pathName} already exists for appId ${appId}`);
+      }
+    });
 
-        if (errors.length) throw new Error('Errors: ' + errors.join('\n'));
-        
-        /* Otherwise, insert the category to the database and return it */
-        const category = {
-            _id: new ObjectID().toString(),
-            "name" : name,
-            "pathName" : pathName,
-            "color" : color,
-            "appIds" : [ appId ],
-        };
+    if (errors.length) throw new Error(`Errors: ${errors.join('\n')}`);
 
-        const _id = await client
-            .db(process.env.DB_NAME)
-            .collection(process.env.COLL_PRESS_CATEGORIES)
-            .insertOne(category);
-        
-        return { _id, ...category };
+    /* Otherwise, insert the category to the database and return it */
+    const category = {
+      _id: new ObjectID().toString(),
+      name,
+      pathName,
+      color,
+      appIds: [appId],
+    };
 
-    } finally {
-        client.close();
-    }
+    const _id = await client
+      .db(DB_NAME)
+      .collection(COLL_PRESS_CATEGORIES)
+      .insertOne(category);
+
+    return { _id, ...category };
+  } finally {
+    client.close();
+  }
 };
