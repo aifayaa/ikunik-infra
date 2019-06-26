319b069766a097dc924e89c09304d37d00072b92
Olivier Leclercq <olivier@crowdaa.com>
2019-06-26 11:19:12 +0400

    Using 502 not_implemented for unimplemented functions

--- userGeneratedContents/handlers/getUserGeneratedContents.js
+++ userGeneratedContents/handlers/getUserGeneratedContents.js
@@ -1,13 +1,5 @@
-import getCategory from '../lib/getUserGeneratedContents';
 import response from '../../libs/httpResponses/response';
 
 export default async (event, context, callback) => {
-  const catId = event.pathParameters.id;
-  const { appId } = event.requestContext.authorizer;
-  try {
-    const results = await getCategory(appId, catId);
-    callback(null, response({ code: 200, body: results }));
-  } catch (e) {
-    callback(null, response({ code: 500, message: e.message }));
-  }
+  callback(null, response({ code: 502, message: 'not_implemented' }));
 };
--- userGeneratedContents/handlers/removeUserGeneratedContents.js
+++ userGeneratedContents/handlers/removeUserGeneratedContents.js
@@ -1,35 +1,5 @@
-import checkPerms from '../../libs/perms/checkPerms';
-import removeUserGeneratedContents from '../lib/removeUserGeneratedContents';
 import response from '../../libs/httpResponses/response';
 
-const permKey = 'userGeneratedContents_all';
-
 export default async (event, context, callback) => {
-  try {
-    const perms = JSON.parse(event.requestContext.authorizer.perms);
-    const { appId } = event.requestContext.authorizer;
-    const userId = event.requestContext.authorizer.principalId;
-    const userGeneratedContentsId = event.pathParameters.id;
-    if (!checkPerms(permKey, perms)) {
-      callback(null, response({ code: 403, message: 'access_forbidden' }));
-      return;
-    }
-    const havePermission = await removeUserGeneratedContents.checkPerm(
-      appId,
-      userId,
-      userGeneratedContentsId,
-    );
-    if (!havePermission) {
-      callback(null, response({ code: 403, message: 'unauthorized' }));
-      return;
-    }
-    const results = await removeUserGeneratedContents.process(
-      appId,
-      userId,
-      userGeneratedContentsId,
-    );
-    callback(null, response({ code: 200, body: results }));
-  } catch (e) {
-    callback(null, response({ code: 500, message: e.message }));
-  }
+  callback(null, response({ code: 502, message: 'not_implemented' }));
 };
