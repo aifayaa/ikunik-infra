ba2ebe7f43edd6bcba233c776eabf05770ed4322
Olivier Leclercq <olivier@crowdaa.com>
2019-07-11 14:16:57 +0400

    Removed getUserGeneratedContents as its not implemented yet

--- userGeneratedContents/handlers/getUserGeneratedContents.js
+++
@@ -1,5 +0,0 @@
-import response from '../../libs/httpResponses/response';
-
-export default async (event, context, callback) => {
-  callback(null, response({ code: 502, message: 'not_implemented' }));
-};
