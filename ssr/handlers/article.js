ed9127027f391579c3635569aa8a0d0f01c44e0e
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2019-07-12 08:47:56 +0400

    fix(ssr): return 404 if article not found

--- ssr/handlers/article.js
+++ ssr/handlers/article.js
@@ -5,26 +5,34 @@ import meta from '../lib/meta';
 import prepareNotifString from '../../pressArticles/lib/prepareNotifString';
 import redirect from '../lib/redirect';
 
-export default async (event, context, callback) => {
+export default async (event) => {
   try {
     const redirectUrl = (event.queryStringParameters || {}).redirect_url;
     const { appName } = (event.queryStringParameters || {});
     const userAgent = event.headers['User-Agent'];
     const redirectResponse = redirect(userAgent, redirectUrl);
     if (redirectResponse) {
-      callback(null, redirectResponse);
-      return;
+      return redirectResponse;
     }
     const appId = await getAppId(appName);
     const articleId = event.pathParameters.id;
     const article = await getArticle(articleId, appId, { getPictures: true, isServer: true });
+    if (!article) throw new Error('article_not_found');
     const body = meta(
       article.title,
       prepareNotifString(article.plainText, 120),
       article.pictures[0].mediumUrl,
     );
-    callback(null, buildResponse({ code: 200, body }));
+    return buildResponse({ code: 200, body });
   } catch (e) {
-    callback(null, buildResponse({ code: 500, message: e.message }));
+    let code;
+    switch (e.message) {
+      case 'article_not_found':
+        code = 404;
+        break;
+      default:
+        code = 500;
+    }
+    return buildResponse({ code, message: e.message });
   }
 };
