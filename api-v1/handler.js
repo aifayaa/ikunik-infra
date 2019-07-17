eb19a1291659d45ddda87be34804bfdc316bf0f9
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2019-07-17 11:52:28 +0400

    fix(api-v1): fix handleGetV1

--- api-v1/handler.js
+++ api-v1/handler.js
@@ -1,8 +1,11 @@
-export const handleGetV1 = () => ({
-  statusCode: 200,
-  body: 'API V1 OK',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true,
-  },
-});
+export const handleGetV1 = () =>
+  new Promise((resolve) => {
+    resolve({
+      statusCode: 200,
+      body: 'API V1 OK',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    });
+  });
