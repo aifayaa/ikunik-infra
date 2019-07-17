d316d4923bb37e39d092d9b725582cc7055ec10c
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2019-07-17 11:19:30 +0400

    fix(api-v1): useless async, useless trycatch block

--- api-v1/handler.js
+++ api-v1/handler.js
@@ -1,23 +1,8 @@
-export const handleGetV1 = async (event, context, callback) => {
-  try {
-    const response = {
-      statusCode: 200,
-      body: 'API V1 OK',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-    };
-    callback(null, response);
-  } catch (e) {
-    const response = {
-      statusCode: 500,
-      body: JSON.stringify({ message: e.message }),
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
-      },
-    };
-    callback(null, response);
-  }
-};
+export const handleGetV1 = () => ({
+  statusCode: 200,
+  body: 'API V1 OK',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
+});
