f5605040ce01e45a8a079a493c783e94634046d8
Olivier Leclercq <olivier@crowdaa.com>
2019-08-27 16:07:54 +0400

    Added users/addAppId

---
+++ users/lib/addAppId.js
@@ -0,0 +1,19 @@
+import { MongoClient } from 'mongodb';
+
+export default async (userId, appId) => {
+  const client = await MongoClient.connect(process.env.MONGO_URL);
+  try {
+    const { value } = await client
+      .db(process.env.DB_NAME)
+      .collection(process.env.COLL_USERS)
+      .findAndModify(
+        { _id: userId },
+        [],
+        { $push: { appIds: [appId] } },
+        { new: true, projection: { appIds: true } },
+      );
+    return value;
+  } finally {
+    client.close();
+  }
+};
