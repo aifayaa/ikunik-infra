42260b608b30e52cbb5b76e18595646baae0cfcb
Olivier Leclercq <olivier@crowdaa.com>
2019-09-05 09:34:25 +0400

    Added tests for every ugc lib

---
+++ userGeneratedContents/test/lib/postGeneratedContents.test.js
@@ -0,0 +1,71 @@
+import sinon from 'sinon';
+import { MongoClient } from 'mongodb';
+import { before, describe, it, after } from 'mocha';
+import { expect } from 'chai';
+
+import postUserGeneratedContents from '../../lib/postUserGeneratedContents';
+import spyMongoMethods from '../../../libs/test/spyMongoMethods';
+
+const {
+  MONGO_URL,
+  DB_NAME,
+  COLL_USER_GENERATED_CONTENTS,
+} = process.env;
+
+describe('lib - postUserGeneratedContents', () => {
+  let spyMongo;
+  let stubMongo;
+  const response = {
+    _id: '_id',
+    parentId: 'parentId',
+    parentCollection: 'parentCollection',
+    rootParentId: 'rootParentId',
+    rootParentCollection: 'rootParentCollection',
+    userId: 'userId',
+    appIds: ['crowdaa_app_id'],
+    type: 'type',
+    data: 'data',
+    trashed: false,
+    createdAt: new Date(),
+    modifiedAt: false,
+  };
+
+  before(() => {
+    spyMongo = spyMongoMethods(response);
+    const fakeClient = {
+      db: spyMongo.db,
+      close: spyMongo.close,
+      startSession: spyMongo.startSession,
+    };
+    stubMongo = sinon.stub(MongoClient, 'connect').returns(fakeClient);
+  });
+
+  it('should return a boolean', async () => {
+    const res = await postUserGeneratedContents(
+      'crowdaa_app_id',
+      'parentId',
+      'parentCollection',
+      'rootParentId',
+      'rootParentCollection',
+      'userId',
+      'type',
+      'data',
+    );
+    expect(res).to.be.a('object');
+  });
+
+  it('mongo connection done', () => {
+    sinon.assert.calledWith(stubMongo, MONGO_URL);
+    sinon.assert.calledWith(spyMongo.db, DB_NAME);
+    sinon.assert.called(spyMongo.close);
+  });
+
+  it('should be called with the good args', () => {
+    sinon.assert.calledWith(spyMongo.collection, COLL_USER_GENERATED_CONTENTS);
+    sinon.assert.calledWith(spyMongo.insertOne, spyMongo.insertOne.getCall(0).args[0]);
+  });
+
+  after(() => {
+    stubMongo.restore();
+  });
+});
---
+++ userGeneratedContents/test/lib/removeUserGeneratedContents.test.js
@@ -0,0 +1,53 @@
+import sinon from 'sinon';
+import { MongoClient } from 'mongodb';
+import { before, describe, it, after } from 'mocha';
+import { expect } from 'chai';
+
+import removeUserGeneratedContents from '../../lib/removeUserGeneratedContents';
+import spyMongoMethods from '../../../libs/test/spyMongoMethods';
+
+const {
+  MONGO_URL,
+  DB_NAME,
+  COLL_USER_GENERATED_CONTENTS,
+} = process.env;
+
+describe('lib - removeUserGeneratedContents', () => {
+  let spyMongo;
+  let stubMongo;
+  const response = false;
+
+  before(() => {
+    spyMongo = spyMongoMethods(response);
+    const fakeClient = {
+      db: spyMongo.db,
+      close: spyMongo.close,
+    };
+    stubMongo = sinon.stub(MongoClient, 'connect').returns(fakeClient);
+  });
+
+  it('should return a boolean', async () => {
+    const res = await removeUserGeneratedContents(
+      'crowdaa_app_id',
+      'userId',
+      'userGeneratedContentsId',
+    );
+    expect(res).to.deep.eq(response);
+    expect(res).to.be.a('boolean');
+  });
+
+  it('mongo connection done', () => {
+    sinon.assert.calledWith(stubMongo, MONGO_URL);
+    sinon.assert.calledWith(spyMongo.db, DB_NAME);
+    sinon.assert.called(spyMongo.close);
+  });
+
+  it('should be called with the good args', () => {
+    sinon.assert.calledWith(spyMongo.collection, COLL_USER_GENERATED_CONTENTS);
+    sinon.assert.calledWith(spyMongo.updateOne, spyMongo.updateOne.getCall(0).args[0]);
+  });
+
+  after(() => {
+    stubMongo.restore();
+  });
+});
