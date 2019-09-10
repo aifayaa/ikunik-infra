c0cc447b83453c2734b8e288ed2b8b19c4ce2be5
Olivier Leclercq <olivier@crowdaa.com>
2019-09-10 09:41:08 +0400

    Added handler and lib for getUserMetrics

---
+++ userMetrics/handlers/getUserMetrics.js
@@ -0,0 +1,21 @@
+import getUserMetrics from '../lib/getUserMetrics';
+import response from '../../libs/httpResponses/response';
+
+export default async (event) => {
+  const userMetricsId = event.pathParameters.id;
+  const { appId } = event.requestContext.authorizer;
+
+  try {
+    if (userMetricsId && typeof userMetricsId !== 'string') {
+      throw new Error('Wrong argument type');
+    }
+
+    const results = await getUserMetrics(
+      appId,
+      userMetricsId,
+    );
+    return response({ code: 200, body: results });
+  } catch (e) {
+    return response({ code: 500, message: e.message });
+  }
+};
