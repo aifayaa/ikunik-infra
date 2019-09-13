7983dfba6078edca78fc19b3577143775f6b5808
Olivier Leclercq <olivier@crowdaa.com>
2019-09-13 16:55:53 +0400

    Added crowd searchPress

--- crowd/handlers/search.js
+++ crowd/handlers/search.js
@@ -1,19 +1,25 @@
 import winston from 'winston';
 import buildPipeline from '../lib/buildPipeline';
 import search from '../lib/search';
+import searchPress from '../lib/searchPress';
 import response from '../../libs/httpResponses/response';
 
-export default async (event, context, callback) => {
+export default async (event) => {
   try {
     event.queryStringParameters = event.queryStringParameters || {};
     const userId = event.requestContext.authorizer.principalId;
     const { appId } = event.requestContext.authorizer;
+    if (event.queryStringParameters.type && event.queryStringParameters.type === 'press') {
+      const results = await searchPress(appId, userId, event.queryStringParameters);
+      return response({ code: 200, body: results });
+    }
+
     const pipeline = buildPipeline(userId, appId, event.queryStringParameters);
     Object.assign(event.queryStringParameters, { filterUserInfo: true });
     const results = await search(pipeline, event.queryStringParameters);
-    callback(null, response({ code: 200, body: results }));
+    return response({ code: 200, body: results });
   } catch (e) {
     winston.error(e);
-    callback(null, response({ code: 500, message: e.message }));
+    return response({ code: 500, message: e.message });
   }
 };
---
+++ crowd/lib/searchPress.js
@@ -0,0 +1,117 @@
+import { MongoClient } from 'mongodb';
+
+const {
+  MONGO_URL,
+  DB_NAME,
+  COLL_PRESS_ARTICLES,
+  COLL_USERS,
+  COLL_USER_METRICS,
+} = process.env;
+
+export default async (appId, userId, { page = 1, limit = 20, articleId = '' }) => {
+  if (page && typeof page !== 'number') {
+    page = parseInt(page, 10);
+  }
+  if (limit && typeof limit !== 'number') {
+    limit = parseInt(limit, 10);
+  }
+  const client = await MongoClient.connect(MONGO_URL, { useNewUrlParser: true });
+  try {
+    const $match = {
+      appIds: {
+        $elemMatch: { $eq: appId },
+      },
+      contentCollection: COLL_PRESS_ARTICLES,
+      type: 'time',
+      trashed: false,
+    };
+
+    if (articleId) {
+      $match.contentId = articleId;
+    }
+
+    const pipeline = [
+      {
+        $match,
+      },
+      {
+        $group: {
+          _id: '$userId',
+          user_ID: { $first: '$userId' },
+          elapsedTime: { $sum: '$time' },
+        },
+      },
+      {
+        $lookup: {
+          from: COLL_USERS,
+          localField: 'user_ID',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      {
+        $unwind: {
+          path: '$user',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          user_ID: 1,
+          elapsedTime: 1,
+          'user.profile.username': 1,
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          count: { $sum: 1 },
+          crowd: { $push: '$$ROOT' },
+        },
+      },
+      {
+        $project: {
+          count: 1,
+          crowd: { $slice: ['$crowd', (page - 1) * limit, limit] },
+        },
+      },
+    ];
+
+    const [result] = await client.db(DB_NAME)
+      .collection(COLL_USER_METRICS)
+      .aggregate(pipeline)
+      .toArray();
+
+    const { crowd, count } = result || { crowd: [], count: 0 };
+    return { crowd, count };
+  } finally {
+    client.close();
+  }
+};
+
+// {
+//   "crowd": [
+//     {
+//       "_id": "B5npyCHsZdEyan6ZM",
+//       "user_ID": "B5npyCHsZdEyan6ZM",
+//       "elapsedTime": 1,
+//       "user": {
+//         "profile": {
+//           "username": "Djothi"
+//         }
+//       },
+//     },
+//     {
+//       "_id": "YwawYyTbNQFgmxqHB",
+//       "user_ID": "YwawYyTbNQFgmxqHB",
+//       "elapsedTime": 1,
+//       "user": {
+//         "profile": {
+//           "username": "Ludovic Maillot"
+//         }
+//       },
+//     }
+//   ],
+//   "count": 2
+// }
