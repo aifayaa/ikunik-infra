6d6ae2fa819d20bb8036604da90d1fabd0043120
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2019-10-03 15:23:11 +0400

    feat(account): add authorizer without appKey check

---
+++ account/handlers/authorizeNoApp.js
@@ -0,0 +1,34 @@
+import winston from 'winston';
+import generatePolicy from '../lib/generatePolicy';
+import hashLoginToken from '../lib/hashLoginToken';
+import authorizeUser from '../lib/authorizeUser';
+
+export default async (
+  { headers, methodArn },
+  context,
+  callback,
+) => {
+  const authorizationToken = headers.Authorization;
+  try {
+    winston.info(authorizationToken, methodArn);
+    const loginToken = authorizationToken.split(' ')[1];
+    const hashedLoginToken = hashLoginToken(loginToken);
+    const user = await authorizeUser(hashedLoginToken);
+    if (user) {
+      winston.info('allow', authorizationToken, user._id);
+      return callback(null, generatePolicy('allow', methodArn, { userId: user._id }));
+    }
+    winston.info('deny', authorizationToken);
+    return callback(null, generatePolicy('deny', methodArn));
+  } catch (e) {
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ message: e.message }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+    };
+    return callback(null, response);
+  }
+};
