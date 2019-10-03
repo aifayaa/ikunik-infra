8ae0f28d595d70e8baa5006c8aa60d9b6014e6ae
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2019-10-03 15:24:53 +0400

    feat(apps): add method getAppInfos

---
+++ apps/lib/getAppInfos.js
@@ -0,0 +1,31 @@
+import { MongoClient } from 'mongodb';
+
+const { MONGO_URL, DB_NAME, COLL_APPS } = process.env;
+
+export default async (appId) => {
+  const client = await MongoClient.connect(MONGO_URL, { useNewUrlParser: true });
+  try {
+    const app = await client
+      .db(DB_NAME)
+      .collection(COLL_APPS)
+      .findOne(
+        {
+          _id: appId,
+        },
+        { projection: { name: 1, key: 1 } },
+      );
+
+    if (!app) {
+      return false;
+    }
+
+    const results = {
+      name: app.name,
+      key: app.key,
+    };
+
+    return results;
+  } finally {
+    client.close();
+  }
+};
---
+++ libs/perms/getPerms.js
@@ -0,0 +1,51 @@
+import { MongoClient } from 'mongodb';
+
+const { DB_NAME, COLL_USERS, COLL_PERM_GROUPS, MONGO_URL } = process.env;
+
+export default async (userId, appId) => {
+  const pipeline = [
+    {
+      $match: { _id: userId },
+    },
+    {
+      $lookup: {
+        from: COLL_PERM_GROUPS,
+        localField: 'permGroupIds',
+        foreignField: '_id',
+        as: 'permGroups',
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        permGroups: {
+          $filter: {
+            input: '$permGroups',
+            as: 'permGroup',
+            cond: { $eq: [appId, '$$permGroup.appId'] },
+          },
+        },
+      },
+    },
+  ];
+  const client = await MongoClient.connect(MONGO_URL, { useNewUrlParser: true });
+  try {
+    const [{ permGroups } = {}] = await client
+      .db(DB_NAME)
+      .collection(COLL_USERS)
+      .aggregate(pipeline)
+      .toArray();
+
+    const perms = permGroups.reduce((acc, curr) => {
+      Object.keys(curr.perms).forEach((key) => {
+        if (!acc[key]) {
+          acc[key] = curr.perms[key];
+        }
+      });
+      return acc;
+    }, {});
+    return perms;
+  } finally {
+    client.close();
+  }
+};
