5c37ea5b04c1e0dcf0518a5f6e5def5fb8764d4b
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2019-10-16 09:04:00 +0400

    feat(auth): add email register

---
+++ auth/lib/checkForCaseInsensitiveUserDuplicates.js
@@ -0,0 +1,98 @@
+import { MongoClient } from 'mongodb';
+
+const { MONGO_URL, DB_NAME, COLL_USERS } = process.env;
+
+/*
+  from meteor string_utils
+   https://github.com/meteor/meteor/blob/devel/packages/meteor/string_utils.js
+*/
+const escapeRegExp = string => String(string).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+/*
+  based on meteor accounts-password module
+  method generateCasePermutationsForString from
+  https://github.com/meteor/meteor/blob/devel/packages/accounts-password/password_server.js
+*/
+const generateCasePermutationsForString = (string) => {
+  // Generates permutations of all case variations of a given string.
+  let permutations = [''];
+  for (let i = 0; i < string.length; i += 1) {
+    const ch = string.charAt(i);
+    permutations = [].concat(...permutations.map((prefix) => {
+      const lowerCaseChar = ch.toLowerCase();
+      const upperCaseChar = ch.toUpperCase();
+      // Don't add unneccesary permutations when ch is not a letter
+      if (lowerCaseChar === upperCaseChar) {
+        return [prefix + ch];
+      }
+      return [prefix + lowerCaseChar, prefix + upperCaseChar];
+    }));
+  }
+  return permutations;
+};
+
+/*
+  based on meteor accounts-password module
+  method selectorForFastCaseInsensitiveLookup from
+  https://github.com/meteor/meteor/blob/devel/packages/accounts-password/password_server.js
+*/
+const selectorForFastCaseInsensitiveLookup = (fieldName, string, appId) => {
+  // Performance seems to improve up to 4 prefix characters
+  const prefix = string.substring(0, Math.min(string.length, 4));
+  const orClause = generateCasePermutationsForString(prefix).map((prefixPermutation) => {
+    const selector = {};
+    selector[fieldName] = new RegExp(`^${escapeRegExp(prefixPermutation)}`);
+    return selector;
+  });
+  const caseInsensitiveClause = {};
+  caseInsensitiveClause[fieldName] = new RegExp(`^${escapeRegExp(string)}$`, 'i');
+  return {
+    $and: [{ $or: orClause }, caseInsensitiveClause, { appIds: { $elemMatch: { $eq: appId } } }],
+  };
+};
+
+/*
+  based on meteor accounts-password module
+  method checkForCaseInsensitiveDuplicates from
+  https://github.com/meteor/meteor/blob/devel/packages/accounts-password/password_server.js
+*/
+export default async (
+  appId,
+  fieldName,
+  displayName,
+  fieldValue,
+  { ownUserId, mongoClient } = {},
+) => {
+  if (fieldValue) {
+    const openClient = !!mongoClient;
+
+    if (openClient) {
+      // initiate mongodb connection if no client given in options
+      mongoClient = await MongoClient.connect(MONGO_URL, {
+        useNewUrlParser: true,
+      });
+    }
+    try {
+      const matchedUsers = await mongoClient
+        .db(DB_NAME)
+        .collection(COLL_USERS)
+        .find(selectorForFastCaseInsensitiveLookup(fieldName, fieldValue, appId))
+        .toArray();
+
+      if (
+        matchedUsers.length > 0 &&
+        // If we don't have a userId yet, any match we find is a duplicate
+        (!ownUserId ||
+          // Otherwise, check to see if there are multiple matches or a match
+          // that is not us
+          (matchedUsers.length > 1 || matchedUsers[0]._id !== ownUserId))
+      ) {
+        throw new Error(`${displayName} already exists.`);
+      }
+    } finally {
+      if (openClient) {
+        mongoClient.close();
+      }
+    }
+  }
+};
---
+++ auth/lib/register.js
@@ -0,0 +1,73 @@
+// based on meteor accounts-password module
+// createUser method from
+// https://github.com/meteor/meteor/blob/devel/packages/accounts-password/password_server.js
+
+import uuid from 'uuid';
+import { MongoClient } from 'mongodb';
+import { hashPassword } from './password';
+import checkForCaseInsensitiveUserDuplicates from './checkForCaseInsensitiveUserDuplicates';
+
+const { DB_NAME, COLL_USERS, COLL_APPS } = process.env;
+
+export const register = async (email, username, password, appId) => {
+  const client = await MongoClient.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+  });
+
+  try {
+    const usersCollection = client.db(DB_NAME).collection(COLL_USERS);
+    const appsCollection = client.db(DB_NAME).collection(COLL_APPS);
+    const app = await appsCollection.findOne({ _id: appId }, { projection: { _id: true } });
+    if (!app) throw new Error('app_not_found');
+
+    const hashed = await hashPassword(password);
+    const newUser = {
+      _id: uuid.v4(),
+      createdAt: new Date(),
+      username,
+      emails: [{ address: email, verified: false }],
+      services: {
+        password: {
+          bcrypt: hashed,
+        },
+      },
+      appIds: [appId],
+      profile: {
+        username,
+      },
+    };
+
+    // Perform a case insensitive check before insert
+    await checkForCaseInsensitiveUserDuplicates(appId, 'username', 'Username', username, {
+      mongoClient: client,
+    });
+    await checkForCaseInsensitiveUserDuplicates(appId, 'emails.address', 'Email', email, {
+      mongoClient: client,
+    });
+    const { insertedId: userId } = await usersCollection.insertOne(newUser);
+
+    // Perform another check after insert, in case a matching user has been
+    // inserted in the meantime
+    try {
+      await checkForCaseInsensitiveUserDuplicates(appId, 'username', 'Username', username, {
+        ownUserId: userId,
+        mongoClient: client,
+      });
+      await checkForCaseInsensitiveUserDuplicates(appId, 'emails.address', 'Email', email, {
+        ownUserId: userId,
+        mongoClient: client,
+      });
+    } catch (ex) {
+      // Remove inserted user if the check fails
+      await usersCollection.removeOne({ _id: userId });
+      throw ex;
+    }
+
+    // TODO: send verification email here
+    // see packages/accounts-password/password_server.js:866
+
+    return { userId };
+  } finally {
+    client.close();
+  }
+};
