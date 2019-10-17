21e679ccff105302fe9463010454343b9f6afb73
Olivier Leclercq <olivier@crowdaa.com>
2019-10-17 15:04:05 +0400

    Moved actions checks to a separate lib to avoid code repetition

--- pressArticles/handlers/postArticle.js
+++ pressArticles/handlers/postArticle.js
@@ -1,5 +1,4 @@
 import removeMd from 'remove-markdown';
-import { URL } from 'url';
 import defaultSettings from '../lib/xmlParsing/settings/default.json';
 import getInfos from '../lib/xmlParsing/getInfos';
 import mdToHtml from '../lib/mdParsing/mdToHtml';
@@ -12,10 +11,7 @@ import { doSendNotifications } from '../lib/sendNotifications';
 import { getArticle } from '../lib/getArticle';
 import { postArticle } from '../lib/postArticle';
 import { publishArticle } from '../lib/publishArticle';
-
-const stringIsAValidUrl = (s) => {
-  try { return new URL(s); } catch (err) { return false; }
-};
+import checkActions from '../lib/checks/checkActions';
 
 const permKey = 'pressArticles_all';
 
@@ -88,19 +84,11 @@ export default async (event) => {
       || !html
       || !(md || xml)
       || !pictures
-      || !Array.isArray(actions)
     ) {
       throw new Error('mal_formed_request');
     }
 
-    actions.forEach((action) => {
-      if (!action.title || !action.url) {
-        throw new Error('mal_formed_request');
-      }
-      if (!stringIsAValidUrl(action.url)) {
-        throw new Error('URL error');
-      }
-    });
+    checkActions(actions);
 
     const userId = event.requestContext.authorizer.principalId;
     let results = await postArticle({
--- pressArticles/handlers/putArticle.js
+++ pressArticles/handlers/putArticle.js
@@ -1,13 +1,9 @@
 import removeMd from 'remove-markdown';
-import { URL } from 'url';
 import mdToHtml from '../lib/mdParsing/mdToHtml';
 import response from '../../libs/httpResponses/response';
 import { checkPerms } from '../../libs/perms/checkPerms';
 import { putArticle } from '../lib/putArticle';
-
-const stringIsAValidUrl = (s) => {
-  try { return new URL(s); } catch (err) { return false; }
-};
+import checkActions from '../lib/checks/checkActions';
 
 const permKey = 'pressArticles_all';
 
@@ -21,7 +17,13 @@ export default async (event) => {
     if (!event.body) {
       throw new Error('mal_formed_request');
     }
-    const { actions, articleId, categoryId, title, summary, md, pictures } = JSON.parse(event.body);
+    const bodyParsed = JSON.parse(event.body);
+    const { articleId, categoryId, title, summary, md, pictures } = bodyParsed;
+    let { actions } = bodyParsed;
+
+    if (!actions) {
+      actions = [];
+    }
 
     if (
       !articleId
@@ -30,20 +32,11 @@ export default async (event) => {
       || !summary
       || !md
       || !pictures
-      || !actions
-      || !Array.isArray(actions)
     ) {
       throw new Error('mal_formed_request');
     }
 
-    actions.forEach((action) => {
-      if (!action.title || !action.url) {
-        throw new Error('mal_formed_request');
-      }
-      if (!stringIsAValidUrl(action.url)) {
-        throw new Error('URL error');
-      }
-    });
+    checkActions(actions);
 
     const userId = event.requestContext.authorizer.principalId;
     const results = await putArticle({
