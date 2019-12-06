5a2cdcc8e4a081dc6520e84411d344dd8abdfd9a
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2019-12-06 15:47:51 +0400

    feat(auth/loginApple): working login with apple

---
+++ auth/handlers/appleLogin.js
@@ -0,0 +1,36 @@
+import response from '../../libs/httpResponses/response';
+import { getUserByApple } from '../lib/getUserByApple';
+
+export default async (event) => {
+  try {
+    if (!event.body) {
+      throw new Error('missing_payload');
+    }
+
+    const {
+      authorizationCode,
+      identityToken,
+      fullName,
+    } = JSON.parse(event.body);
+    const { appId } = event.requestContext.authorizer;
+    const tokenInfo = await getUserByApple(authorizationCode, identityToken, appId, {
+      fullName,
+    });
+
+    /* get User in db or create new one if not exists */
+    return response({ code: 200, body: tokenInfo });
+  } catch (e) {
+    let code;
+    switch (e.message) {
+      case 'invalid_token':
+        code = 401;
+        break;
+      case 'missing_payload':
+        code = 400;
+        break;
+      default:
+        code = 500;
+    }
+    return response({ code, message: e.message });
+  }
+};
