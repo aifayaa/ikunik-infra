54391708177bfbdf482eab69d1a673a07b56134c
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-06 10:49:09 +0400

    feat(account): use node12.x, packages update

--- account/webpack.config.js
+++ account/webpack.config.js
@@ -4,12 +4,12 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   externals: [nodeExternals()],
   module: {
     rules: [
       {
         test: /\.(jsx?)$/,
-        loaders: ['babel-loader'],
         include: __dirname,
         exclude: /node_modules/,
       },
