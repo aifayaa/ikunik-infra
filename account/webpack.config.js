b3de0d59bcc0e73024c785f725c548994d1e7a4c
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-07 09:49:31 +0400

    feat(account/authorizePublic): manage empty appId, promise oriented

--- account/webpack.config.js
+++ account/webpack.config.js
@@ -1,18 +1,23 @@
+const path = require('path');
 const slsw = require('serverless-webpack');
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  externals: [nodeExternals()],
+  devtool: 'nosources-source-map',
   module: {
     rules: [
       {
-        test: /\.(jsx?)$/,
-        include: __dirname,
+        test: /\.js$/,
         exclude: /node_modules/,
       },
     ],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
   },
 };
