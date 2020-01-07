402af2baadcd595c81a5a6b32f2a8a93b541caaa
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-07 16:21:46 +0400

    feat(carts): use node12, update packages

--- carts/webpack.config.js
+++ carts/webpack.config.js
@@ -11,11 +11,6 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
       },
     ],
   },
