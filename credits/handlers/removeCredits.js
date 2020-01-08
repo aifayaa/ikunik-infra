b481120084f0b9cc7e3c54b51e9a3f52f76f887d
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-08 14:46:55 +0400

    feat(credits): Promise complient handlers

--- credits/handlers/removeCredits.js
+++ credits/handlers/removeCredits.js
@@ -4,8 +4,8 @@ import removeCredits from '../lib/removeCredits';
 export default async ({ userId, appId, amount }, context, callback) => {
   try {
     const results = await removeCredits(userId, appId, amount);
-    callback(null, response({ code: 200, body: results }));
+    return response({ code: 200, body: results });
   } catch (e) {
-    callback(null, response({ code: 500, message: e.message }));
+    return response({ code: 500, message: e.message });
   }
 };
--- credits/lib/getCredits.js
+++ credits/lib/getCredits.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 export default async (userID, appId) => {
-  const client = await MongoClient.connect(MONGO_URL);
+  const client = await MongoClient.connect(MONGO_URL, { useUnifiedTopology: true });
   try {
     const credits = await client
       .db(DB_NAME)
