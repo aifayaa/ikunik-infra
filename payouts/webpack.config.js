532db7e5cf27789e6baa8db5f57058b21029cdd5
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-10 10:35:54 +0400

    feat(payouts): use node12, update packages

--- payouts/webpack.config.js
+++ payouts/webpack.config.js
@@ -1,18 +1,24 @@
+const path = require('path');
 const slsw = require('serverless-webpack');
-const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
+  stats: 'errors-only',
   target: 'node',
-  externals: [nodeExternals()],
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  devtool: 'nosources-source-map',
   module: {
     rules: [
       {
-        test: /\.(jsx?)$/,
-        loaders: ['babel-loader'],
-        include: __dirname,
+        test: /\.js$/,
         exclude: /node_modules/,
-      },
+      }
     ],
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
   },
 };
