fb9bd4c5aae5546b59215422cea259ff1d56274d
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-10 10:10:31 +0400

    feat(media): promise oriented handlers

--- media/handlers/checkUserMedia.js
+++ media/handlers/checkUserMedia.js
@@ -1,11 +1,11 @@
 import checkUserMedia from '../lib/checkUserMedia';
 import response from '../../libs/httpResponses/response';
 
-export default async ({ userId, mediaIds, appId }, _context, callback) => {
+export default async ({ userId, mediaIds, appId }) => {
   try {
     const results = await checkUserMedia(userId, appId, mediaIds);
-    callback(null, response({ code: 200, body: results }));
+    return response({ code: 200, body: results });
   } catch (e) {
-    callback(null, response({ code: 500, message: e.message }));
+    return response({ code: 500, message: e.message });
   }
 };
--- media/handlers/postMediumView.js
+++ media/handlers/postMediumView.js
@@ -1,15 +1,15 @@
 import postMediumView from '../lib/postMediumView';
 import response from '../../libs/httpResponses/response';
 
-export default async (event, _context, callback) => {
+export default async (event) => {
   try {
     const userId = event.requestContext.authorizer.principalId;
     const { appId } = event.requestContext.authorizer;
     const mediumType = event.pathParameters.type;
     const mediumId = event.pathParameters.id;
     const results = await postMediumView(userId, appId, mediumType, mediumId);
-    callback(null, response({ code: 200, body: results }));
+    return response({ code: 200, body: results });
   } catch (e) {
-    callback(null, response({ code: 500, message: e.message }));
+    return response({ code: 500, message: e.message });
   }
 };
