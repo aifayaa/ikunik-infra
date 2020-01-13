415f5aab8d962f31c47b84b6f91533a5fb6393a2
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-13 17:02:04 +0400

    feat(stages): promise oriented handlers

--- stages/handlers/getStage.js
+++ stages/handlers/getStage.js
@@ -1,13 +1,16 @@
 import doGetStage from '../lib/getStage';
 import response from '../../libs/httpResponses/response';
 
-export const handleGetStage = async (event, context, callback) => {
+export const handleGetStage = async (event) => {
   const stageId = event.pathParameters.id;
   const { appId } = event.requestContext.authorizer;
   try {
     const results = await doGetStage(stageId, appId);
-    callback(null, response({ code: 200, body: results }));
+    if (results) {
+      return response({ code: 200, body: results });
+    }
+    return response({ code: 500, message: 'stage_not_found' });
   } catch (e) {
-    callback(null, response({ code: 500, message: e.message }));
+    return response({ code: 500, message: e.message });
   }
 };
