263ac76166ce7b0028ce61593c9b0d2df69d0f1e
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-16 14:22:52 +0400

    fix(tos): fix getTos without queryStringParameters

--- termsOfServices/handlers/getTos.js
+++ termsOfServices/handlers/getTos.js
@@ -6,11 +6,13 @@ export default async (event) => {
   const tosId = event.pathParameters && event.pathParameters.id;
   const options = {};
   try {
-    ['outdated', 'required'].forEach((v) => {
-      if (typeof event.queryStringParameters[v] !== 'undefined') {
-        options[v] = event.queryStringParameters[v] === 'true';
-      }
-    });
+    if (event.queryStringParameters) {
+      ['outdated', 'required'].forEach((v) => {
+        if (typeof event.queryStringParameters[v] !== 'undefined') {
+          options[v] = event.queryStringParameters[v] === 'true';
+        }
+      });
+    }
     const results = await getTos(appId, tosId, options);
     if (results.length) return response({ code: 200, body: results });
     return response({ code: 404, message: 'tos_not_found' });
