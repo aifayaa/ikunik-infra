2f54c52aff7fe4f13d8e65fcdff67d6a8f4ca1b3
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-01-16 11:10:51 +0400

    feat(subscriptions): promise oriented handlers

--- subscriptions/handlers/getUserSubscriptions.js
+++ subscriptions/handlers/getUserSubscriptions.js
@@ -1,18 +1,17 @@
 import getUserSubscriptions from '../lib/getUserSubscriptions';
 import response from '../../libs/httpResponses/response';
 
-export default async (event, _context, callback) => {
+export default async (event) => {
   const { appId } = event.requestContext.authorizer;
   const userId = event.requestContext.authorizer.principalId;
   const urlId = event.pathParameters.id;
   if (userId !== urlId) {
-    callback(null, response({ code: 403, message: 'Forbidden' }));
-    return;
+    return response({ code: 403, message: 'Forbidden' });
   }
   try {
     const results = await getUserSubscriptions(userId, appId);
-    callback(null, response({ code: 200, body: results }));
+    return response({ code: 200, body: results });
   } catch (e) {
-    callback(null, response({ code: 500, message: e.message }));
+    return response({ code: 500, message: e.message });
   }
 };
