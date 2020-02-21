a484d17e45b1888dd2ccda495bfe67385d15491f
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-02-21 09:49:24 +0400

    fix(getSelection): add comment about runhit bug

--- selections/handlers/getSelection.js
+++ selections/handlers/getSelection.js
@@ -3,16 +3,33 @@ import getSelection from '../libs/getSelection';
 import response from '../../libs/httpResponses/response';
 
 export default async (event) => {
+  const selectionId = event.pathParameters.id;
+  const userId = event.requestContext.authorizer.principalId;
+  const { appId } = event.requestContext.authorizer;
   try {
-    const selectionId = event.pathParameters.id;
-    const userId = event.requestContext.authorizer.principalId;
-    const { appId } = event.requestContext.authorizer;
     if (!selectionId) throw new Error('Missing id');
     const results = await getSelection(selectionId, userId, appId);
+    if (!results) throw new Error('not_found');
     return response({ code: 200, body: results });
   } catch (e) {
-    const code = (e.message === 'Not found') ? 404 : 500;
+    const code = (e.message === 'not_found') ? 404 : 500;
     winston.error(e);
     return response({ code, message: e.message });
   }
 };
+
+// BUG: a bug occured on awax stage where getSelection randomly return 404,
+// a quick fix was to add this portion of code which store
+// selection result on a collection, but selections can be user
+// related, that's the case for subscription content which is related
+// to user subscriptions, activate this code will broke subscriptions:
+//
+// import doGetBackupSelection from '../libs/doGetBackupSelection';
+// import doRegisterBackupSelection from '../libs/doRegisterBackupSelection';
+//
+// if (!results) {
+//   results = await doGetBackupSelection(selectionId);
+//   if (!results) throw new Error('not_found');
+// } else {
+//   await doRegisterBackupSelection(results);
+// }
--- selections/handlers/getSelections.js
+++ selections/handlers/getSelections.js
@@ -6,14 +6,6 @@ export default async (event) => {
   try {
     const { type, web, mobile, root } = event.queryStringParameters || {};
     const { appId } = event.requestContext.authorizer;
-    console.log('getSelections ');
-    console.log({
-      type,
-      web,
-      mobile,
-      root,
-      appId,
-    });
     const results = await getSelections({ type, web, mobile, root, appId });
     return response({ code: 200, body: results });
   } catch (e) {
