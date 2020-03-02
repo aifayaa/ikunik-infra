180234bb11e3a3532cef3e13dfca2abe975b09ea
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-03-02 11:48:53 +0400

    fix(blast): use env for androidPackage and iosAppId

--- blast/lib/emailUtils.js
+++ blast/lib/emailUtils.js
@@ -1,3 +1,8 @@
+const {
+  ANDROID_PACKAGE_ID = 'com.crowdaa.free.www',
+  IOS_APP_ID = '1074256465',
+} = process.env;
+
 export const generateEmailHTML = ({ name, data, html }, contact) => {
   if (html) return html;
   switch (name) {
@@ -17,10 +22,10 @@ export const generateEmailHTML = ({ name, data, html }, contact) => {
                   <p class="text-center"><img src="${data.image}" /></p><!-- /hero -->
                   <h3><strong>"${data.album}"</strong> est disponible!</h3>
                   <p>${data.message}</p>
-                  <a style="margin-left: 30px;" href="https://play.google.com/store/apps/details?id=com.crowdaa.awax.www">
+                  <a style="margin-left: 30px;" href="https://play.google.com/store/apps/details?id=${ANDROID_PACKAGE_ID}">
                   <img style="width: 200px; " src="http://website-2068.kxcdn.com/android_btn.png">
                   </a>
-                  <a style="margin-left: 30px;" href="https://itunes.apple.com/fr/app/crowdaa/id1446017390?ls=1&mt=8" >
+                  <a style="margin-left: 30px;" href="https://itunes.apple.com/fr/app/crowdaa/id${IOS_APP_ID}?ls=1&mt=8" >
                   <img style="width: 200px; " src="http://website-2068.kxcdn.com/AppStoreButton.gif">
                   </a>
                   <br/>
