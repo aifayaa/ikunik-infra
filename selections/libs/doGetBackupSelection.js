f6f0cbd12f8996e5b4ed645f62a517afc84b9db5
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-03-03 10:10:34 +0400

    fix(selections): use destructuration

--- selections/libs/doGetBackupSelection.js
+++ selections/libs/doGetBackupSelection.js
@@ -1,11 +1,16 @@
 import MongoClient from '../../libs/mongoClient';
 
+const {
+  DB_NAME,
+  COLL_SELECTIONS_BACKUP,
+} = process.env;
+
 export default async (selectionId) => {
   const client = await MongoClient.connect();
   try {
     const selection = await client
-      .db(process.env.DB_NAME)
-      .collection(process.env.COLL_SELECTIONS_BACKUP)
+      .db(DB_NAME)
+      .collection(COLL_SELECTIONS_BACKUP)
       .findOne({ _id: selectionId });
     if (!selection) throw new Error('Not found');
     return selection;
--- selections/libs/doRegisterBackupSelection.js
+++ selections/libs/doRegisterBackupSelection.js
@@ -1,11 +1,16 @@
 import MongoClient from '../../libs/mongoClient';
 
+const {
+  DB_NAME,
+  COLL_SELECTIONS_BACKUP,
+} = process.env;
+
 export default async (selection) => {
   const client = await MongoClient.connect();
   try {
     await client
-      .db(process.env.DB_NAME)
-      .collection(process.env.COLL_SELECTIONS_BACKUP)
+      .db(DB_NAME)
+      .collection(COLL_SELECTIONS_BACKUP)
       .replaceOne({ _id: selection._id }, selection, { upsert: true });
     return;
   } finally {
