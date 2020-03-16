2b2b2e257c3003befd80f6e141a917b427c55f49
Ludovic MAILLOT <maillot.ludovic.nicolas@gmail.com>
2020-03-16 15:28:07 +0400

    fix(postCategory): set pathname as not required

--- pressCategories/lib/postCategory.js
+++ pressCategories/lib/postCategory.js
@@ -10,16 +10,25 @@ export default async (appId, name, pathName, color, picture) => {
   const client = await MongoClient.connect();
 
   try {
-    /* Request for categories having the same appId */
+    /* Request for categories having the same appId and name or pathName */
+    const queryExists = {
+      $or: [{ name }],
+      appIds: { $elemMatch: { $eq: appId } },
+    };
+    if (pathName) queryExists.$or.push({ pathName });
     const categoryFound = await client.db(DB_NAME)
       .collection(COLL_PRESS_CATEGORIES)
-      .findOne({
-        pathName: { $elemMatch: { $eq: pathName } },
-        appIds: { $elemMatch: { $eq: appId } },
-      });
+      .findOne(queryExists);
 
     if (categoryFound) {
-      return `Pathname ${pathName} already exists for appId ${appId}`;
+      let errorCatFound = `Already exists for appId ${appId}`;
+      if (pathName && pathName === categoryFound.pathName) {
+        errorCatFound = `Pathname ${pathName}, ${errorCatFound}`;
+      }
+      if (name === categoryFound.name) {
+        errorCatFound = `Name ${name}, ${errorCatFound}`;
+      }
+      return errorCatFound;
     }
 
     /* Otherwise, insert the category to the database and return it */
