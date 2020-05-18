2c1b0adf120307d7bcc89389991bd6d3dad80a55
Ludovic Maillot <ludovic@crowdaa.com>
2020-05-18 16:37:09 +0400

    feat(blast): add email template 'default'

--- blast/lib/emailUtils.js
+++ blast/lib/emailUtils.js
@@ -60,6 +60,56 @@ export const generateEmailHTML = ({ name, data, html }, contact) => {
         </tr>
       </table><!-- /FOOTER -->
     `;
+    case 'default':
+      return `
+      <table class="body-wrap">
+        <tr>
+          <td></td>
+          <td style="background-color: #ebebeb;">
+            <div class="content">
+            <table>
+              <tr>
+                <td>
+                  <h3>Salut <i>${contact.name}</i> !</h3>
+                  <p>${data.message}</p>
+                  <a style="margin-left: 30px;" href="https://play.google.com/store/apps/details?id=${ANDROID_PACKAGE_ID}">
+                  <img style="width: 200px; " src="http://website-2068.kxcdn.com/android_btn.png">
+                  </a>
+                  <a style="margin-left: 30px;" href="https://apps.apple.com/us/app/id${IOS_APP_ID}?ls=1&mt=8" >
+                  <img style="width: 200px;" src="http://website-2068.kxcdn.com/AppStoreButton.gif">
+                  </a>
+                  <br/>
+                  <br/>
+                </td>
+              </tr>
+            </table>
+            </div>
+          </td>
+          <td></td>
+        </tr>
+        </table><!-- /BODY -->
+        <!-- FOOTER -->
+        <table class="footer-wrap">
+        <tr>
+          <td></td>
+          <td class="container">
+              <!-- content -->
+              <div class="content">
+              <table>
+              <tr>
+                <td align="center">
+                  <p>
+                    <a href="%unsubscribe_url%"><unsubscribe>Se désabonner</unsubscribe></a>
+                  </p>
+                </td>
+              </tr>
+            </table>
+              </div><!-- /content -->
+          </td>
+          <td></td>
+        </tr>
+      </table><!-- /FOOTER -->
+    `;
     default:
       return null;
   }
