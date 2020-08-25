3363c6cb1f561a2409ad755cfafe49c30ef53edf
Olivier Leclercq <olivier@crowdaa.com>
2020-08-25 16:01:28 +0400

    Added end point to handle ugc moderation

---
+++ userGeneratedContents/lib/reviewUserGeneratedContents.js
@@ -0,0 +1,49 @@
+import MongoClient from '../../libs/mongoClient';
+
+const {
+  DB_NAME,
+  COLL_USER_GENERATED_CONTENTS,
+} = process.env;
+
+export default async (
+  appId,
+  userId,
+  ugc,
+  {
+    reason = '',
+    valid,
+  } = {},
+) => {
+  /* Mongo client */
+  const client = await MongoClient.connect();
+
+  try {
+    const $set = {
+      reviewed: true,
+      modifiedAt: new Date(),
+    };
+
+    if (!valid) {
+      $set.moderated = true;
+
+      if (reason) {
+        $set.reason = reason;
+      }
+    }
+
+    const { matchedCount } = await client
+      .db(DB_NAME)
+      .collection(COLL_USER_GENERATED_CONTENTS)
+      .updateOne(
+        {
+          _id: ugc._id,
+          appIds: { $elemMatch: { $eq: appId } },
+        },
+        { $set },
+      );
+
+    return !!matchedCount;
+  } finally {
+    client.close();
+  }
+};
