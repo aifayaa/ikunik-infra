6bf48d85bd49e0a09e9977e11ce6ad8f0afc308a
Amelie Salaun <ameliesalaun@Mac-mini-de-Mac.local>
2020-09-21 08:35:55 +0400

    Add sorting & html query string

--- apps/handlers/getAppTos.js
+++ apps/handlers/getAppTos.js
@@ -3,14 +3,27 @@ import response from '../../libs/httpResponses/response';
 
 export default async (event) => {
   const appId = event.pathParameters.id;
-
+  const options = {};
   try {
-    const results = await getAppTos(appId);
-
-    if (results === false) {
-      return response({ code: 404, message: 'tos_not_found' });
+    if (event.queryStringParameters) {
+      ['outdated', 'required', 'html'].forEach((v) => {
+        if (typeof event.queryStringParameters[v] !== 'undefined') {
+          options[v] = event.queryStringParameters[v] === 'true';
+        }
+      });
     }
-    return response({ code: 200, body: results });
+    const results = await getAppTos(appId, options);
+    if (results.length && event.queryStringParameters.html === 'true') {
+      return response({
+        code: 200,
+        headers: {
+          'Content-Type': 'text/html',
+        },
+        body: results,
+      });
+    }
+    if (results.length) return response({ code: 200, body: results });
+    return response({ code: 404, message: 'tos_not_found' });
   } catch (e) {
     return response({ code: 500, message: e.message });
   }
--- apps/lib/getAppTos.js
+++ apps/lib/getAppTos.js
@@ -1,18 +1,34 @@
 import MongoClient from '../../libs/mongoClient';
+import tosFields from '../tosFieldsProjection.json';
 
 const {
   COLL_TOS,
   DB_NAME,
 } = process.env;
 
-export const getAppTos = async (appId) => {
+export const getAppTos = async (appId, options = {}) => {
+  const query = {
+    appIds: { $elemMatch: { $eq: appId } },
+  };
+
+  if (typeof options.outdated !== 'undefined') {
+    query.outdated = {
+      $exists: options.outdated,
+    };
+  }
+  if (typeof options.required !== 'undefined') {
+    query.required = options.required;
+  }
+
+  const { public: projection } = tosFields;
+
   const client = await MongoClient.connect();
   try {
     const tosbyapp = await client
       .db(DB_NAME)
       .collection(COLL_TOS)
-      .find({
-        appIds: { $elemMatch: { $eq: appId } },
+      .find(query, {
+        projection,
       }).toArray();
     return tosbyapp;
   } finally {
