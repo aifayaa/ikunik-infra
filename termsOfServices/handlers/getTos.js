b12ff250abad6442276eef4ec2c5a633f04bfadb
Amelie Salaun <ameliesalaun@Mac-mini-de-Mac.local>
2020-10-07 09:32:28 +0400

    Manage case & convert accept to array

--- termsOfServices/handlers/getTos.js
+++ termsOfServices/handlers/getTos.js
@@ -18,8 +18,11 @@ export default async (event) => {
     const results = await getTos(appId, tosId, options);
     const body = getHtmlResults(results);
 
+    const accept = event.headers['accept'] || event.headers['Accept'];
+    const acceptArray = accept.split(accept, ',');
+
     if (results) {
-      if (event.headers.Accept === 'text/html') {
+      if (acceptArray.includes('text/html')) {
         return response({
           code: 200,
           headers: {
