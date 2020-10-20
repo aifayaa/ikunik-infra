963cf8c834ccba53ede31522816d22190c1fdbcf
Amelie Salaun <ameliesalaun@Mac-mini-de-Mac.local>
2020-10-20 11:09:56 +0400

    Test getHtmlResults

--- apps/test/handlers/getAppTos.test.js
+++ apps/test/handlers/getAppTos.test.js
@@ -2,11 +2,14 @@ import sinon from 'sinon';
 import { describe, it, before, after } from 'mocha';
 import { expect } from 'chai';
 
+import * as getHtmlResults from '../../../termsOfServices/htmlResults';
 import * as lib from '../../../termsOfServices/lib/getTos';
 import handler from '../../handlers/getAppTos';
 
 describe('handlers - getAppTos', () => {
   let stubLib;
+  let stubHtmlLib;
+
   const event = {
     pathParameters: {
       id: 'appId',
@@ -19,13 +22,15 @@ describe('handlers - getAppTos', () => {
     let response;
     before(async () => {
       // Create a sandbox to remove track of every fake created
-      // Wrapping the existing function in a stub function & return null
-      stubLib = sandbox.stub(lib, 'getTos').returns(null);
+      // Wrapping the existing function in a stub function & return empty array
+      stubLib = sandbox.stub(lib, 'getTos').returns([{}]);
+      // stubHtmlLib = sandbox.stub(getHtmlResults, 'getHtmlResults').returns([{}]);
       response = await handler(event);
     });
     // Passes if stubLib spy was called only once
     it('should call lib once', () => {
       expect(stubLib.calledOnce).to.be.true;
+      // expect(stubHtmlLib.calledOnce).to.be.true;
     });
     // Passes if stubLib spy was called with the right params
     it('should call lib with right params', () => {
@@ -54,11 +59,14 @@ describe('handlers - getAppTos', () => {
       stubLib = sandbox.stub(lib, 'getTos').returns([{
         _id: 'crowdaa_app_id',
       }]);
+      // stubHtmlLib = sandbox.stub(getHtmlResultsLib, 'getHtmlResults').returns();
+      // console.log(stubHtmlLib);
       response = await handler(event);
     });
     it('should call lib once', () => {
       // Passes if stubLib spy was called only once
       expect(stubLib.calledOnce).to.be.true;
+      // expect(stubHtmlLib.calledOnce).to.be.true;
     });
 
     it('should call lib with right params', () => {
@@ -83,11 +91,14 @@ describe('handlers - getAppTos', () => {
     before(async () => {
       // Create a promise reject message to fake error response
       stubLib = sandbox.stub(lib, 'getTos').callsFake(() => Promise.reject(new Error('error_message')));
+      stubHtmlLib = sandbox.stub(getHtmlResults, 'getHtmlResults').callsFake(() => Promise.reject(new Error('error_message')));
       response = await handler(event);
     });
     // Passes if stubLib spy was called only once
     it('should call lib once', () => {
       expect(stubLib.calledOnce).to.be.true;
+      // expect(stubHtmlLib.calledOnce).to.be.true;
+      sinon.assert.calledOnce(stubHtmlLib);
     });
     // Passes if stubLib spy was called with the right params
     it('should call lib with right params', () => {
