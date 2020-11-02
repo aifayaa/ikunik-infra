d84cd2c26a80aca54792ebf63a054b08e2ef8daa
Amelie Salaun <ameliesalaun@Mac-mini-de-Mac.local>
2020-11-02 09:03:31 +0400

    Tests with 2 errors

--- apps/test/handlers/getAppTos.test.js
+++ apps/test/handlers/getAppTos.test.js
@@ -2,9 +2,9 @@ import sinon from 'sinon';
 import { describe, it, before, after } from 'mocha';
 import { expect } from 'chai';
 
-import * as getHtmlResults from '../../../termsOfServices/htmlResults';
 import * as lib from '../../../termsOfServices/lib/getTos';
 import handler from '../../handlers/getAppTos';
+import * as getHtmlResults from '../../../termsOfServices/htmlResults';
 
 describe('handlers - getAppTos', () => {
   let stubLib;
@@ -14,25 +14,27 @@ describe('handlers - getAppTos', () => {
     pathParameters: {
       id: 'appId',
     },
+    headers: {
+      accept: 'accept' || 'Accept',
+    },
   };
 
   const sandbox = sinon.createSandbox();
 
-  describe('Lib return empty', () => {
+  describe('tos_not_found', () => {
     let response;
     before(async () => {
-      // Create a sandbox to remove track of every fake created
-      // Wrapping the existing function in a stub function & return empty array
-      stubLib = sandbox.stub(lib, 'getTos').returns([{}]);
-      // stubHtmlLib = sandbox.stub(getHtmlResults, 'getHtmlResults').returns([{}]);
+      //return 500 instead of 404
+      stubHtmlLib = sandbox.stub(getHtmlResults, 'getHtmlResults').throws(new Error('tos_not_found'));
+      console.log(stubHtmlLib);
+      stubLib = sandbox.stub(lib, 'getTos').returns([]);
       response = await handler(event);
+      console.log(response);
     });
-    // Passes if stubLib spy was called only once
     it('should call lib once', () => {
       expect(stubLib.calledOnce).to.be.true;
-      // expect(stubHtmlLib.calledOnce).to.be.true;
     });
-    // Passes if stubLib spy was called with the right params
+
     it('should call lib with right params', () => {
       const appId = event.pathParameters.id;
       sinon.assert.calledWith(
@@ -42,65 +44,62 @@ describe('handlers - getAppTos', () => {
         { outdated: false, required: true },
       );
     });
-    // expect returning error 404
-    it('should return a response with HTTP code 404', () => {
-      expect(response.statusCode).to.eql(404);
+
+    it('should return 404', () => {
+      expect(response.statusCode).to.equal(404);
+      expect(JSON.parse(response.body).message).to.eq('tos_not_found');
     });
-    // Clean the sandbox
+
     after(sandbox.restore);
   });
 
-  describe('Lib return results', () => {
-    let response;
+  // /////////////////// TEST OK ////////////////////////
+  // describe('Lib return results', () => {
+  //   let response;
 
-    before(async () => {
-      // Create a sandbox to remove track of every fake created
-      // Wrapping the existing function in a stub function & return parameters
-      stubLib = sandbox.stub(lib, 'getTos').returns([{
-        _id: 'crowdaa_app_id',
-      }]);
-      // stubHtmlLib = sandbox.stub(getHtmlResultsLib, 'getHtmlResults').returns();
-      // console.log(stubHtmlLib);
-      response = await handler(event);
-    });
-    it('should call lib once', () => {
-      // Passes if stubLib spy was called only once
-      expect(stubLib.calledOnce).to.be.true;
-      // expect(stubHtmlLib.calledOnce).to.be.true;
-    });
+  //   before(async () => {
+  //     stubLib = sandbox.stub(lib, 'getTos').returns([{
+  //       _id: 'crowdaa_app_id',
+  //     }]);
+  //     response = await handler(event);
+  //   });
 
-    it('should call lib with right params', () => {
-      const appId = event.pathParameters.id;
-      // Passes if stubLib spy was called with the right params
-      sinon.assert.calledWith(
-        stubLib,
-        appId,
-        false,
-        { outdated: false, required: true },
-      );
-    });
-    // expect returning error 200
-    it('should return a response with HTTP code 200', () => {
-      expect(response.statusCode).to.eql(200);
-    });
-    // Clean the sandbox
-    after(sandbox.restore);
-  });
+  //   it('should call lib once', () => {
+  //     expect(stubLib.calledOnce).to.be.true;
+  //   });
+
+  //   it('should call lib with right params', () => {
+  //     const appId = event.pathParameters.id;
+  //     sinon.assert.calledWith(
+  //       stubLib,
+  //       appId,
+  //       false,
+  //       { outdated: false, required: true },
+  //     );
+  //   });
+
+  //   it('should return a response with HTTP code 200', () => {
+  //     expect(response.statusCode).to.eql(200);
+  //   });
+  //   after(sandbox.restore);
+  // });
+  // /////////////////// TEST OK ////////////////////////
+
   describe('Lib trigger error', () => {
     let response;
     before(async () => {
-      // Create a promise reject message to fake error response
       stubLib = sandbox.stub(lib, 'getTos').callsFake(() => Promise.reject(new Error('error_message')));
-      stubHtmlLib = sandbox.stub(getHtmlResults, 'getHtmlResults').callsFake(() => Promise.reject(new Error('error_message')));
+      stubHtmlLib = sandbox.stub(getHtmlResults, 'getHtmlResults').throws(new Error('tos_not_found'));
       response = await handler(event);
+      console.log(response);
     });
-    // Passes if stubLib spy was called only once
+
     it('should call lib once', () => {
-      expect(stubLib.calledOnce).to.be.true;
-      // expect(stubHtmlLib.calledOnce).to.be.true;
+      // AssertError: expected getHtmlResults to be called once but was called 0 times
       sinon.assert.calledOnce(stubHtmlLib);
+      expect(stubLib.calledOnce).to.be.true;
     });
-    // Passes if stubLib spy was called with the right params
+
     it('should call lib with right params', () => {
       const appId = event.pathParameters.id;
       sinon.assert.calledWith(
@@ -110,9 +109,10 @@ describe('handlers - getAppTos', () => {
         { outdated: false, required: true },
       );
     });
-    // expect returning error 500
+
     it('should return a response with HTTP code 500', () => {
       expect(response.statusCode).to.eql(500);
+      // console.log(response.statusCode);
     });
     after(sandbox.restore);
   });
