f1b3c3546654d8d880d272ead9eb6c6b5a602aa4
Maxime Ferrino <maxime@crowdaa.com>
2020-11-05 15:23:22 +0400

    Fixed webpack file syntax

--- swagger/webpack.config.js
+++ swagger/webpack.config.js
@@ -16,36 +16,36 @@ module.exports = {
         test: /\.yaml$/,
         use: [
           { loader: 'json-loader' },
-          { loader: 'yaml-loader' }
-        ]
+          { loader: 'yaml-loader' },
+        ],
       },
       {
         test: /\.css$/,
         use: [
           { loader: 'style-loader' },
           { loader: 'css-loader' },
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin([
-      outputPath
+      outputPath,
     ]),
     new CopyWebpackPlugin([
       {
         // Copy the Swagger OAuth2 redirect file to the project root;
         // that file handles the OAuth2 redirect after authenticating the end-user.
         from: 'node_modules/swagger-ui/dist/oauth2-redirect.html',
-        to: './'
-      }
+        to: './',
+      },
     ]),
     new HtmlWebpackPlugin({
-      template: 'index.html'
-    })
+      template: 'index.html',
+    }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: outputPath,
-  }
+  },
 };
