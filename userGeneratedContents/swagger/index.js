c44e789022c6421a3088b25b65de700ba04595b3
Maxime Ferrino <maxime@crowdaa.com>
2020-11-09 17:12:10 +0400

    Added patchUserGeneratedContent API documentation

--- userGeneratedContents/swagger/index.js
+++ userGeneratedContents/swagger/index.js
@@ -2,6 +2,7 @@ import postUserGeneratedContents from './postUserGeneratedContents';
 import getAllUserGeneratedContents from './getAllUserGeneratedContents';
 import getUserGeneratedContents from './getUserGeneratedContents';
 import getChildrenUserGeneratedContents from './getChildrenUserGeneratedContents';
+import patchUserGeneratedContents from './patchUserGeneratedContents';
 
 import yaml from '../serverless.yml';
 
@@ -10,9 +11,9 @@ export default (libs, output) => {
   getAllUserGeneratedContents(libs, output);
   getUserGeneratedContents(libs, output);
   getChildrenUserGeneratedContents(libs, output);
+  patchUserGeneratedContents(libs, output);
 
   /**
-   * Missing : /press/articles/{id}/userGeneratedContents (Method get)
    * Missing : /userGeneratedContents/{id} (Method patch)
    * Missing : /userGeneratedContents/{id} (Method delete)
    * Missing : /userGeneratedContents/{id}/report (Method post)
