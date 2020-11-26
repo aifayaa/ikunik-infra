cd076885cb1fae5cbcf5514bcdf8cb000fbcc79a
Maxime Ferrino <maxime@crowdaa.com>
2020-11-26 13:31:54 +0400

    SLS: Fixed mutualization of /admin/press route

--- press/handler.js
+++ press/handler.js
@@ -3,3 +3,6 @@ import response from '../libs/httpResponses/response';
 export const handleGetPress = () => new Promise((resolve) => {
   resolve(response({ code: 200, body: 'API PRESS OK' }));
 });
+export const handleAdminGetPress = () => new Promise((resolve) => {
+  resolve(response({ code: 200, body: 'API ADMIN PRESS OK' }));
+});
