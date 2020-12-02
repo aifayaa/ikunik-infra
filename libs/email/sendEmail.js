6a37939ae1826e5f33823803b3d0da59856d81e5
Maxime Ferrino <maxime@crowdaa.com>
2020-12-02 14:11:09 +0400

    Removed unused sendEmail() function

--- libs/email/sendEmail.js
+++ libs/email/sendEmail.js
@@ -11,29 +11,6 @@ const {
 
 let transport = null;
 
-export const sendEmail = async (subject, html, to) => {
-  if (!transport) {
-    transport = nodemailer.createTransport({
-      host: SMTP_SERVER.split(':')[0],
-      port: SMTP_SERVER.split(':')[1] | 0,
-      secure: SMTP_SECURE === 'true',
-      auth: {
-        user: SMTP_LOGIN,
-        pass: SMTP_PASSWORD,
-      },
-    });
-  }
-
-  const response = await transport.sendMail({
-    from: SMTP_FROM,
-    to,
-    subject,
-    html,
-  });
-
-  return (response);
-};
-
 export const sendEmailTemplate = async (lang, template, to, subject, content) => {
   intlInit(lang);
 
