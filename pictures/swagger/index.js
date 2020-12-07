6c8e01868f9d7db051439ada2c8893035e370407
Maxime Ferrino <maxime@crowdaa.com>
2020-12-07 10:16:35 +0400

    Added video module. Added missing pictures api doc

--- pictures/swagger/index.js
+++ pictures/swagger/index.js
@@ -1,9 +1,11 @@
 import getPicture from './getPicture';
+import getPictureDataLocation from './getPictureDataLocation';
 
 import yaml from '../serverless.yml';
 
 export default (libs, output) => {
   getPicture(libs, output);
+  getPictureDataLocation(libs, output);
 
   libs.checks.forMissingAPIs(yaml, output);
 };
