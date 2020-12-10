d740535c57c82bf936b49cdf3b5d9cfde3039b13
Olivier Leclercq <olivier@crowdaa.com>
2020-12-10 14:02:51 +0400

    Added sorting on getApps request + some code cleaning & refacto

--- users/handlers/getApps.js
+++ users/handlers/getApps.js
@@ -1,16 +1,45 @@
+import { typeCheck } from 'type-check';
+import errorMessage from '../../libs/httpResponses/errorMessage';
 import getApps from '../lib/getApps';
 import response from '../../libs/httpResponses/response';
 
 export default async (event) => {
-  const userId = event.requestContext.authorizer.principalId;
-  const urlId = event.pathParameters.id;
-
-  if (userId !== urlId) {
-    return response({ code: 403, message: 'Forbidden' });
-  }
+  const { principalId: userId } = event.requestContext.authorizer;
+  const { id: urlId } = event.pathParameters;
+  const { sortBy, sortOrder } = event.queryStringParameters || {};
 
   try {
-    const appsResults = await getApps(userId);
+    if (userId !== urlId) {
+      throw new Error('forbidden');
+    }
+
+    if (sortBy) {
+      if (!typeCheck('{ sortBy: String }', { sortBy })) {
+        throw new Error('wrong_argument_type');
+      }
+
+      /* Using array here in case we add other sort values later */
+      if (['name'].indexOf(sortBy) < 0) {
+        throw new Error('wrong_argument_value');
+      }
+    }
+
+    if (sortOrder) {
+      if (!typeCheck('{ sortOrder: String }', { sortOrder })) {
+        throw new Error('wrong_argument_type');
+      }
+
+      if (['asc', 'desc'].indexOf(sortOrder) < 0) {
+        throw new Error('wrong_argument_value');
+      }
+    }
+
+    if ((sortBy && !sortOrder) || (!sortBy && sortOrder)) {
+      throw new Error('missing_argument');
+    }
+
+    const appsResults = await getApps(userId, { sortBy, sortOrder });
+
     if (appsResults.length === 0) {
       return response({ code: 404, message: 'crowdaa_no_apps_permissions' });
     }
@@ -20,6 +49,6 @@ export default async (event) => {
       body: { items: appsResults, totalCount: appsResults.length },
     });
   } catch (e) {
-    return response({ code: 500, message: e.message });
+    return response(errorMessage(e));
   }
 };
