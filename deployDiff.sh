96b482e52081c7e677566fb86447e822c4c7f8bc
Maxime Ferrino <maxime@crowdaa.com>
2020-12-15 15:05:28 +0400

    Added full deploy feature on CI

--- deployDiff.sh
+++ deployDiff.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
 folders="folderList"
+touch fullDeploy
+doFullDeploy=$(cat fullDeploy)
 
-for folder in $(<$folders)
-do
-   echo "___________Deploying $folder ___________"
-   if [ "$folder" != "libs" ]; then
-         cd "$folder"
+set -e
+
+doDeploy() {
+  for folder in $(<$folders)
+  do
+    echo "___________Deploying $folder ___________"
+    if [ "$folder" != "libs" ]; then
+      cd "$folder"
       if [ "$folder" == "files" ]; then
-         REGION=$2 npm run "deploy:$1"
+        REGION=$2 npm run "deploy:$1"
       else
-         npm i
-         npx --node-arg=--max-old-space-size=2000 serverless deploy --stage "$1" --region "$2"
+        npm i
+        npx --node-arg=--max-old-space-size=2000 serverless deploy --stage "$1" --region "$2"
       fi
       cd ..
-   fi
-done
+    fi
+  done
+}
+
+if [ "$doFullDeploy" = 'true' ]; then
+  cp folderList{,.bak}
+  cp api-vi/serverless.yml{,.bak}
+
+  cp deployOrderList folderList
+  sed -i -e '/^# remove on first deploy --- START$/,/^# remove  when first deploy --- END$/d' api-v1/serverless.yml
+
+  doDeploy
+
+  mv folderList{.bak,}
+  mv api-vi/serverless.yml{.bak,}
+fi
+
+doDeploy
