f53992837eaa8a6db613723cb5a336e9cdbd0ae2
Maxime Ferrino <maxime@crowdaa.com>
2020-12-16 11:58:26 +0000

    Moved CI_FIRST_DEPLOY variable Added back missing dependancies in tickets

--- deployDiff.sh
+++ deployDiff.sh
@@ -3,43 +3,61 @@
 export STAGE="$1"
 export REGION="$2"
 
+export BACKUP_EXTENSION='.bak'
+
 folders="folderList"
-touch fullDeploy
-doFullDeploy=$(cat fullDeploy)
+doFullDeploy="$CI_FIRST_DEPLOY"
 
 set -e
 
+doServerless() {
+  command="$1"
+  npx --node-arg=--max-old-space-size=2000 serverless "$command" --stage "$STAGE" --region "$REGION"
+}
+
+doCreateDomain() {
+  doServerless delete_domain
+  doServerless create_domain
+}
+
 doDeploy() {
+  fullDeploy="$1"
   for folder in $(<$folders)
   do
     echo "___________Deploying $folder ___________"
     if [ "$folder" != "libs" ]; then
       cd "$folder"
-      if [ "$folder" == "files" ]; then
-        npm run "deploy:$STAGE"
-      else
-        npm i
-        npx --node-arg=--max-old-space-size=2000 serverless deploy --stage "$STAGE" --region "$REGION"
-      fi
+      npm i
+      case "$folder" in
+        files) npm run "deploy:$STAGE";;
+        api-v1|ssr)
+          if [ "$fullDeploy" = 'full' ]; then doCreateDomain; fi;
+          doServerless deploy;;
+        *) doServerless deploy;;
+      esac
       cd ..
     fi
   done
 }
 
 if [ "$doFullDeploy" = 'true' ]; then
-  cp folderList{,.bak}
-  cp api-v1/serverless.yml{,.bak}
+  cp folderList{,"$BACKUP_EXTENSION"}
+  cp api-v1/serverless.yml{,"$BACKUP_EXTENSION"}
 
   cp deployOrderList folderList
-  sed -i -e '/^# remove on first deploy --- START$/,/^# remove  when first deploy --- END$/d' api-v1/serverless.yml
+  sedcmds='/^# remove on first deploy --- START1$/,/^# remove  when first deploy --- END1$/d'
+  sedcmds="$sedcmds;"'/^## uncomment on first deploy --- START1$/,/^## uncomment  when first deploy --- END1$/s/^#//'
+  sed -i -e "$sedcmds" api-v1/serverless.yml
 
-  doDeploy
+  doDeploy full
 
-  mv folderList{.bak,}
-  mv api-v1/serverless.yml{.bak,}
+  mv folderList{"$BACKUP_EXTENSION",}
+  mv api-v1/serverless.yml{"$BACKUP_EXTENSION",}
 
   cd api-v1
-  npx --node-arg=--max-old-space-size=2000 serverless deploy --stage "$STAGE" --region "$REGION"
+  echo "___________Re-Deploying api-v1 ___________"
+  npm i
+  doServerless deploy
   cd ..
 else 
   doDeploy
