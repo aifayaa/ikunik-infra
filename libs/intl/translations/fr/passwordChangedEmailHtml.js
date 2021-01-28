461ad067969d315020163b03b755614b3a47d1ff
Maxime Ferrino <maxime@crowdaa.com>
2021-01-28 10:44:45 +0400

    Added missing comma in translation

--- libs/intl/translations/fr/passwordChangedEmailHtml.js
+++ libs/intl/translations/fr/passwordChangedEmailHtml.js
@@ -2,7 +2,7 @@ export default `
 <p>
   Bonjour <strong>{{username}}</strong>,<br>
   <br>
-  Le mot de passe de votre compte Crowdaa associé à l'adresse email <strong>{{email}}</strong>, a été mis à jour avec succès.<br>
+  Le mot de passe de votre compte Crowdaa, associé à l'adresse email <strong>{{email}}</strong>, a été mis à jour avec succès.<br>
   Vous pouvez maintenant vous connecter avec votre nouveau mot de passe.
 </p>
 `;
