6ea94237345049f1f12a11752a269113f68b765c
Maxime Ferrino <maxime@crowdaa.com>
2021-10-05 15:36:16 +0400

    Added codePreview with admin login feature

---
+++ users/handlers/getAppsPreviews.js
@@ -0,0 +1,53 @@
+import { typeCheck } from 'type-check';
+import errorMessage from '../../libs/httpResponses/errorMessage';
+import getAppsPreviews from '../lib/getAppsPreviews';
+import response from '../../libs/httpResponses/response';
+
+export default async (event) => {
+  const { principalId: userId } = event.requestContext.authorizer;
+  const { id: urlId } = event.pathParameters;
+  const {
+    sortBy = 'name',
+    sortOrder = 'desc',
+  } = event.queryStringParameters || {};
+
+  try {
+    if (userId !== urlId) {
+      throw new Error('forbidden');
+    }
+
+    if (sortBy) {
+      if (!typeCheck('{ sortBy: String }', { sortBy })) {
+        throw new Error('wrong_argument_type');
+      }
+
+      /* Using array here in case we add other sort values later */
+      if (['name'].indexOf(sortBy) < 0) {
+        throw new Error('wrong_argument_value');
+      }
+    }
+
+    if (sortOrder) {
+      if (!typeCheck('{ sortOrder: String }', { sortOrder })) {
+        throw new Error('wrong_argument_type');
+      }
+
+      if (['asc', 'desc'].indexOf(sortOrder) < 0) {
+        throw new Error('wrong_argument_value');
+      }
+    }
+
+    const appsResults = await getAppsPreviews(userId, { sortBy, sortOrder });
+
+    if (appsResults.length === 0) {
+      return response({ code: 404, message: 'crowdaa_no_apps_permissions' });
+    }
+
+    return response({
+      code: 200,
+      body: { items: appsResults, totalCount: appsResults.length },
+    });
+  } catch (e) {
+    return response(errorMessage(e));
+  }
+};
