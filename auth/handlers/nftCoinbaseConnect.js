97d73d98b96bdaa024b2824be4224d4224044a94
Maxime Ferrino <maxime@crowdaa.com>
2022-03-01 09:25:15 +0400

    Added NFT auth for mobile app

---
+++ auth/handlers/nftCoinbaseConnect.js
@@ -0,0 +1,30 @@
+import response from '../../libs/httpResponses/response';
+import { nftCoinbaseConnect } from '../lib/nftCoinbaseConnect';
+import errorMessage from '../../libs/httpResponses/errorMessage';
+
+export default async (event) => {
+  try {
+    if (!event.body) {
+      throw new Error('missing_payload');
+    }
+
+    const { principalId: userId, appId } = event.requestContext.authorizer;
+    const {
+      code,
+    } = JSON.parse(event.body);
+
+    [code, appId, userId].forEach((item) => {
+      if (!item) throw new Error('missing_argument');
+      if (item && typeof item !== 'string') {
+        throw new Error('wrong_argument_type');
+      }
+    });
+
+    const result = await nftCoinbaseConnect(userId, code, appId);
+
+    /* get User in db or create new one if not exists */
+    return response({ code: 200, body: result });
+  } catch (e) {
+    return response(errorMessage(e));
+  }
+};
