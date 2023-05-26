b6b881867eb43a59b47b1bd556c5d315f896ba42
Maxime Ferrino <maxime@crowdaa.com>
2023-05-26 10:27:30 +0400

    Added Vivatech event form API

---
+++ forms/handlers/postFormRegisterVivatech.js
@@ -0,0 +1,22 @@
+import postFormRegisterVivatech from '../lib/postFormRegisterVivatech';
+import errorMessage from '../../libs/httpResponses/errorMessage';
+import response from '../../libs/httpResponses/response';
+
+export default async (event) => {
+  try {
+    if (!event.body) {
+      throw new Error('mal_formed_request');
+    }
+
+    const bodyParsed = JSON.parse(event.body);
+
+    if (typeof bodyParsed !== 'object') {
+      throw new Error('wrong_argument_type');
+    }
+
+    const inserted = await postFormRegisterVivatech(bodyParsed);
+    return response({ code: 200, body: inserted });
+  } catch (e) {
+    return response(errorMessage({ message: e.message }));
+  }
+};
