08597e8a7ba9da38a731115a6d7c5803de2496a4
Maxime Ferrino <maxime@crowdaa.com>
2023-06-13 16:55:57 +0400

    Added pressBanners

---
+++ pressBanners/lib/getBanners.js
@@ -0,0 +1,24 @@
+import MongoClient from '../../libs/mongoClient';
+import mongoCollections from '../../libs/mongoCollections.json';
+
+const {
+  COLL_PRESS_BANNERS,
+} = mongoCollections;
+
+export default async function getBanners(appId, tab = null) {
+  const client = await MongoClient.connect();
+
+  try {
+    const query = { appId };
+    if (tab) query.tab = tab;
+    const banners = await client
+      .db()
+      .collection(COLL_PRESS_BANNERS)
+      .find(query)
+      .toArray();
+
+    return (banners);
+  } finally {
+    client.close();
+  }
+}
