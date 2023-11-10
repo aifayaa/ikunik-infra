130733790c973b8d743afdff17f5c42479c5dd6a
Maxime Ferrino <maxime@crowdaa.com>
2023-11-10 17:01:42 +0400

    Added ghanty personalData APIs (not tested)

---
+++ providers/handlers/ghanty/getPersonalData.js
@@ -0,0 +1,18 @@
+import getPersonalData from '../../lib/ghanty/getPersonalData';
+import errorMessage from '../../../libs/httpResponses/errorMessage';
+import response from '../../../libs/httpResponses/response';
+
+export default async (event) => {
+  const { appId, principalId: userId } = event.requestContext.authorizer;
+
+  try {
+    if (!userId) {
+      throw new Error('forbidden');
+    }
+
+    const body = await getPersonalData(appId, userId);
+    return response({ code: 200, body });
+  } catch (e) {
+    return response(errorMessage({ message: e.message }));
+  }
+};
---
+++ providers/lib/ghanty/getPersonalData.js
@@ -0,0 +1,33 @@
+import MongoClient from '../../../libs/mongoClient';
+import mongoCollections from '../../../libs/mongoCollections.json';
+import { MyFidApi } from '../../../libs/backends/ghanty-myfid';
+
+const {
+  COLL_APPS,
+  COLL_USERS,
+} = mongoCollections;
+
+export default async (
+  appId,
+  userId,
+) => {
+  const client = await MongoClient.connect();
+  try {
+    const app = await client.db().collection(COLL_APPS).findOne({ _id: appId });
+    const user = await client.db().collection(COLL_USERS).findOne({ _id: userId });
+    if (!app) {
+      throw new Error('app_not_found');
+    }
+    if (!user) {
+      throw new Error('user_not_found');
+    }
+    const fidApi = new MyFidApi(app);
+    await fidApi.renewTokenIfNeeded(client);
+
+    const response = await fidApi.call(`/users/${user.username}/personnaldata`);
+
+    return (response);
+  } finally {
+    client.close();
+  }
+};
