66a94c56bc29366a4fb3c85c104c04af1977dad7
Maxime Ferrino <maxime@crowdaa.com>
2023-11-20 11:25:04 +0400

    WIP

--- userGeneratedContents/lib/getChildrenUserGeneratedContents.js
+++ userGeneratedContents/lib/getChildrenUserGeneratedContents.js
@@ -7,91 +7,112 @@ const {
   COLL_USER_GENERATED_CONTENTS,
 } = mongoCollections;
 
-export default async (
+function buildPipeline({
   appId,
-  rootParentId,
-  rootParentCollection,
+  parentId,
+  parentCollection,
+  fetchAll,
   start,
   limit,
+}) {
+  const moderatedContentFilter = {
+    $or: [
+      /* pre moderation case */
+      { moderated: false, reviewed: true },
+      /* post moderation cases */
+      { moderated: { $exists: false }, reviewed: { $exists: false } },
+    ],
+  };
+
+  const pipeline = [
+    {
+      $match: {
+        parentId,
+        parentCollection,
+        trashed: false,
+        appId,
+        ...(fetchAll ? {} : moderatedContentFilter),
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+  ];
+
+  if (start) {
+    pipeline.push({
+      $skip: Number(start),
+    });
+  }
+
+  if (limit) {
+    pipeline.push({
+      $limit: Number(limit),
+    });
+  }
+
+  pipeline.push({
+    $lookup: {
+      from: COLL_USERS,
+      localField: 'userId',
+      foreignField: '_id',
+      as: 'user',
+    },
+  }, {
+    $unwind: {
+      path: '$user',
+      preserveNullAndEmptyArrays: true,
+    },
+  }, {
+    $project: {
+      data: 1,
+      parentCollection: 1,
+      parentId: 1,
+      rootParentCollection: 1,
+      rootParentId: 1,
+      type: 1,
+      user: {
+        firstname: 1,
+        isUserPicture: 1,
+        lastname: 1,
+        profile: {
+          avatar: 1,
+          isUserPicture: 1,
+          userPictureData: 1,
+          username: 1,
+        },
+        status: 1,
+        username: 1,
+        _id: 1,
+      },
+    },
+  });
+
+  return (pipeline);
+}
+
+export default async (
+  appId,
+  parentId,
+  parentCollection,
+  start,
+  limit,
+  children = false,
   fetchAll = false,
 ) => {
   let client;
   try {
     client = await MongoClient.connect();
 
-    const moderatedContentFilter = {
-      $or: [
-        /* pre moderation case */
-        { moderated: false, reviewed: true },
-        /* post moderation cases */
-        { moderated: { $exists: false }, reviewed: { $exists: false } },
-      ],
-    };
-
-    const pipeline = [
-      {
-        $match: {
-          rootParentId,
-          rootParentCollection,
-          trashed: false,
-          appId,
-          ...(fetchAll ? {} : moderatedContentFilter),
-        },
-      },
-      {
-        $sort: {
-          createdAt: -1,
-        },
-      },
-    ];
-
-    if (start) {
-      pipeline.push({
-        $skip: Number(start),
-      });
-    }
-
-    if (limit) {
-      pipeline.push({
-        $limit: Number(limit),
-      });
-    }
-
-    pipeline.push({
-      $lookup: {
-        from: COLL_USERS,
-        localField: 'userId',
-        foreignField: '_id',
-        as: 'user',
-      },
-    }, {
-      $unwind: {
-        path: '$user',
-        preserveNullAndEmptyArrays: true,
-      },
-    }, {
-      $project: {
-        data: 1,
-        parentCollection: 1,
-        parentId: 1,
-        rootParentCollection: 1,
-        rootParentId: 1,
-        type: 1,
-        user: {
-          firstname: 1,
-          isUserPicture: 1,
-          lastname: 1,
-          profile: {
-            avatar: 1,
-            isUserPicture: 1,
-            userPictureData: 1,
-            username: 1,
-          },
-          status: 1,
-          username: 1,
-          _id: 1,
-        },
-      },
+    const pipeline = buildPipeline({
+      appId,
+      parentId,
+      parentCollection,
+      fetchAll,
+      start,
+      limit,
     });
 
     const ugcs = await client
@@ -101,8 +122,23 @@ export default async (
       .toArray();
 
     const promises = ugcs.map(async (ugc) => {
-      if (ugc && ugc.type === 'article') {
+      if ((ugc && ugc.type === 'article') || children) {
         ugc.commentsCount = await getUGCArticleCommentsCount(appId, ugc._id);
+      }
+      if (children && ugc.commentsCount > 0) {
+        const pipeline2 = buildPipeline({
+          appId,
+          parentId: ugc._id,
+          parentCollection: COLL_USER_GENERATED_CONTENTS,
+          fetchAll,
+          start: 0,
+          limit: 3,
+        });
+        ugc.comments = await client
+          .db()
+          .collection(COLL_USER_GENERATED_CONTENTS)
+          .aggregate(pipeline2)
+          .toArray();
       }
     });
 
