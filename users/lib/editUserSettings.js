d80316da6d3935a6e82b704a801f1462ee3af7e9
Maxime Ferrino <maxime@crowdaa.com>
2023-11-23 11:32:40 +0400

    Added user settings read/write APIs

---
+++ users/lib/editUserSettings.js
@@ -0,0 +1,45 @@
+import MongoClient from '../../libs/mongoClient';
+import mongoCollections from '../../libs/mongoCollections.json';
+
+const {
+  COLL_USERS,
+} = mongoCollections;
+
+function isBool(x) {
+  return (typeof x === 'boolean');
+}
+
+export const allowedSettingsChecks = {
+  'notifications.ugc_comment_replies': isBool,
+  'notifications.ugc_post_replies': isBool,
+};
+
+export default async (userId, appId, newSettings) => {
+  const client = await MongoClient.connect();
+  const db = client.db();
+
+  try {
+    const user = await db.collection(COLL_USERS).findOne({ _id: userId, appId });
+    if (!user) {
+      throw new Error('user_not_found');
+    }
+
+    const $set = Object.keys(newSettings).reduce((acc, key) => {
+      acc[`settings.${key}`] = newSettings[key];
+      return (acc);
+    }, {});
+
+    const { matchedCount } = await db
+      .collection(COLL_USERS)
+      .updateOne({
+        _id: userId,
+        appId,
+      }, {
+        $set,
+      });
+
+    return !!matchedCount;
+  } finally {
+    client.close();
+  }
+};
