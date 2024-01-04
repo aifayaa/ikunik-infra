4cbc916c3cb6abf658a777c6241b88483dbd8e22
Maxime Ferrino <maxime@crowdaa.com>
2024-01-04 15:56:46 +0400

    Now able to delete users from the dashboard

--- users/handlers/deleteUser.js
+++ users/handlers/deleteUser.js
@@ -1,14 +1,17 @@
 import deleteUser from '../lib/deleteUser';
 import response from '../../libs/httpResponses/response';
+import { checkPerms } from '../../libs/perms/checkPerms';
 
+const allowedPerms = ['pressArticles_all'];
 export default async (event) => {
   const userId = event.requestContext.authorizer.principalId;
   const { appId } = event.requestContext.authorizer;
   const urlId = event.pathParameters.id;
+  const perms = JSON.parse(event.requestContext.authorizer.perms);
 
   try {
-    if (userId !== urlId) {
-      return response({ code: 403, message: 'Forbidden' });
+    if (userId !== urlId && !checkPerms(allowedPerms, perms)) {
+      return response({ code: 403, message: 'access_forbidden' });
     }
 
     const deleteRefs = await deleteUser(userId, appId);
