e0ddcedb68cc20e2410ca3cbc2f0e43139c03a85
Maxime Ferrino <maxime@crowdaa.com>
2024-02-29 17:04:11 +0400

    Eslint fix

--- perms/handlers/checkPerms.js
+++ perms/handlers/checkPerms.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-relative-packages
 import { checkPerms } from '../../libs/perms/checkPerms';
 
 export default ({ required, perms }) => checkPerms(required, perms);
