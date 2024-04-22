92153762a36cae996202d71d2e05d2dab87eda16
frozar <fabien.rozar@gmail.com>
2024-04-22 15:12:23 +0400

    Create delete route for organizations

---
+++ organizations/handlers/deleteOrg.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-relative-packages */
+import errorMessage from '../../libs/httpResponses/errorMessage';
+import response from '../../libs/httpResponses/response';
+import { checkPermsForOrganization } from '../../libs/perms/checkPermsFor';
+import deleteOrg from '../lib/deleteOrg';
+
+export default async (event) => {
+  const { principalId: userId } = event.requestContext.authorizer;
+  const orgId = event.pathParameters.id;
+
+  try {
+    const allowed = await checkPermsForOrganization(userId, orgId, 'owner');
+    if (!allowed) {
+      throw new Error('access_forbidden');
+    }
+
+    await deleteOrg(userId, orgId);
+    return response({ code: 200, body: `Delete of org '${orgId}'` });
+  } catch (e) {
+    return response(errorMessage({ message: e.message }));
+  }
+};
