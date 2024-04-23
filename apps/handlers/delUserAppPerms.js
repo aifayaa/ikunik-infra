f4e9bf8ede0d60248219928ba82b35f1a7a530f5
NicolasC <nicolas.courtonne@crowdaa.com>
2024-04-23 17:28:05 +0200

    feat(delUserAppPerms): DELETE /apps/{id}/users/{userId}

--- apps/handlers/delUserAppPerms.js
+++ apps/handlers/delUserAppPerms.js
@@ -1,12 +1,22 @@
 /* eslint-disable import/no-relative-packages */
 import errorMessage from '../../libs/httpResponses/errorMessage';
 import response from '../../libs/httpResponses/response';
+import { checkPermsForOrganization } from '../../libs/perms/checkPermsFor';
+import delUserAppPerms from '../lib/delUserAppPerms';
 
-export default async () => {
+export default async (event) => {
+  const { principalId: userId } = event.requestContext.authorizer;
+  const { id: appId, userId: targetId } = event.pathParameters;
   try {
-    const res = { delUserAppPerms: true };
+    if (!userId) throw new Error('user_not_found');
+    if (!targetId) throw new Error('target_user_not_found');
+    if (!appId) throw new Error('org_not_found');
+    const allowed = await checkPermsForOrganization(userId, appId, 'admin');
+    if (!allowed) throw new Error('access_forbidden');
 
-    return await response({ code: 200, body: res });
+    const res = await delUserAppPerms(targetId, appId);
+
+    return response({ code: 200, body: res });
   } catch (e) {
     return response(errorMessage({ message: e.message }));
   }
---
+++ apps/lib/delUserAppPerms.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-relative-packages */
+import MongoClient from '../../libs/mongoClient';
+import mongoCollections from '../../libs/mongoCollections.json';
+
+const { COLL_USERS } = mongoCollections;
+
+export default async (userId, appId) => {
+  const client = await MongoClient.connect();
+
+  try {
+    const result = await client
+      .db()
+      .collection(COLL_USERS)
+      .updateOne({ _id: userId }, { $pull: { 'perms.apps': { _id: appId } } });
+
+    if (result.modifiedCount === 0) {
+      throw new Error('org_not_found');
+    }
+
+    return { userUpdated: true };
+  } finally {
+    client.close();
+  }
+};
