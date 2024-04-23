9ba69d4d22bdedf3eff17da208719dde62f0ddbd
frozar <fabien.rozar@gmail.com>
2024-04-23 10:23:42 +0400

    More verbose script sls-offline

--- sls-offline.sh
+++ sls-offline.sh
@@ -23,6 +23,10 @@ if [ -z "$STAGE" ] || [ -z "$REGION" ] || [ -z "$USERID" ]; then
   exit 1
 fi
 
+echo "STAGE : ${STAGE}"
+echo "REGION : ${REGION}"
+echo "USERID : ${USERID}"
+
 AUTHORIZER='{"appId": "ca38e8a8-445a-44bf-b60e-c67fc440c65d", "principalId": "'${USERID}'", "perms": {"apps_getInfos":true,"apps_getProfile":true,"crowd_blast":true,"files_upload":true,"pressArticles_all":true,"pressCategories_all":true,"search_press":true,"userGeneratedContents_all":true,"userGeneratedContents_notify":true}, "integrationLatency": "43"}' npx sls offline --reloadHandler --stage=${STAGE} --region=${REGION}
 
 
