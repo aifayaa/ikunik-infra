429e5498bcd8e3a971caec4f6b84deecb8d6b059
frozar <fabien.rozar@gmail.com>
2024-04-23 17:02:20 +0400

    Add end-point GET apps/{id}/users

--- apps/handlers/getAppUsers.js
+++ apps/handlers/getAppUsers.js
@@ -1,12 +1,18 @@
 /* eslint-disable import/no-relative-packages */
 import errorMessage from '../../libs/httpResponses/errorMessage';
 import response from '../../libs/httpResponses/response';
+import getAppUsers from '../lib/getAppUsers';
 
-export default async () => {
+export default async (event) => {
+  const { principalId: userId } = event.requestContext.authorizer;
+  const appId = event.pathParameters.id;
+
   try {
-    const res = { getAppUsers: true };
+    // TODO: check if userId has access to appId before anything else
 
-    return await response({ code: 200, body: res });
+    const users = await getAppUsers(appId);
+
+    return response({ code: 200, body: users });
   } catch (e) {
     return response(errorMessage({ message: e.message }));
   }
