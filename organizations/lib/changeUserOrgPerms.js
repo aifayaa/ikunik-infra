ae884b2a3bc42342c1d64d8c5af1b762845c90f2
NicolasC <nicolas.courtonne@crowdaa.com>
2024-04-26 15:42:28 +0200

    feat(changeUserOrgPerms): PATCH /organizations/{id}/user/{userId}

---
+++ organizations/lib/changeUserOrgPerms.js
@@ -0,0 +1,51 @@
+/* eslint-disable import/no-relative-packages */
+import MongoClient from '../../libs/mongoClient';
+import mongoCollections from '../../libs/mongoCollections.json';
+
+const { COLL_USERS } = mongoCollections;
+
+export default async (userId, orgId, data) => {
+  const client = await MongoClient.connect();
+
+  try {
+    const authPerms = ['owner', 'admin', 'editor', 'moderator', 'viewer'];
+
+    if (!authPerms.includes(data.newPerm)) {
+      return { userPermUpdated: false };
+    }
+
+    const db = client.db();
+
+    const user = await db.collection(COLL_USERS).findOne({ _id: userId });
+
+    if (!user) {
+      return { userPermUpdated: false };
+    }
+
+    if (user.perms && user.perms.orgs) {
+      const org = await db
+        .collection(COLL_USERS)
+        .findOne({ _id: userId, 'perms.orgs._id': orgId });
+
+      if (!org) {
+        return { userPermUpdated: false };
+      }
+
+      const result = await db
+        .collection(COLL_USERS)
+        .updateOne(
+          { _id: userId, 'perms.orgs._id': orgId },
+          { $set: { 'perms.orgs.$.roles': data.newPerm } }
+        );
+
+      if (result.modifiedCount === 0) {
+        return { userPermUpdated: false };
+      }
+
+      return { userPermUpdated: true };
+    }
+    return { userPermUpdated: false };
+  } finally {
+    client.close();
+  }
+};
