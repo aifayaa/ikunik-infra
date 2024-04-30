73fa3aac3c9992347cfbdefcf9fd026e5ec0b9d9
frozar <fabien.rozar@gmail.com>
2024-04-30 16:40:13 +0400

    Clean the sanity check of delete organization end-point

--- organizations/handlers/deleteOrg.js
+++ organizations/handlers/deleteOrg.js
@@ -6,18 +6,13 @@ import deleteOrg from '../lib/deleteOrg';
 
 export default async (event) => {
   const { principalId: userId } = event.requestContext.authorizer;
-
-  if (!userId) {
-    throw new Error('user_not_found');
-  }
-
   const orgId = event.pathParameters.id;
 
-  if (!orgId) {
-    throw new Error('org_not_found');
-  }
-
   try {
+    if (!userId) {
+      throw new Error('user_not_found');
+    }
+
     const allowed = await checkPermsForOrganization(userId, orgId, 'owner');
     if (!allowed) {
       throw new Error('access_forbidden');
