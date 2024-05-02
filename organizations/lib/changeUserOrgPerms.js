96743572e31df92c01d6c07b757deebb882067d8
frozar <fabien.rozar@gmail.com>
2024-05-02 10:36:07 +0400

    Protect the special case of the organization owner

--- organizations/lib/changeUserOrgPerms.js
+++ organizations/lib/changeUserOrgPerms.js
@@ -10,20 +10,40 @@ export default async (userId, orgId, data) => {
   try {
     const db = client.db();
 
-    const result = await db
+    const user = await db.collection(COLL_USERS).findOne({ _id: userId });
+
+    // If the target user is a superAdmin, don't change permissions
+    const { superAdmin } = user;
+    if (superAdmin === true) {
+      return user;
+    }
+
+    // If the target user is the owner of the group, don't change permissions
+    const userOrganizationPerms = user.perms.organizations.find(
+      (org) => org._id === orgId
+    );
+
+    if (userOrganizationPerms) {
+      const userRoles = userOrganizationPerms.roles;
+
+      if (userRoles.includes('owner')) {
+        return user;
+      }
+    }
+
+    const updatedRoles = data.roles;
+    const commandRes = await db
       .collection(COLL_USERS)
       .updateOne(
         { _id: userId, 'perms.organizations._id': orgId },
-        { $set: { 'perms.organizations.$.roles': data.roles } }
+        { $set: { 'perms.organizations.$.roles': updatedRoles } }
       );
 
-    if (result.modifiedCount === 0) {
+    if (commandRes && commandRes.nModified && commandRes.nModified.ok !== 1) {
       throw new Error('update_failed');
     }
 
-    return await db
-      .collection(COLL_USERS)
-      .findOne({ _id: userId, 'perms.organizations._id': orgId });
+    return await db.collection(COLL_USERS).findOne({ _id: userId });
   } finally {
     client.close();
   }
