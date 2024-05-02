58c026bc6f4566197681b4144afa0a65116ac952
frozar <fabien.rozar@gmail.com>
2024-05-02 13:29:55 +0400

    Implement GET /organizations/{id}/users

--- organizations/handlers/getOrgUsers.js
+++ organizations/handlers/getOrgUsers.js
@@ -1,12 +1,20 @@
 /* eslint-disable import/no-relative-packages */
 import errorMessage from '../../libs/httpResponses/errorMessage';
 import response from '../../libs/httpResponses/response';
+import { checkPermsForOrganization } from '../../libs/perms/checkPermsFor';
+import getUserOrgPerms from '../lib/getUserOrgPerms';
 
-export default async () => {
+export default async (event) => {
+  const { principalId: userId } = event.requestContext.authorizer;
+  const { id: orgId } = event.pathParameters;
+
   try {
-    const res = { getOrgUsers: true };
+    const allowed = await checkPermsForOrganization(userId, orgId, 'member');
+    if (!allowed) throw new Error('access_forbidden');
 
-    return await response({ code: 200, body: res });
+    const res = await getUserOrgPerms(orgId);
+
+    return response({ code: 200, body: res });
   } catch (e) {
     return response(errorMessage({ message: e.message }));
   }
---
+++ organizations/lib/getUserOrgPerms.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-relative-packages */
+import MongoClient from '../../libs/mongoClient';
+import mongoCollections from '../../libs/mongoCollections.json';
+
+const { COLL_USERS } = mongoCollections;
+
+export default async (orgId) => {
+  const client = await MongoClient.connect();
+
+  try {
+    const db = client.db();
+
+    return await db
+      .collection(COLL_USERS)
+      .find({ 'perms.organizations._id': orgId })
+      .toArray();
+  } finally {
+    client.close();
+  }
+};
