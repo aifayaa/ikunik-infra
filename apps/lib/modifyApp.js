e1a4d2b8450543065a50356340f3326b4b6758c9
Maxime Ferrino <maxime@crowdaa.com>
2024-05-06 23:42:38 +0400

    fix(modifyApp): added missing fields to be modified.

--- apps/lib/modifyApp.js
+++ apps/lib/modifyApp.js
@@ -7,7 +7,7 @@ import {
 } from '../../libs/httpResponses/errorCodes';
 import MongoClient from '../../libs/mongoClient';
 import mongoCollections from '../../libs/mongoCollections.json';
-import { objGet } from '../../libs/utils';
+import { objGet, objSet } from '../../libs/utils';
 import { getAppDefaultBuildFields, getAppLockedFields } from './appsUtils';
 
 const { COLL_APPS } = mongoCollections;
@@ -24,13 +24,39 @@ export default async (appId, update) => {
     if (update.name) {
       $set.name = update.name;
     }
-    if (update.androidName) {
+
+    if (
+      update.iosDescription ||
+      update.iosAuthor ||
+      update.iosEmail ||
+      update.iosName
+    ) {
+      if (!objGet(app, ['builds', 'ios'])) {
+        const fields = getAppDefaultBuildFields(app.name, 'ios');
+        Object.keys(fields).forEach((field) => {
+          $set[`builds.ios.${field}`] = fields[field];
+          objSet(app, ['builds', 'ios', field], fields[field]);
+        });
+      }
+    }
+
+    if (
+      update.androidDescription ||
+      update.androidAuthor ||
+      update.androidEmail ||
+      update.androidName
+    ) {
       if (!objGet(app, ['builds', 'android'])) {
-        $set['builds.android'] = getAppDefaultBuildFields(
-          update.androidName,
-          'android'
-        );
-      } else if (!lockedFields.androidName) {
+        const fields = getAppDefaultBuildFields(app.name, 'android');
+        Object.keys(fields).forEach((field) => {
+          $set[`builds.android.${field}`] = fields[field];
+          objSet(app, ['builds', 'android', field], fields[field]);
+        });
+      }
+    }
+
+    if (update.androidName) {
+      if (!lockedFields.androidName) {
         $set['builds.android.name'] = update.androidName;
       } else {
         throw new CrowdaaException(
@@ -41,9 +67,7 @@ export default async (appId, update) => {
       }
     }
     if (update.iosName) {
-      if (!objGet(app, ['builds', 'ios'])) {
-        $set['builds.ios'] = getAppDefaultBuildFields(update.iosName, 'ios');
-      } else if (!lockedFields.iosName) {
+      if (!lockedFields.iosName) {
         $set['builds.ios.name'] = update.iosName;
       } else {
         throw new CrowdaaException(
@@ -52,6 +76,26 @@ export default async (appId, update) => {
           `Cannot change the iOS name for app ${app.name}`
         );
       }
+    }
+
+    if (update.iosAuthor) {
+      $set['builds.ios.author'] = update.iosAuthor;
+    }
+    if (update.iosEmail) {
+      $set['builds.ios.email'] = update.iosEmail;
+    }
+    if (update.iosDescription) {
+      $set['builds.ios.description'] = update.iosDescription;
+    }
+
+    if (update.androidAuthor) {
+      $set['builds.android.author'] = update.androidAuthor;
+    }
+    if (update.androidEmail) {
+      $set['builds.android.email'] = update.androidEmail;
+    }
+    if (update.androidDescription) {
+      $set['builds.android.description'] = update.androidDescription;
     }
 
     await db.collection(COLL_APPS).findOneAndUpdate({ _id: appId }, { $set });
