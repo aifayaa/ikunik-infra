a28f2142730feaeb5cf944d20b69126793e28e73
frozar <fabien.rozar@gmail.com>
2024-05-07 10:05:27 +0400

    Filter settings.iap from application document response

--- apps/lib/appsUtils.js
+++ apps/lib/appsUtils.js
@@ -6,6 +6,7 @@ export const appPrivateFields = [
   'credentials',
   'appleAccounts',
   'firebaseProjectId',
+  'settings.iap',
   'settings.userDataCollection',
   'settings.platformApplicationArns',
   'backend',
