a5f4a57d802dee17dc5e3e253fce121b5a68a18e
Maxime Ferrino <maxime@crowdaa.com>
2024-05-17 16:48:47 +0200

    feat(afrikpay): Added module for AfrikPay

---
+++ afrikpay/handlers/deleteAccount.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-relative-packages */
+import deleteAccount from '../lib/deleteAccount';
+import errorMessage from '../../libs/httpResponses/errorMessage';
+import response from '../../libs/httpResponses/response';
+
+export default async (event) => {
+  const { appId, principalId: userId } = event.requestContext.authorizer;
+
+  try {
+    if (!userId) {
+      throw new Error('forbidden');
+    }
+
+    await deleteAccount(appId, userId);
+    return response({ code: 200, body: { ok: true } });
+  } catch (e) {
+    return response(errorMessage({ message: e.message }));
+  }
+};
