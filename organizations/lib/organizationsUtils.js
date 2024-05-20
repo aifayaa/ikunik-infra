7b1ed612451bd3b5e95117679d11dd452ff9e384
NicolasC <nicolas.courtonne@crowdaa.com>
2024-05-20 17:41:56 +0400

    Move(orgs/fieldsChecks): Move functions to organizationsUtils.js

--- organizations/lib/organizationsUtils.js
+++ organizations/lib/organizationsUtils.js
@@ -8,3 +8,23 @@ export const applicationRolesInOrganization = [
   'moderator',
   'viewer',
 ];
+
+const isNonEmptyString = (val) => typeof val === 'string' && val.length > 0;
+
+export const createFieldChecks = {
+  name(val) {
+    return isNonEmptyString(val);
+  },
+};
+
+export const putAppFieldChecks = {
+  appId(val) {
+    return isNonEmptyString(val);
+  },
+};
+
+export const setOrgDebugPaidChecks = {
+  paymentOk(val) {
+    return typeof val === 'boolean';
+  },
+};
