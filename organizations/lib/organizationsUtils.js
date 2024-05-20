8e2b949296e8ea124f866d9403c4b6a7cf59c7e0
frozar <fabien.rozar@gmail.com>
2024-05-20 22:55:13 +0400

    Delete unused functions and variables

--- organizations/lib/organizationsUtils.js
+++ organizations/lib/organizationsUtils.js
@@ -8,17 +8,3 @@ export const applicationRolesInOrganization = [
   'moderator',
   'viewer',
 ];
-
-const isNonEmptyString = (val) => typeof val === 'string' && val.length > 0;
-
-export const createFieldChecks = {
-  name(val) {
-    return isNonEmptyString(val);
-  },
-};
-
-export const putAppFieldChecks = {
-  appId(val) {
-    return isNonEmptyString(val);
-  },
-};
