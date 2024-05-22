567488d9de86d152e1cfdca815090199255df65e
frozar <fabien.rozar@gmail.com>
2024-05-22 07:33:31 +0400

    Create organisation: handle error case

--- organizations/lib/createOrg.js
+++ organizations/lib/createOrg.js
@@ -15,96 +15,110 @@ const { STRIPE_SECRET_KEY } = process.env;
 const { COLL_ORGANIZATIONS, COLL_USERS } = mongoCollections;
 
 export default async (userId, name, email, appleTeamId, appleCompanyName) => {
-  const client = await MongoClient.connect();
+  let customerId;
 
-  // Documentation, how to use transaction:
-  // https://www.mongodb.com/docs/drivers/node/current/usage-examples/transaction-conv/#std-label-node-usage-convenient-txn
-  // Return result of transaction by side effect
-  let sessionRes;
+  const stripe = (() => {
+    if (STRIPE_SECRET_KEY === undefined) {
+      throw new CrowdaaError(
+        ERROR_TYPE_SETUP,
+        MISSING_ENVIRONMENT_VARIABLE_CODE,
+        `Missing environment variable STRIPE_SECRET_KEY: ${STRIPE_SECRET_KEY}`,
+        { httpCode: 500 }
+      );
+    }
 
-  await client
-    .withSession(async (sessionArg) => {
-      await sessionArg.withTransaction(async (session) => {
-        const stripe = (() => {
-          if (STRIPE_SECRET_KEY === undefined) {
-            throw new CrowdaaError(
-              ERROR_TYPE_SETUP,
-              MISSING_ENVIRONMENT_VARIABLE_CODE,
-              `Missing environment variable STRIPE_SECRET_KEY: ${STRIPE_SECRET_KEY}`,
-              { httpCode: 500 }
-            );
+    return new Stripe(STRIPE_SECRET_KEY, {
+      apiVersion: '2024-04-10',
+      typescript: true,
+    });
+  })();
+
+  try {
+    const client = await MongoClient.connect();
+
+    // Documentation, how to use transaction:
+    // https://www.mongodb.com/docs/drivers/node/current/usage-examples/transaction-conv/#std-label-node-usage-convenient-txn
+    // Return result of transaction by side effect
+    let sessionRes;
+
+    await client
+      .withSession(async (sessionArg) => {
+        await sessionArg.withTransaction(async (session) => {
+          const newOrganization = {
+            name,
+            email,
+            apple: {
+              setupDone: false,
+            },
+
+            _id: new ObjectID().toString(),
+            createdAt: new Date(),
+            createdBy: userId,
+          };
+
+          const customer = await stripe.customers.create({
+            name,
+            email,
+            metadata: {
+              _id: new ObjectID().toString(),
+              createdAt: newOrganization.createdAt,
+              createdBy: newOrganization.createdBy,
+            },
+          });
+
+          customerId = customer.id;
+
+          newOrganization.customer_id = customerId;
+
+          if (appleTeamId) {
+            newOrganization.apple.teamId = appleTeamId;
+            newOrganization.apple.teamStatus = 'checking';
+          }
+          if (appleCompanyName) {
+            newOrganization.apple.companyName = appleCompanyName;
           }
 
-          return new Stripe(STRIPE_SECRET_KEY, {
-            apiVersion: '2024-04-10',
-            typescript: true,
-          });
-        })();
+          const db = client.db();
 
-        const newOrganization = {
-          name,
-          email,
-          apple: {
-            setupDone: false,
-          },
+          await db
+            .collection(COLL_ORGANIZATIONS)
+            .insertOne(newOrganization, { session });
 
-          _id: new ObjectID().toString(),
-          createdAt: new Date(),
-          createdBy: userId,
-        };
-
-        const customer = await stripe.customers.create({
-          name,
-          email,
-          metadata: {
-            _id: new ObjectID().toString(),
-            createdAt: newOrganization.createdAt,
-            createdBy: newOrganization.createdBy,
-          },
-        });
-
-        newOrganization.customer_id = customer.id;
-
-        if (appleTeamId) {
-          newOrganization.apple.teamId = appleTeamId;
-          newOrganization.apple.teamStatus = 'checking';
-        }
-        if (appleCompanyName) {
-          newOrganization.apple.companyName = appleCompanyName;
-        }
-
-        const db = client.db();
-
-        await db
-          .collection(COLL_ORGANIZATIONS)
-          .insertOne(newOrganization, { session });
-
-        await db.collection(COLL_USERS).updateOne(
-          { _id: userId },
-          {
-            $push: {
-              'perms.organizations': {
-                _id: newOrganization._id,
-                roles: ['owner'],
+          await db.collection(COLL_USERS).updateOne(
+            { _id: userId },
+            {
+              $push: {
+                'perms.organizations': {
+                  _id: newOrganization._id,
+                  roles: ['owner'],
+                },
               },
             },
-          },
-          { session }
-        );
+            { session }
+          );
 
-        const { _id: orgId } = newOrganization;
-        await syncCreateOrganizationBaserow(userId, {
-          orgId,
-          name,
-          customer_id: newOrganization.customer_id,
+          const { _id: orgId } = newOrganization;
+          await syncCreateOrganizationBaserow(userId, {
+            orgId,
+            name,
+            customerId,
+          });
+
+          sessionRes = newOrganization;
         });
-
-        sessionRes = newOrganization;
+      })
+      .finally(() => {
+        client.close();
       });
-    })
-    .finally(() => {
-      client.close();
-    });
 
-  return sessionRes;
+    return sessionRes;
+  } catch (exception) {
+    // If something wrong happen and the Stripe customer_id has been created,
+    // -> delete it
+    if (customerId) {
+      await stripe.customers.del(customerId);
+    }
+
+    throw exception;
+  }
 };
