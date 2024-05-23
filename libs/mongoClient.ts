ceb83f21b5a10f70e3d895e364c8dd43c9be2010
frozar <fabien.rozar@gmail.com>
2024-05-23 15:38:58 +0400

    Revert "Merge branch 'feat/ts-mongdb' into 'dev'"
    This reverts commit 01cd6cafddb58d2a63009765e68860bdb5bb14d6, reversing
    changes made to f7b54265b55516ef42f7700100108750f736ec0d.

--- libs/mongoClient.ts
+++ libs/mongoClient.ts
@@ -51,13 +51,9 @@ MongoClient.connect = async function connectMethodOverride(mongoUrl?: string) {
   }
 
   const client = await originConnect(effectiveMongoUrl, DEFAULT_OPTS);
-
-  const originClose = client.close.bind(client);
-
   client.forceCloseThisConnectionNow = () => {
-    originClose(true);
+    client.close(true);
   };
-
   client.close = () => new Promise((resolve) => resolve());
 
   prevClients[connectArgs] = client;
