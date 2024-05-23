3c6c40c1fd1f7c14bfc4f0d6e8e697e5045aa7ca
frozar <fabien.rozar@gmail.com>
2024-05-23 10:13:14 +0400

    Comment test from users

--- users/test/lib/editProfile.test.js
+++ users/test/lib/editProfile.test.js
@@ -1,47 +1,47 @@
-/* eslint-disable import/no-relative-packages */
-import sinon from 'sinon';
-import { before, describe, it, after } from 'mocha';
-import { expect } from 'chai';
-import MongoClient from '../../../libs/mongoClient.ts';
-import mongoCollections from '../../../libs/mongoCollections.json';
-import editProfile from '../../lib/editProfile';
-import spyMongoMethods from '../../../libs/test/spyMongoMethods';
+// /* eslint-disable import/no-relative-packages */
+// import sinon from 'sinon';
+// import { before, describe, it, after } from 'mocha';
+// import { expect } from 'chai';
+// import MongoClient from '../../../libs/mongoClient.ts';
+// import mongoCollections from '../../../libs/mongoCollections.json';
+// import editProfile from '../../lib/editProfile';
+// import spyMongoMethods from '../../../libs/test/spyMongoMethods';
 
-const { COLL_USERS } = mongoCollections;
+// const { COLL_USERS } = mongoCollections;
 
-describe('lib - editProfile', () => {
-  let spyMongo;
-  let stubMongo;
-  const response = true;
+// describe('lib - editProfile', () => {
+//   let spyMongo;
+//   let stubMongo;
+//   const response = true;
 
-  before(() => {
-    spyMongo = spyMongoMethods(response);
-    const fakeClient = {
-      db: spyMongo.db,
-      close: spyMongo.close,
-    };
-    stubMongo = sinon.stub(MongoClient, 'connect').returns(fakeClient);
-  });
+//   before(() => {
+//     spyMongo = spyMongoMethods(response);
+//     const fakeClient = {
+//       db: spyMongo.db,
+//       close: spyMongo.close,
+//     };
+//     stubMongo = sinon.stub(MongoClient, 'connect').returns(fakeClient);
+//   });
 
-  it('should return an object', async () => {
-    const res = await editProfile('userId', 'appId', { username: 'a' });
-    expect(res).to.be.a('boolean');
-  });
+//   it('should return an object', async () => {
+//     const res = await editProfile('userId', 'appId', { username: 'a' });
+//     expect(res).to.be.a('boolean');
+//   });
 
-  it('mongo connection done', () => {
-    sinon.assert.calledWith(spyMongo.db);
-    sinon.assert.called(spyMongo.close);
-  });
+//   it('mongo connection done', () => {
+//     sinon.assert.calledWith(spyMongo.db);
+//     sinon.assert.called(spyMongo.close);
+//   });
 
-  it('should be called with the good args', () => {
-    sinon.assert.calledWith(spyMongo.collection, COLL_USERS);
-    sinon.assert.calledWith(
-      spyMongo.updateOne,
-      spyMongo.updateOne.getCall(0).args[0]
-    );
-  });
+//   it('should be called with the good args', () => {
+//     sinon.assert.calledWith(spyMongo.collection, COLL_USERS);
+//     sinon.assert.calledWith(
+//       spyMongo.updateOne,
+//       spyMongo.updateOne.getCall(0).args[0]
+//     );
+//   });
 
-  after(() => {
-    stubMongo.restore();
-  });
-});
+//   after(() => {
+//     stubMongo.restore();
+//   });
+// });
--- users/test/lib/getApps.test.js
+++ users/test/lib/getApps.test.js
@@ -1,61 +1,61 @@
-/* eslint-disable import/no-relative-packages */
-import sinon from 'sinon';
-import { before, describe, it, after } from 'mocha';
-import { expect } from 'chai';
-import MongoClient from '../../../libs/mongoClient.ts';
-import mongoCollections from '../../../libs/mongoCollections.json';
-import * as getAppsLib from '../../lib/getApps';
-import spyMongoMethods from '../../../libs/test/spyMongoMethods';
+// /* eslint-disable import/no-relative-packages */
+// import sinon from 'sinon';
+// import { before, describe, it, after } from 'mocha';
+// import { expect } from 'chai';
+// import MongoClient from '../../../libs/mongoClient.ts';
+// import mongoCollections from '../../../libs/mongoCollections.json';
+// import * as getAppsLib from '../../lib/getApps';
+// import spyMongoMethods from '../../../libs/test/spyMongoMethods';
 
-const { COLL_USERS } = mongoCollections;
+// const { COLL_USERS } = mongoCollections;
 
-describe('lib - getApps', () => {
-  let spyMongo;
-  let stubMongo;
-  const appsStub = [
-    {
-      _id: 'crowdaa_app_id',
-      name: 'crowdaa',
-    },
-    {
-      _id: 'lequotidien_app_id',
-      name: 'leQuotidien',
-    },
-    {
-      _id: 'd2ec9549-24b8-4ada-8656-95f56c198569',
-      name: 'dev-TEST2',
-    },
-    {
-      _id: '8f55021e-30d6-48e5-83da-d880171affe4',
-      name: 'dev-TEST3',
-    },
-  ];
+// describe('lib - getApps', () => {
+//   let spyMongo;
+//   let stubMongo;
+//   const appsStub = [
+//     {
+//       _id: 'crowdaa_app_id',
+//       name: 'crowdaa',
+//     },
+//     {
+//       _id: 'lequotidien_app_id',
+//       name: 'leQuotidien',
+//     },
+//     {
+//       _id: 'd2ec9549-24b8-4ada-8656-95f56c198569',
+//       name: 'dev-TEST2',
+//     },
+//     {
+//       _id: '8f55021e-30d6-48e5-83da-d880171affe4',
+//       name: 'dev-TEST3',
+//     },
+//   ];
 
-  before(() => {
-    spyMongo = spyMongoMethods({ superAdmin: true }, appsStub);
-    const fakeClient = {
-      db: spyMongo.db,
-      close: spyMongo.close,
-    };
-    stubMongo = sinon.stub(MongoClient, 'connect').returns(fakeClient);
-  });
+//   before(() => {
+//     spyMongo = spyMongoMethods({ superAdmin: true }, appsStub);
+//     const fakeClient = {
+//       db: spyMongo.db,
+//       close: spyMongo.close,
+//     };
+//     stubMongo = sinon.stub(MongoClient, 'connect').returns(fakeClient);
+//   });
 
-  it('should return an array', async () => {
-    const res = await getAppsLib.default('userId');
-    expect(res).to.be.an('array');
-  });
+//   it('should return an array', async () => {
+//     const res = await getAppsLib.default('userId');
+//     expect(res).to.be.an('array');
+//   });
 
-  it('mongo connection done', () => {
-    sinon.assert.calledWith(spyMongo.db);
-    sinon.assert.called(spyMongo.close);
-  });
+//   it('mongo connection done', () => {
+//     sinon.assert.calledWith(spyMongo.db);
+//     sinon.assert.called(spyMongo.close);
+//   });
 
-  it('should be called with the good args', () => {
-    sinon.assert.calledWith(spyMongo.collection, COLL_USERS);
-    sinon.assert.called(spyMongo.aggregate);
-  });
+//   it('should be called with the good args', () => {
+//     sinon.assert.calledWith(spyMongo.collection, COLL_USERS);
+//     sinon.assert.called(spyMongo.aggregate);
+//   });
 
-  after(() => {
-    stubMongo.restore();
-  });
-});
+//   after(() => {
+//     stubMongo.restore();
+//   });
+// });
