ee9b4313d2e5cd3ef0d6402c25b115bae388a5b1
Maxime Ferrino <maxime@crowdaa.com>
2024-05-30 10:45:20 +0200

    fix(users): Fixed getApps to list org-related apps too.

--- users/handlers/getApps.js
+++ users/handlers/getApps.js
@@ -1,45 +1,18 @@
 /* eslint-disable import/no-relative-packages */
-import { typeCheck } from 'type-check';
 import errorMessage from '../../libs/httpResponses/errorMessage';
-import getApps from '../lib/getApps';
 import response from '../../libs/httpResponses/response.ts';
+import getApps from '../lib/getApps';
 
 export default async (event) => {
   const { principalId: userId } = event.requestContext.authorizer;
   const { id: urlId } = event.pathParameters;
-  const { sortBy, sortOrder } = event.queryStringParameters || {};
 
   try {
     if (userId !== urlId) {
       throw new Error('forbidden');
     }
 
-    if (sortBy) {
-      if (!typeCheck('{ sortBy: String }', { sortBy })) {
-        throw new Error('wrong_argument_type');
-      }
-
-      /* Using array here in case we add other sort values later */
-      if (['name'].indexOf(sortBy) < 0) {
-        throw new Error('wrong_argument_value');
-      }
-    }
-
-    if (sortOrder) {
-      if (!typeCheck('{ sortOrder: String }', { sortOrder })) {
-        throw new Error('wrong_argument_type');
-      }
-
-      if (['asc', 'desc'].indexOf(sortOrder) < 0) {
-        throw new Error('wrong_argument_value');
-      }
-    }
-
-    if ((sortBy && !sortOrder) || (!sortBy && sortOrder)) {
-      throw new Error('missing_argument');
-    }
-
-    const appsResults = await getApps(userId, { sortBy, sortOrder });
+    const appsResults = await getApps(userId);
 
     return response({
       code: 200,
