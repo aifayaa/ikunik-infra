354e17d9e96cdad5c04188fc4c949b79ddf48793
frozar <fabien.rozar@gmail.com>
2024-06-18 18:43:16 +0400

    Convert password to TS

--- users/lib/userEntity.ts
+++ users/lib/userEntity.ts
@@ -5,6 +5,11 @@ import {
 
 export type UserType = {
   _id: string;
+  services: {
+    password?: {
+      bcrypt: string;
+    };
+  };
   superAdmin?: boolean;
   perms?: {
     apps?: Array<{ _id: string; roles: Array<AppsPermType> }>;
