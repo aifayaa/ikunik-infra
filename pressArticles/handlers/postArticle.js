dd05b06dc693770402221b731d0e0275beadc44c
Maxime Ferrino <maxime@crowdaa.com>
2024-06-20 10:34:41 +0200

    prettier fix...

--- pressArticles/handlers/postArticle.js
+++ pressArticles/handlers/postArticle.js
@@ -85,7 +85,7 @@ export default async (event) => {
           likes,
           md = '',
           mediaCaptions,
-          pdfs =[],
+          pdfs = [],
           pdfsOpenButton = '',
           pictures,
           productId,
