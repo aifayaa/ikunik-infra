02c0522cc8282643d8d7084f18d8ad99e8982a74
Maxime Ferrino <maxime@crowdaa.com>
2024-07-26 15:44:13 +0400

    Improved CI. Fixed eslint typo in api-keys

--- api-keys/getClient.js
+++ api-keys/getClient.js
@@ -2,7 +2,10 @@ const findKey = require('lodash/findKey');
 const plan = require('./api-keys.tf.json');
 
 export default (apiKey) => {
-  const key = findKey(plan.resource.aws_api_gateway_api_key, ((keys) => keys.value === apiKey));
+  const key = findKey(
+    plan.resource.aws_api_gateway_api_key,
+    (keys) => keys.value === apiKey
+  );
   if (!key) return 'crowdaa';
   return plan.resource.aws_api_gateway_api_key[key].name;
 };
