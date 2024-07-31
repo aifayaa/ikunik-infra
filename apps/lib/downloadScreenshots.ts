fccbbab696d10ba4d58c4129fa991db709309491
Maxime Ferrino <maxime@crowdaa.com>
2024-07-31 17:59:06 +0400

    feat(apps): downloadScreenshots API

---
+++ apps/lib/downloadScreenshots.ts
@@ -0,0 +1,109 @@
+/* eslint-disable import/no-relative-packages */
+import AWS from 'aws-sdk';
+import nodePath from 'node:path';
+import nodeFs from 'node:fs';
+import nodeStream from 'node:stream';
+import archiver from 'archiver';
+import MongoClient from '../../libs/mongoClient.js';
+
+import { getApp } from './appsUtils';
+
+const { S3_APPS_RESSOURCES } = process.env;
+
+const DESTINATION_SCREENSHOT__DIR_KEY_PATH = 'screenshots';
+const DESTINATION_SCREENSHOT_KEY_PATH = `${DESTINATION_SCREENSHOT__DIR_KEY_PATH}/screenshots.zip`;
+const LOCAL_ZIP_PATH = '/tmp/screenshots.zip';
+
+const s3 = new AWS.S3({
+  signatureVersion: 'v4',
+});
+
+export default async (appId: string, validityDuration: number) => {
+  const client = await MongoClient.connect();
+
+  try {
+    await getApp(appId);
+
+    const screenshotFiles = await s3
+      .listObjects({
+        Bucket: S3_APPS_RESSOURCES as string,
+        Prefix: `${appId}/${DESTINATION_SCREENSHOT__DIR_KEY_PATH}`,
+      })
+      .promise();
+
+    if (!screenshotFiles.Contents || screenshotFiles.Contents.length === 0) {
+      return null;
+    }
+
+    const screenshotFilesInfos = screenshotFiles.Contents;
+
+    await new Promise(
+      (resolve: (value?: any) => void, reject: (reason: any) => void) => {
+        const archive = archiver('zip', {
+          zlib: { level: 9 },
+        });
+        archive.on('warning', function (err: { code: string }) {
+          if (err.code === 'ENOENT') {
+            console.warn('File not found:', err);
+          } else {
+            throw err;
+          }
+        });
+
+        archive.on('error', (err: Error) => {
+          throw err;
+        });
+
+        archive.on('end', () => {
+          resolve();
+        });
+
+        const output = nodeFs.createWriteStream(LOCAL_ZIP_PATH);
+        archive.pipe(output);
+
+        screenshotFilesInfos.forEach(({ Key }) => {
+          if (!Key) {
+            return null;
+          }
+
+          const fileName = nodePath.basename(Key);
+
+          const stream = s3
+            .getObject({
+              Bucket: S3_APPS_RESSOURCES as string,
+              Key,
+            })
+            .createReadStream()
+            .on('error', reject);
+
+          archive.append(stream, { name: fileName });
+        });
+
+        archive.finalize();
+      }
+    );
+
+    const input = await nodeFs.promises.readFile(LOCAL_ZIP_PATH);
+
+    await s3
+      .putObject({
+        Body: input,
+        Bucket: S3_APPS_RESSOURCES as string,
+        Key: `${appId}/${DESTINATION_SCREENSHOT_KEY_PATH}`,
+      })
+      .promise();
+
+    const s3Params = {
+      Bucket: S3_APPS_RESSOURCES,
+      Key: `${appId}/${DESTINATION_SCREENSHOT_KEY_PATH}`,
+      Expires: validityDuration,
+    };
+    console.log('s3Params', s3Params);
+
+    const url = s3.getSignedUrl('getObject', s3Params);
+
+    return url;
+  } finally {
+    client.close();
+  }
+};
