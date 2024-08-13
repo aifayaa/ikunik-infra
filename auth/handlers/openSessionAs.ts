ae60e763b003dcc8a4cd864743090c3d6e5ae844
Maxime Ferrino <maxime@crowdaa.com>
2024-08-13 16:06:40 +0400

    feat(openSessionAs): Added optionnal id parameter

--- auth/handlers/openSessionAs.ts
+++ auth/handlers/openSessionAs.ts
@@ -2,7 +2,7 @@
 import { z } from 'zod';
 import { formatValidationErrors } from '../../libs/httpResponses/formatValidationErrors';
 import { checkPermsIsSuperAdmin } from '../../libs/perms/checkPermsFor';
-import openSessionAs from '../lib/openSessionAs';
+import openSessionAs, { OpenSessionAsType } from '../lib/openSessionAs';
 import response, { handleException } from '../../libs/httpResponses/response';
 import { formatResponseBody } from '../../libs/httpResponses/formatResponseBody';
 import { APIGatewayProxyEvent } from 'aws-lambda';
@@ -12,27 +12,34 @@ import {
 } from '../../libs/httpResponses/errorCodes';
 import { CrowdaaError } from '../../libs/httpResponses/CrowdaaError';
 
-/**
- * As the source user must be an 'admin' of the application, he can give
- * any roles to the target user.
- */
 export default async (event: APIGatewayProxyEvent) => {
   const { principalId: sourceUserId } = event.requestContext.authorizer as {
     principalId: string;
   };
 
   try {
-    const openSessionAsSchema = z
-      .object({
-        email: z
-          .string({
-            required_error: 'email is required',
-            invalid_type_error: 'email must be a string',
-          })
-          .trim(),
-      })
-      .required();
-
+    const openSessionAsSchema = z.union([
+      z
+        .object({
+          email: z
+            .string({
+              required_error: 'email is required',
+              invalid_type_error: 'email must be a string',
+            })
+            .trim(),
+        })
+        .required(),
+      z
+        .object({
+          id: z
+            .string({
+              required_error: 'id is required',
+              invalid_type_error: 'id must be a string',
+            })
+            .trim(),
+        })
+        .required(),
+    ]);
     if (!event.body) {
       throw new CrowdaaError(
         ERROR_TYPE_VALIDATION_ERROR,
@@ -46,16 +53,16 @@ export default async (event: APIGatewayProxyEvent) => {
 
     let validatedBody;
     try {
-      validatedBody = openSessionAsSchema.parse(body);
+      validatedBody = openSessionAsSchema.parse(body) as OpenSessionAsType;
     } catch (exception) {
       return formatValidationErrors(exception);
     }
 
     await checkPermsIsSuperAdmin(sourceUserId);
 
-    const { email } = validatedBody;
+    const { email, id } = validatedBody;
 
-    const sessionData = await openSessionAs(email);
+    const sessionData = await openSessionAs({ email, id });
 
     return response({
       code: 200,
--- auth/lib/openSessionAs.ts
+++ auth/lib/openSessionAs.ts
@@ -3,32 +3,71 @@
 // createUser method from
 // https://github.com/meteor/meteor/blob/devel/packages/accounts-password/password_server.js
 
+import {
+  APP_NOT_FOUND_CODE,
+  ERROR_TYPE_NOT_FOUND,
+  ERROR_TYPE_VALIDATION_ERROR,
+} from '@libs/httpResponses/errorCodes';
 import MongoClient from '../../libs/mongoClient';
 import mongoCollections from '../../libs/mongoCollections.json';
-import { UTMType, UserProfileType } from '../../users/lib/userEntity';
+import { UTMType, UserProfileType, UserType } from '../../users/lib/userEntity';
 import { crowdaaLogin } from './backends/crowdaaLogin';
 import { crowdaaRegister } from './backends/crowdaaRegister';
 import syncAdminRegisterBaserow from './backends/syncAdminRegisterBaserow';
 import postLoginChecks from './postLoginChecks';
+import { CrowdaaError } from '@libs/httpResponses/CrowdaaError';
+import { VALIDATION_FAILED_CODE } from '@libs/httpResponses/formatValidationErrors';
 
 const { ADMIN_APP } = process.env;
 
-const { COLL_APPS } = mongoCollections;
+const { COLL_APPS, COLL_USERS } = mongoCollections;
 
-export default async (email: string) => {
+export type OpenSessionAsType = {
+  email?: string;
+  id?: string;
+};
+
+export default async ({ email, id }: OpenSessionAsType) => {
   const client = await MongoClient.connect();
 
   try {
     const appsCollection = client.db().collection(COLL_APPS);
 
     const app = await appsCollection.findOne({ _id: ADMIN_APP });
-    if (!app) throw new Error('app_not_found');
+    if (!app)
+      throw new CrowdaaError(
+        ERROR_TYPE_NOT_FOUND,
+        APP_NOT_FOUND_CODE,
+        `Cannot find application with ID '${ADMIN_APP}'`
+      );
 
-    const ret = await crowdaaLogin('', email, '', app, {
-      noPasswordCheck: true,
-    });
+    if (id) {
+      const user = await client
+        .db()
+        .collection(COLL_USERS)
+        .findOne({ _id: id, appId: ADMIN_APP });
+      if (user) {
+        const userEmail =
+          user.emails && user.emails[0] && user.emails[0].address;
+        if (userEmail) {
+          email = userEmail;
+        }
+      }
+    }
 
-    return ret;
+    if (email) {
+      const ret = await crowdaaLogin('', email, '', app, {
+        noPasswordCheck: true,
+      });
+
+      return ret;
+    }
+
+    throw new CrowdaaError(
+      ERROR_TYPE_VALIDATION_ERROR,
+      VALIDATION_FAILED_CODE,
+      `Missing both ${id} and ${email} fields in the request`
+    );
   } finally {
     client.close();
   }
