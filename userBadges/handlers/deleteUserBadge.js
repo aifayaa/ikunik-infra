9d8ce4306a20ab403d09a89581a1c9b764e690f8
Maxime Ferrino <maxime@crowdaa.com>
2024-08-23 10:45:45 +0400

    Added badges limits

--- userBadges/handlers/deleteUserBadge.js
+++ userBadges/handlers/deleteUserBadge.js
@@ -3,6 +3,7 @@ import deleteUserBadge from '../lib/deleteUserBadge';
 import errorMessage from '../../libs/httpResponses/errorMessage';
 import response from '../../libs/httpResponses/response.ts';
 import { checkPermsForApp } from '../../libs/perms/checkPermsFor.ts';
+import checkAppPlanForLimits from '../../appsFeaturePlans/lib/checkAppPlanForLimits.ts';
 
 export default async (event) => {
   const { appId, principalId: userId } = event.requestContext.authorizer;
@@ -10,6 +11,15 @@ export default async (event) => {
 
   try {
     await checkPermsForApp(userId, appId, ['admin']);
+
+    const allowed = await checkAppPlanForLimits(appId, 'badges', () => {
+      // We check for hard limits only and block them
+      return Promise.resolve(0);
+    });
+
+    if (!allowed) {
+      throw new Error('app_limits_exceeded');
+    }
 
     await deleteUserBadge(userBadgeId, appId);
     return response({ code: 200, body: { ok: true } });
--- userBadges/handlers/editUserBadge.js
+++ userBadges/handlers/editUserBadge.js
@@ -4,6 +4,7 @@ import fieldChecks from '../lib/badgeFieldsChecks';
 import errorMessage from '../../libs/httpResponses/errorMessage';
 import response from '../../libs/httpResponses/response.ts';
 import { checkPermsForApp } from '../../libs/perms/checkPermsFor.ts';
+import checkAppPlanForLimits from '../../appsFeaturePlans/lib/checkAppPlanForLimits.ts';
 
 export default async (event) => {
   const { appId, principalId: userId } = event.requestContext.authorizer;
@@ -23,6 +24,15 @@ export default async (event) => {
 
       if (!cb(bodyParsed[field])) throw new Error('mal_formed_request');
     });
+
+    const allowed = await checkAppPlanForLimits(appId, 'badges', () => {
+      // We check for hard limits only and block them
+      return Promise.resolve(0);
+    });
+
+    if (!allowed) {
+      throw new Error('app_limits_exceeded');
+    }
 
     const userBadge = await editUserBadge(userBadgeId, appId, bodyParsed, {
       userId,
