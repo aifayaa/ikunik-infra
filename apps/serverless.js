2e123580bc479e233382b18f27b6faf79d139e17
frozar <fabien.rozar@gmail.com>
2024-08-28 15:21:55 +0400

    Cleanup

--- apps/serverless.js
+++ apps/serverless.js
@@ -714,48 +714,6 @@ const serverlessConfiguration = {
         },
       ],
     },
-    // stripeCheckout: {
-    //   handler: 'handlers/postAppsIdCheckout.default',
-    //   events: [
-    //     {
-    //       http: {
-    //         path: 'apps/{id}/checkout',
-    //         method: 'POST',
-    //         cors: true,
-    //         authorizer: {
-    //           type: 'CUSTOM',
-    //           authorizerId:
-    //             '${cf:account-${self:provider.stage}.ApiGatewayAuthorizerAdminId}',
-    //         },
-    //         request: {
-    //           parameters: {
-    //             paths: { id: true },
-    //             headers: {
-    //               Authorization: true,
-    //             },
-    //           },
-    //         },
-    //       },
-    //     },
-    //   ],
-    // },
-    // stripeWebhook: {
-    //   handler: 'handlers/postAppsWebhook.default',
-    //   events: [
-    //     {
-    //       http: {
-    //         path: 'apps/webhooks/stripe',
-    //         method: 'post',
-    //         cors: true,
-    //         authorizer: {
-    //           type: 'CUSTOM',
-    //           authorizerId:
-    //             '${cf:account-${self:provider.stage}.ApiGatewayAuthorizerPublicId}',
-    //         },
-    //       },
-    //     },
-    //   ],
-    // },
     // 2024/08/08: Integration of Pascal's work
     // stripeEnableSubscription: {
     //   handler: 'handlers/putAppsIdEnableSubscription.default',
