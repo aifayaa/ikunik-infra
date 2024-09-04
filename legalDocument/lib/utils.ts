6f7b2e17d5cae0fba5c009acf5eca8e114c741ee
Maxime Ferrino <maxime@crowdaa.com>
2024-09-04 17:48:37 +0400

    feat(legalDocument): HTML script & entities filtering

--- legalDocument/lib/utils.ts
+++ legalDocument/lib/utils.ts
@@ -6,20 +6,40 @@ const s3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 
+// Inspired from : https://stackoverflow.com/a/9899441
+export function removeScriptsFromHtml(html: string) {
+  const scriptRegex = /<script\b.*[^<]\bscript>/gi;
+  while (scriptRegex.test(html)) {
+    html = html.replace(scriptRegex, '');
+  }
+  return html;
+}
+
 export function wrapHtmlTag(title: string, html: string) {
+  const escapedTitle = title.replace(/[&<>"']/g, (tag) => {
+    const tagsToReplace = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#039;',
+    } as Record<string, string>;
+    return tagsToReplace[tag] || tag;
+  });
+
   const htmlWrapperHead =
     '<!doctype html>' +
     '<html>' +
     '<head>' +
     '<meta charset="utf-8" />' +
     '<meta name="viewport" content="width=device-width" />' +
-    '<title>Terms of Services</title>' +
+    `<title>${escapedTitle}</title>` +
     '</head>' +
     '<body>';
 
   const htmlWrapperTail = '</body></html>';
 
-  return `${htmlWrapperHead}<h1>${title}</h1>${html}${htmlWrapperTail}`;
+  return `${htmlWrapperHead}<h1>${escapedTitle}</h1>${html}${htmlWrapperTail}`;
 }
 
 export function computeS3Filepath(
