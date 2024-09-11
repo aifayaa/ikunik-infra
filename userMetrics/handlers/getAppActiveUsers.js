cff0c9f275130752010846d70bd6d266e86c0874
frozar <fabien.rozar@gmail.com>
2024-09-11 17:44:47 +0400

    Bugfix: queryStringParameter are string

--- userMetrics/handlers/getAppActiveUsers.js
+++ userMetrics/handlers/getAppActiveUsers.js
@@ -8,14 +8,19 @@ import { getAppActiveUsers } from '../lib/getAppActiveUsers';
 export default async (event) => {
   const { appId, principalId: userId } = event.requestContext.authorizer;
 
-  const { period = -1 } = event.queryStringParameters || {};
+  const { period: periodString } = event.queryStringParameters || {
+    period: '-1',
+  };
 
   try {
     await checkPermsForApp(userId, appId, ['admin']);
 
     const app = await getApp(appId);
 
-    const results = await getAppActiveUsers(app, { period });
+    const period = parseInt(periodString, 10);
+    const results = await getAppActiveUsers(app, {
+      period,
+    });
     return response({ code: 200, body: results });
   } catch (e) {
     return response(errorMessage(e));
