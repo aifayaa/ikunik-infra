68f331750618c204be9ef6928a70d495e151856e
frozar <fabien.rozar@gmail.com>
2024-10-08 16:37:26 +0400

    Delete unused code 'invitation_unauthorized_status'

--- libs/httpResponses/errorMessage.js
+++ libs/httpResponses/errorMessage.js
@@ -32,7 +32,6 @@ export default ({ code, message = 'Error' } = {}) => {
       errorCode = 404;
       break;
     case 'already_exists':
-    case 'invitation_unauthorized_status':
     case 'invitation_unauthorized_action':
     case 'invitation_expired':
     case 'invitation_unrecognized_user':
