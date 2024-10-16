b9f82342e1d55428d99c9903fd539882b94983b9
Maxime Ferrino <maxime@crowdaa.com>
2024-10-16 11:43:49 +0400

    Removed perms checks for getTicket

--- ticketing/handlers/getTicket.ts
+++ ticketing/handlers/getTicket.ts
@@ -1,17 +1,14 @@
 /* eslint-disable import/no-relative-packages */
 import response, { handleException } from '../../libs/httpResponses/response';
-import { checkPermsForApp } from '../../libs/perms/checkPermsFor';
 import { formatResponseBody } from '../../libs/httpResponses/formatResponseBody';
 import getTicket from '../lib/getTicket';
 
 export default async (event: any) => {
-  const { appId, principalId: userId } = event.requestContext.authorizer;
-  const bookableId = event.pathParameters.id;
+  const { appId } = event.requestContext.authorizer;
+  const ticketId = event.pathParameters.id;
 
   try {
-    await checkPermsForApp(userId, appId, ['admin']);
-
-    const ticket = await getTicket(bookableId, appId);
+    const ticket = await getTicket(ticketId, appId);
 
     return response({
       code: 200,
