c7d55df03ace94e4c290026f857c2e1d631d2ad3
frozar <fabien.rozar@gmail.com>
2024-10-22 14:25:34 +0400

    Add the 'reportedContent' module

---
+++ reportedContents/lib/getMyReportedContents.ts
@@ -0,0 +1,67 @@
+/* eslint-disable import/no-relative-packages */
+import MongoClient from '../../libs/mongoClient';
+import mongoCollections from '../../libs/mongoCollections.json';
+
+const { COLL_USER_GENERATED_CONTENTS_USER_REPORTS, COLL_USERS } =
+  mongoCollections;
+
+type reportedUserType = {
+  _id: string;
+  appId: string;
+  createdAt: Date;
+  details: string;
+  reason: string;
+  reportedUserId: string;
+  ugcId: string;
+  userId: string;
+};
+
+type aggregatedReportedUserType =
+  | reportedUserType
+  | { reportedUserUsername: string | undefined };
+
+export default async (userId: string, { appId }: { appId: string }) => {
+  const client = await MongoClient.connect();
+
+  try {
+    const db = client.db();
+
+    const aggregatedReportedUsers: Array<aggregatedReportedUserType> = await db
+      .collection(COLL_USER_GENERATED_CONTENTS_USER_REPORTS)
+      .aggregate([
+        {
+          $match: {
+            appId,
+            userId,
+          },
+        },
+        {
+          $lookup: {
+            from: COLL_USERS,
+            localField: 'reportedUserId',
+            foreignField: '_id',
+            pipeline: [
+              {
+                $project: { 'profile.username': 1, _id: 0 },
+              },
+              { $set: { username: '$profile.username' } },
+              { $unset: 'profile' },
+            ],
+            as: 'reportedUser',
+          },
+        },
+        {
+          $set: {
+            reportedUserUsername: '$reportedUser.username',
+          },
+        },
+        { $unset: 'reportedUser' },
+        { $unwind: '$reportedUserUsername' },
+      ])
+      .toArray();
+
+    return aggregatedReportedUsers;
+  } finally {
+    client.close();
+  }
+};
---
+++ reportedContents/serverless.js
@@ -0,0 +1,140 @@
+/* eslint-disable no-template-curly-in-string */
+
+const serverlessConfiguration = {
+  service: 'reportedContents',
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs16.x',
+    stage: '${opt:stage, "dev"}',
+    memorySize: 128,
+    timeout: 30,
+    environment: '${file(../env.js)}',
+    apiGateway: {
+      restApiId: '${cf:api-v1-${self:provider.stage}.RestApiId}',
+      restApiRootResourceId:
+        '${cf:api-v1-${self:provider.stage}.RestApiRootResourceId}',
+    },
+    region: '${opt:region, "us-east-1"}',
+    deploymentBucket: 'ms-deployment-${self:provider.region}',
+  },
+  functions: {
+    // blockContent: {
+    //   handler: 'handlers/blockContent.default',
+    //   events: [
+    //     {
+    //       http: {
+    //         path: 'blockedContents/{type}/{id}/block',
+    //         method: 'put',
+    //         cors: true,
+    //         authorizer: {
+    //           type: 'CUSTOM',
+    //           authorizerId:
+    //             '${cf:account-${self:provider.stage}.ApiGatewayAuthorizerId}',
+    //         },
+    //         request: {
+    //           parameters: {
+    //             paths: {
+    //               id: true,
+    //               type: true,
+    //             },
+    //             headers: {
+    //               Authorization: true,
+    //             },
+    //           },
+    //         },
+    //       },
+    //     },
+    //   ],
+    // },
+    // unblockContent: {
+    //   handler: 'handlers/unblockContent.default',
+    //   events: [
+    //     {
+    //       http: {
+    //         path: 'blockedContents/{type}/{id}/unblock',
+    //         method: 'put',
+    //         cors: true,
+    //         authorizer: {
+    //           type: 'CUSTOM',
+    //           authorizerId:
+    //             '${cf:account-${self:provider.stage}.ApiGatewayAuthorizerId}',
+    //         },
+    //         request: {
+    //           parameters: {
+    //             paths: {
+    //               id: true,
+    //               type: true,
+    //             },
+    //             headers: {
+    //               Authorization: true,
+    //             },
+    //           },
+    //         },
+    //       },
+    //     },
+    //   ],
+    // },
+    getMyReportedContents: {
+      handler: 'handlers/getMyReportedContents.default',
+      events: [
+        {
+          http: {
+            path: 'reportedContents',
+            method: 'get',
+            cors: true,
+            authorizer: {
+              type: 'CUSTOM',
+              authorizerId:
+                '${cf:account-${self:provider.stage}.ApiGatewayAuthorizerId}',
+            },
+            request: {
+              parameters: {
+                headers: {
+                  Authorization: true,
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
+    // getReportedUGC: {
+    //   handler: 'handlers/getReportedUserGeneratedContents.default',
+    //   events: [
+    //     {
+    //       http: {
+    //         path: 'userGeneratedContents',
+    //         method: 'get',
+    //         cors: true,
+    //         authorizer: {
+    //           type: 'CUSTOM',
+    //           authorizerId:
+    //             '${cf:account-${self:provider.stage}.ApiGatewayAuthorizerPublicId}',
+    //         },
+    //       },
+    //     },
+    //   ],
+    // },
+  },
+  custom: {
+    logRetentionInDays: 30,
+    prune: {
+      automatic: true,
+      number: 3,
+    },
+    'serverless-disable-request-validators': {
+      action: 'delete',
+    },
+    esbuild: {
+      config: '../esbuild.config.cjs',
+    },
+  },
+  plugins: [
+    'serverless-esbuild',
+    'serverless-offline',
+    'serverless-disable-request-validators',
+    'serverless-prune-plugin',
+    'serverless-plugin-log-retention',
+  ],
+};
+module.exports = serverlessConfiguration;
