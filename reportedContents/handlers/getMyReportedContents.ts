ae823204e2b825802dc26a62082b8bbfc3f25764
frozar <fabien.rozar@gmail.com>
2024-10-22 17:10:13 +0400

    Normalise the answer of getMyReportedContents

--- reportedContents/handlers/getMyReportedContents.ts
+++ reportedContents/handlers/getMyReportedContents.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import getMyReportedContents from '../lib/getMyReportedContents';
 import response, { handleException } from '../../libs/httpResponses/response';
+import { formatResponseBody } from '@libs/httpResponses/formatResponseBody';
 
 export default async (event: APIGatewayProxyEvent) => {
   const { principalId: userId } = event.requestContext.authorizer as {
@@ -11,7 +12,7 @@ export default async (event: APIGatewayProxyEvent) => {
   try {
     const results = await getMyReportedContents(userId, { appId });
 
-    return response({ code: 200, body: results });
+    return response({ code: 200, body: formatResponseBody({ data: results }) });
   } catch (exception) {
     return handleException(exception);
   }
