6f28551631ad3a0be09ac295243ed13a1e6f6b2a
frozar <fabien.rozar@gmail.com>
2024-10-24 14:25:52 +0400

    Be able to unreport userArticle

--- reportedContents/lib/unreportContent.ts
+++ reportedContents/lib/unreportContent.ts
@@ -3,7 +3,10 @@ import MongoClient from '../../libs/mongoClient';
 import mongoCollections from '../../libs/mongoCollections.json';
 import { reportType } from './type';
 
-const { COLL_USER_GENERATED_CONTENTS_USER_REPORTS } = mongoCollections;
+const {
+  COLL_USER_GENERATED_CONTENTS_USER_REPORTS,
+  COLL_USER_GENERATED_CONTENTS_REPORTS,
+} = mongoCollections;
 
 export default async (
   userId: string,
@@ -31,6 +34,22 @@ export default async (
           .deleteOne({
             _id: alreadyBlocked._id,
           });
+      }
+    }
+
+    if (type === 'userArticle') {
+      const alreadyBlocked = await db
+        .collection(COLL_USER_GENERATED_CONTENTS_REPORTS)
+        .findOne({
+          appId,
+          ugcId: contentId,
+          userId,
+        });
+
+      if (alreadyBlocked) {
+        await db.collection(COLL_USER_GENERATED_CONTENTS_REPORTS).deleteOne({
+          _id: alreadyBlocked._id,
+        });
       }
     }
 
