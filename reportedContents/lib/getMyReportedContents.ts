a00aa902f291b2ba43dd7991cc74706bb142ff29
frozar <fabien.rozar@gmail.com>
2024-10-24 14:16:15 +0400

    Return reported user articles

--- reportedContents/lib/getMyReportedContents.ts
+++ reportedContents/lib/getMyReportedContents.ts
@@ -3,8 +3,12 @@ import MongoClient from '../../libs/mongoClient';
 import mongoCollections from '../../libs/mongoCollections.json';
 import { reportType } from './type';
 
-const { COLL_USER_GENERATED_CONTENTS_USER_REPORTS, COLL_USERS } =
-  mongoCollections;
+const {
+  COLL_USER_GENERATED_CONTENTS_USER_REPORTS,
+  COLL_USERS,
+  COLL_USER_GENERATED_CONTENTS_REPORTS,
+  COLL_USER_GENERATED_CONTENTS,
+} = mongoCollections;
 
 type reportedUserType = {
   _id: string;
@@ -17,11 +21,30 @@ type reportedUserType = {
   userId: string;
 };
 
+type reportedUserArticleType = {
+  _id: string;
+  appId: string;
+  createdAt: Date;
+  details: string;
+  reason: string;
+  ugcId: string;
+  userId: string;
+};
+
 type aggregatedReportedUserType =
   | reportedUserType
   | { type: reportType; reportedUserUsername: string | undefined };
 
-export default async (userId: string, { appId }: { appId: string }) => {
+type aggregatedReportedUserArticleType =
+  | reportedUserArticleType
+  | { type: reportType; reportedUserArticleTitle: string | undefined };
+
+export default async (
+  userId: string,
+  { appId }: { appId: string }
+): Promise<
+  Array<aggregatedReportedUserType | aggregatedReportedUserArticleType>
+> => {
   const client = await MongoClient.connect();
 
   try {
@@ -66,7 +89,52 @@ export default async (userId: string, { appId }: { appId: string }) => {
       type: 'user',
     }));
 
-    return aggregatedReportedUsers;
+    const aggregatedReportedUserArticles: Array<aggregatedReportedUserArticleType> =
+      (
+        await db
+          .collection(COLL_USER_GENERATED_CONTENTS_REPORTS)
+          .aggregate([
+            {
+              $match: {
+                appId,
+                userId,
+              },
+            },
+            {
+              $lookup: {
+                from: COLL_USER_GENERATED_CONTENTS,
+                localField: 'ugcId',
+                foreignField: '_id',
+                pipeline: [
+                  {
+                    $project: { 'data.title': 1, _id: 0 },
+                  },
+                  { $set: { reportedUserArticleTitle: '$data.title' } },
+                  { $unset: 'data' },
+                ],
+                as: 'reportedUserArticle',
+              },
+            },
+            {
+              $set: {
+                reportedUserArticleTitle:
+                  '$reportedUserArticle.reportedUserArticleTitle',
+              },
+            },
+            { $unset: 'reportedUserArticle' },
+            { $unwind: '$reportedUserArticleTitle' },
+          ])
+          .toArray()
+      ).map((reportedUserArticle: aggregatedReportedUserArticleType) => ({
+        ...reportedUserArticle,
+        type: 'userArticle',
+      }));
+
+    return (
+      aggregatedReportedUsers as Array<
+        aggregatedReportedUserType | aggregatedReportedUserArticleType
+      >
+    ).concat(aggregatedReportedUserArticles);
   } finally {
     client.close();
   }
