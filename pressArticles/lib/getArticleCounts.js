9f0b5df2783c6930f8f40c23247d11bd5c88f3c9
Maxime Ferrino <maxime@crowdaa.com>
2024-12-18 16:15:14 +0400

    feat(pressArticles): Improved getArticleCounts speed

--- pressArticles/lib/getArticleCounts.js
+++ pressArticles/lib/getArticleCounts.js
@@ -1,37 +1,30 @@
 /* eslint-disable import/no-relative-packages */
-import getDBCounter from '../../counters/lib/getDBCounter';
+import MongoClient from '../../libs/mongoClient';
 import mongoCollections from '../../libs/mongoCollections.json';
 
 const { COLL_PRESS_ARTICLES, COLL_USER_GENERATED_CONTENTS } = mongoCollections;
 
 export const getArticleCommentsCount = async (appId, articleId) => {
-  const counter = await getDBCounter(
-    appId,
-    'pressArticle-comments',
-    articleId,
-    {
-      collection: COLL_USER_GENERATED_CONTENTS,
-      pipeline: [
-        {
-          $match: {
-            appId,
-            rootParentCollection: COLL_PRESS_ARTICLES,
-            rootParentId: articleId,
-            type: 'comment',
-            trashed: false,
-            $or: [
-              { moderated: false, reviewed: true },
-              { moderated: { $exists: false }, reviewed: { $exists: false } },
-            ],
-          },
-        },
-        {
-          $count: 'total',
-        },
-      ],
-      outputField: 'total',
-    }
-  );
-
-  return counter;
+  let client;
+  try {
+    client = await MongoClient.connect();
+    const count = await client
+      .db()
+      .collection(COLL_USER_GENERATED_CONTENTS)
+      .find({
+        appId,
+        rootParentCollection: COLL_PRESS_ARTICLES,
+        rootParentId: articleId,
+        type: 'comment',
+        trashed: false,
+        $or: [
+          { moderated: false, reviewed: true },
+          { moderated: { $exists: false }, reviewed: { $exists: false } },
+        ],
+      })
+      .count();
+    return count;
+  } finally {
+    await client.close();
+  }
 };
