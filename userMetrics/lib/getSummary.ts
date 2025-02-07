599476d3277a4d66c610fb040cf5ac03fa8ad43b
Maxime Ferrino <maxime@crowdaa.com>
2025-02-07 12:38:43 +0400

    feat(userMetrics): Added getSummary params

--- userMetrics/lib/getSummary.ts
+++ userMetrics/lib/getSummary.ts
@@ -1,16 +1,43 @@
 import MongoClient from '@libs/mongoClient';
 
-export async function getSummary(appId: string) {
+type GetSummaryParamsType = {
+  fromDate?: Date | null;
+  toDate: Date | null;
+  getTotalReadingTime?: boolean;
+};
+
+type GetSummaryReturnType = {
+  users: number;
+  singleDevices: number;
+  allDevices: number;
+  totalReadingTime?: number;
+};
+
+export async function getSummary(
+  appId: string,
+  { fromDate, toDate, getTotalReadingTime }: GetSummaryParamsType
+) {
   const client = await MongoClient.connect();
 
   try {
+    const commonQuery: any = {};
+    if (fromDate && toDate) {
+      commonQuery.createdAt = {
+        $gte: fromDate,
+        $lte: toDate,
+      };
+    }
     const users =
-      (await client.db().collection('users').find({ appId }).count()) || 0;
+      (await client
+        .db()
+        .collection('users')
+        .find({ appId, ...commonQuery })
+        .count()) || 0;
     const [{ count: singleDevices = 0 } = {}] = await client
       .db()
       .collection('userMetrics')
       .aggregate([
-        { $match: { appId, userId: null } },
+        { $match: { appId, userId: null, ...commonQuery } },
         { $group: { _id: '$deviceId' } },
         { $count: 'count' },
       ])
@@ -19,17 +46,31 @@ export async function getSummary(appId: string) {
       .db()
       .collection('userMetrics')
       .aggregate([
-        { $match: { appId } },
+        { $match: { appId, ...commonQuery } },
         { $group: { _id: '$deviceId' } },
         { $count: 'count' },
       ])
       .toArray();
 
-    return {
+    const ret: GetSummaryReturnType = {
       users,
       singleDevices,
       allDevices,
     };
+    if (getTotalReadingTime) {
+      const [{ total: totalReadingTime = 0 } = {}] = await client
+        .db()
+        .collection('userMetrics')
+        .aggregate([
+          { $match: { appId, type: 'time', ...commonQuery } },
+          { $group: { _id: 'total', total: { $sum: '$time' } } },
+        ])
+        .toArray();
+
+      ret.totalReadingTime = totalReadingTime;
+    }
+
+    return ret;
   } finally {
     await client.close();
   }
