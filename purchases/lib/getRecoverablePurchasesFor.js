a3b8e1927a8d88e2e56e16c18dc0e58ccb9ffc96
Maxime Ferrino <maxime@crowdaa.com>
2025-03-24 11:41:29 +0400

    hotfix(purchases): Fixed purchase recovery APIs

--- purchases/lib/getRecoverablePurchasesFor.js
+++ purchases/lib/getRecoverablePurchasesFor.js
@@ -4,6 +4,7 @@ import mongoCollections from '../../libs/mongoCollections.json';
 
 const {
   COLL_CONTENT_PERMISSIONS,
+  COLL_PURCHASES,
   COLL_USERS,
   COLL_USER_METRICS,
   COLL_PUSH_NOTIFICATIONS,
@@ -17,13 +18,135 @@ export const getRecoverablePurchasesForDevice = async (
   const client = await MongoClient.connect();
 
   try {
+    const rawPurchases = await client
+      .db()
+      .collection(COLL_PURCHASES)
+      .aggregate([
+        {
+          $match: {
+            appId,
+            deviceId,
+            userId: { $ne: userId },
+          },
+        },
+        {
+          $lookup: {
+            from: COLL_USERS,
+            localField: 'userId',
+            foreignField: '_id',
+            as: 'user',
+          },
+        },
+        {
+          $unwind: {
+            path: '$user',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+      ])
+      .toArray();
+
+    const recoverablePurchases = rawPurchases.filter(
+      (purchase) => !purchase.user
+    );
+    return recoverablePurchases;
+  } finally {
+    client.close();
+  }
+};
+
+export const getRecoverablePurchasesForUser = async (
+  appId,
+  deviceId,
+  userId
+) => {
+  const client = await MongoClient.connect();
+
+  try {
+    const allUserDevices = [];
+
+    const notificationDevices = await client
+      .db()
+      .collection(COLL_PUSH_NOTIFICATIONS)
+      .find(
+        { appId, userId, deviceUUID: { $ne: deviceId } },
+        { projection: { deviceUUID: 1 } }
+      )
+      .toArray();
+
+    notificationDevices.forEach((notif) => {
+      if (allUserDevices.indexOf(notif.deviceUUID) < 0) {
+        allUserDevices.push(notif.deviceUUID);
+      }
+    });
+
+    const userMetricsDevices = await client
+      .db()
+      .collection(COLL_USER_METRICS)
+      .find(
+        { appId, userId, deviceId: { $nin: allUserDevices } },
+        { projection: { deviceId: 1 } }
+      )
+      .toArray();
+
+    userMetricsDevices.forEach((metric) => {
+      if (allUserDevices.indexOf(metric.deviceId) < 0) {
+        allUserDevices.push(metric.deviceId);
+      }
+    });
+
+    const rawPurchases = await client
+      .db()
+      .collection(COLL_PURCHASES)
+      .aggregate([
+        {
+          $match: {
+            appId,
+            deviceId: { $in: allUserDevices },
+            userId: { $ne: userId },
+          },
+        },
+        {
+          $lookup: {
+            from: COLL_USERS,
+            localField: 'userId',
+            foreignField: '_id',
+            as: 'user',
+          },
+        },
+        {
+          $unwind: {
+            path: '$user',
+            preserveNullAndEmptyArrays: true,
+          },
+        },
+      ])
+      .toArray();
+
+    const recoverablePurchases = rawPurchases.filter(
+      (purchase) => !purchase.user
+    );
+    return recoverablePurchases;
+  } finally {
+    client.close();
+  }
+};
+
+export const getRecoverablePermissionsForDevice = async (
+  appId,
+  deviceId,
+  userId
+) => {
+  const client = await MongoClient.connect();
+
+  try {
     /*
      * This collection does not contain an appId so we need a two step check
      * to ensure that it's set properly.
      * We should never see two different appIds in there since the deviceId is
      * meant to be unique, but better be safe than sorry.
      */
-    const rawPurchases = await client
+    const rawPermissions = await client
       .db()
       .collection(COLL_CONTENT_PERMISSIONS)
       .aggregate([
@@ -50,25 +173,25 @@ export const getRecoverablePurchasesForDevice = async (
       ])
       .toArray();
 
-    const purchasesContentHash = {};
-    rawPurchases.forEach((purchase) => {
-      const { contentCollection, contentId } = purchase;
-      if (!purchasesContentHash[contentCollection]) {
-        purchasesContentHash[contentCollection] = {};
+    const permissionsContentHash = {};
+    rawPermissions.forEach((permission) => {
+      const { contentCollection, contentId } = permission;
+      if (!permissionsContentHash[contentCollection]) {
+        permissionsContentHash[contentCollection] = {};
       }
-      purchasesContentHash[contentCollection][contentId] = purchase;
+      permissionsContentHash[contentCollection][contentId] = permission;
     });
 
-    const purchasesCollections = Object.keys(purchasesContentHash);
+    const permissionCollections = Object.keys(permissionsContentHash);
 
-    const appPurchases = [];
-    const promises = purchasesCollections.map(async (collection) => {
+    const appPermissions = [];
+    const promises = permissionCollections.map(async (collection) => {
       const items = await client
         .db()
         .collection(collection)
         .find(
           {
-            _id: { $in: Object.keys(purchasesContentHash[collection]) },
+            _id: { $in: Object.keys(permissionsContentHash[collection]) },
             appId,
           },
           { projection: { _id: 1 } }
@@ -77,22 +200,22 @@ export const getRecoverablePurchasesForDevice = async (
 
       const ids = items.map(({ _id }) => _id);
       ids.forEach((id) => {
-        const purchase = purchasesContentHash[collection][id];
-        if (purchase) {
-          appPurchases.push(purchase);
+        const permission = permissionsContentHash[collection][id];
+        if (permission) {
+          appPermissions.push(permission);
         }
       });
     });
 
     await Promise.all(promises);
 
-    return appPurchases;
+    return appPermissions;
   } finally {
     client.close();
   }
 };
 
-export const getRecoverablePurchasesForUser = async (
+export const getRecoverablePermissionsForUser = async (
   appId,
   deviceId,
   userId
@@ -138,38 +261,54 @@ export const getRecoverablePurchasesForUser = async (
      * We should never see two different appIds in there since the deviceId is
      * meant to be unique, but better be safe than sorry.
      */
-    const rawDevicesPurchases = await client
+    const rawDevicesPermissions = await client
       .db()
       .collection(COLL_CONTENT_PERMISSIONS)
       .aggregate([
         {
           $match: {
             deviceId: { $in: allUserDevices },
-            userId: null,
+            userId: { $ne: userId },
+          },
+        },
+        {
+          $lookup: {
+            from: COLL_USERS,
+            localField: 'userId',
+            foreignField: '_id',
+            as: 'user',
+          },
+        },
+        {
+          $unwind: {
+            path: '$user',
+            preserveNullAndEmptyArrays: true,
           },
         },
       ])
       .toArray();
 
-    const purchasesContentHash = {};
-    rawDevicesPurchases.forEach((purchase) => {
-      const { contentCollection, contentId } = purchase;
-      if (!purchasesContentHash[contentCollection]) {
-        purchasesContentHash[contentCollection] = {};
+    const permissionsContentHash = {};
+    rawDevicesPermissions.forEach((permission) => {
+      if (permission.user) return;
+
+      const { contentCollection, contentId } = permission;
+      if (!permissionsContentHash[contentCollection]) {
+        permissionsContentHash[contentCollection] = {};
       }
-      purchasesContentHash[contentCollection][contentId] = purchase;
+      permissionsContentHash[contentCollection][contentId] = permission;
     });
 
-    const purchasesCollections = Object.keys(purchasesContentHash);
+    const permissionCollections = Object.keys(permissionsContentHash);
 
-    const devicesPurchases = [];
-    const promises = purchasesCollections.map(async (collection) => {
+    const devicesPermissions = [];
+    const promises = permissionCollections.map(async (collection) => {
       const items = await client
         .db()
         .collection(collection)
         .find(
           {
-            _id: { $in: Object.keys(purchasesContentHash[collection]) },
+            _id: { $in: Object.keys(permissionsContentHash[collection]) },
             appId,
           },
           { projection: { _id: 1 } }
@@ -178,16 +317,16 @@ export const getRecoverablePurchasesForUser = async (
 
       const ids = items.map(({ _id }) => _id);
       ids.forEach((id) => {
-        const purchase = purchasesContentHash[collection][id];
-        if (purchase) {
-          devicesPurchases.push(purchase);
+        const permission = permissionsContentHash[collection][id];
+        if (permission) {
+          devicesPermissions.push(permission);
         }
       });
     });
 
     await Promise.all(promises);
 
-    return devicesPurchases;
+    return devicesPermissions;
   } finally {
     client.close();
   }
