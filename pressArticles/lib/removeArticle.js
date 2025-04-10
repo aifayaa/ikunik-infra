82f1f1c42d629bd2b5b5b9a914125bbc588fb34b
Maxime Ferrino <maxime@crowdaa.com>
2025-04-10 14:56:39 +0400

    hotfix(pressArticles): Added WP API calling when removing an article

--- pressArticles/lib/removeArticle.js
+++ pressArticles/lib/removeArticle.js
@@ -1,8 +1,9 @@
 /* eslint-disable import/no-relative-packages */
+import { WordpressAPI } from '../../libs/backends/wordpress';
 import MongoClient from '../../libs/mongoClient';
 import mongoCollections from '../../libs/mongoCollections.json';
 
-const { COLL_PRESS_DRAFTS, COLL_PRESS_ARTICLES } = mongoCollections;
+const { COLL_APPS, COLL_PRESS_DRAFTS, COLL_PRESS_ARTICLES } = mongoCollections;
 
 export const removeArticle = async (_userId, appId, articleId) => {
   const client = await MongoClient.connect();
@@ -30,6 +31,24 @@ export const removeArticle = async (_userId, appId, articleId) => {
     );
 
     await session.commitTransaction();
+
+    try {
+      const app = await client.db().collection(COLL_APPS).findOne({
+        _id: appId,
+      });
+
+      const wpApi = new WordpressAPI(app);
+
+      await wpApi.call('POST', '/crowdaa-sync/v1/sync/article/removed', {
+        articleId,
+      });
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Error calling delete WP API for ${_userId}/${appId}/${articleId}`,
+        e
+      );
+    }
 
     return { articleId };
   } finally {
