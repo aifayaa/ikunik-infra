3729b5ebca3fe0fc5b3a5797fabf1116c876de08
Maxime Ferrino <maxime@crowdaa.com>
2025-11-18 14:28:20 +0400

    fix(notifications): Completed notificationClicked API (Not tested yet)

--- notifications/handlers/notificationClicked.ts
+++ notifications/handlers/notificationClicked.ts
@@ -7,41 +7,27 @@ import { z } from 'zod';
 import { CrowdaaError } from '@libs/httpResponses/CrowdaaError';
 import {
   ERROR_TYPE_VALIDATION_ERROR,
-  INVALID_INPUT_FORMAT_CODE,
+  MISSING_BODY_CODE,
 } from '@libs/httpResponses/errorCodes';
-
-function unknownToInt(val: unknown) {
-  if (typeof val === 'string') {
-    const int = parseInt(val, 10);
-    if (Number.isNaN(int)) {
-      throw new CrowdaaError(
-        ERROR_TYPE_VALIDATION_ERROR,
-        INVALID_INPUT_FORMAT_CODE,
-        'Invalid format for integer input'
-      );
-    }
-
-    return int;
-  }
-
-  return val;
-}
-
-const querySchema = z.object({
-  nextToken: z
-    .string({
-      invalid_type_error: 'nextToken must be a string',
-    })
-    .optional(),
-  limit: z
-    .preprocess(unknownToInt, z.number().int().min(1).max(100).optional())
-    .default(30),
-});
 
 const pathSchema = z.object({
   id: z
     .string({
-      invalid_type_error: 'id must be a string',
+      invalid_type_error: 'blastQueueId must be a string',
+    })
+    .min(1),
+});
+
+const bodySchema = z.object({
+  notificationId: z
+    .string({
+      invalid_type_error: 'notificationId must be a string',
+    })
+    .min(1)
+    .optional(),
+  deviceId: z
+    .string({
+      invalid_type_error: 'deviceId must be a string',
     })
     .min(1),
 });
@@ -54,17 +40,32 @@ export default async (event: APIGatewayProxyEvent) => {
   };
 
   try {
-    const { id: notificationId } = pathSchema.parse(event.pathParameters || {});
+    if (!event.body) {
+      throw new CrowdaaError(
+        ERROR_TYPE_VALIDATION_ERROR,
+        MISSING_BODY_CODE,
+        `Body is missing from the request`
+      );
+    }
 
-    const session = await notificationClicked(
+    // Validate the body of the request
+    const body = JSON.parse(event.body);
+
+    const { notificationId, deviceId } = bodySchema.parse(body);
+
+    const { id: blastQueueId } = pathSchema.parse(event.pathParameters || {});
+
+    const result = await notificationClicked(
       userId || null,
       appId,
+      deviceId,
+      blastQueueId,
       notificationId
     );
     return response({
       code: 200,
       body: formatResponseBody({
-        data: session,
+        data: result,
       }),
     });
   } catch (exception) {
--- notifications/lib/notificationClicked.ts
+++ notifications/lib/notificationClicked.ts
@@ -1,29 +1,170 @@
-import MongoClient from '../../libs/mongoClient';
+import MongoClient, { ObjectID } from '../../libs/mongoClient';
 import mongoCollections from '../../libs/mongoCollections.json';
-import { UserNotificationType } from './notificationsEntities';
+import {
+  DeviceNotificationType,
+  NotificationType,
+  UserNotificationType,
+} from './notificationsEntities';
 
 const { COLL_NOTIFICATIONS } = mongoCollections;
+
+async function getNotification(
+  userId: string | null,
+  appId: string,
+  deviceId: string,
+  blastQueueId: string,
+  notificationId: ObjectID | undefined,
+  { client }: { client: any }
+) {
+  if (notificationId) {
+    const notification = (await client
+      .db()
+      .collection(COLL_NOTIFICATIONS)
+      .findOne({
+        _id: notificationId,
+        appId,
+        blastQueueId,
+      })) as NotificationType | null;
+
+    return notification;
+  } else if (userId) {
+    const notification = (await client
+      .db()
+      .collection(COLL_NOTIFICATIONS)
+      .findOne({
+        appId,
+        blastQueueId,
+        target: 'user',
+        userId,
+      })) as UserNotificationType | null;
+
+    return notification;
+  } else {
+    const notification = (await client
+      .db()
+      .collection(COLL_NOTIFICATIONS)
+      .findOne({
+        appId,
+        blastQueueId,
+        target: 'device',
+        deviceId,
+      })) as DeviceNotificationType | null;
+
+    return notification;
+  }
+}
 
 export default async function notificationClicked(
   userId: string | null,
   appId: string,
-  notificationId: string
+  deviceId: string,
+  blastQueueId: string,
+  notificationId: string | undefined
 ) {
   const client = await MongoClient.connect();
   try {
-    const notification = (await client
-      .db()
-      .collection(COLL_NOTIFICATIONS)
-      .findOne({ _id: notificationId, appId })) as UserNotificationType | null;
+    const notification = await getNotification(
+      userId,
+      appId,
+      deviceId,
+      blastQueueId,
+      notificationId ? ObjectID.createFromHexString(notificationId) : undefined,
+      { client }
+    );
 
     if (!notification) {
-      return { ok: false };
+      return { notification: null };
     }
 
-    if (notification.userId !== userId) {
+    let clicked = false;
+
+    if (notification.target === 'user') {
+      if (!notification.clicked) {
+        clicked = true;
+        notification.clicked = true;
+        notification.clickedAt = new Date();
+        notification.clickedOn.push(deviceId);
+
+        await client
+          .db()
+          .collection(COLL_NOTIFICATIONS)
+          .updateOne(
+            { _id: notification._id },
+            {
+              $set: {
+                clicked: notification.clicked,
+                clickedAt: notification.clickedAt,
+              },
+              $addToSet: {
+                clickedOn: deviceId,
+              },
+            }
+          );
+      }
+
+      await client
+        .db()
+        .collection(COLL_NOTIFICATIONS)
+        .updateOne(
+          {
+            appId,
+            blastQueueId,
+            target: 'device',
+            deviceId,
+            clicked: false,
+          },
+          {
+            $set: {
+              clicked: notification.clicked,
+              clickedAt: notification.clickedAt,
+            },
+          }
+        );
+    } else if (notification.target === 'device') {
+      if (!notification.clicked) {
+        clicked = true;
+        notification.clicked = true;
+        notification.clickedAt = new Date();
+
+        await client
+          .db()
+          .collection(COLL_NOTIFICATIONS)
+          .updateOne(
+            {
+              _id: notification._id,
+            },
+            {
+              $set: {
+                clicked: notification.clicked,
+                clickedAt: notification.clickedAt,
+              },
+            }
+          );
+      }
+
+      await client
+        .db()
+        .collection(COLL_NOTIFICATIONS)
+        .updateOne(
+          {
+            appId,
+            blastQueueId,
+            target: 'user',
+            clicked: false,
+          },
+          {
+            $set: {
+              clicked: notification.clicked,
+              clickedAt: notification.clickedAt,
+            },
+            $addToSet: {
+              clickedOn: deviceId,
+            },
+          }
+        );
     }
 
-    return { ok: true };
+    return { notification, clicked };
   } finally {
     await client.close();
   }
