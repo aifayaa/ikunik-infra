77d0e76ca812364b0e8f1dddb1d7d8bc0a8face3
Maxime Ferrino <maxime@crowdaa.com>
2025-11-21 09:07:35 +0400

    feat(ssr): Added link for the web app redirection on desktop

--- ssr/handlers/article.js
+++ ssr/handlers/article.js
@@ -1,19 +1,68 @@
 /* eslint-disable import/no-relative-packages */
 import get from 'lodash/get';
 import response from '../../libs/httpResponses/response.ts';
-import getAppFromName from '../lib/getAppFromName';
 import { getArticle } from '../../pressArticles/lib/getArticle';
 import meta from '../lib/meta';
 import prepareNotifString from '../../pressArticles/lib/prepareNotifString';
 import redirect from '../lib/redirect';
+import MongoClient from '../../libs/mongoClient';
+import mongoCollections from '../../libs/mongoCollections.json';
+
+const { APP_NAME_DEFAULT } = process.env;
+
+const { COLL_APPS } = mongoCollections;
+
+async function getApp(appName) {
+  const client = await MongoClient.connect();
+  const name = appName || APP_NAME_DEFAULT;
+
+  try {
+    const app = await client
+      .db()
+      .collection(COLL_APPS)
+      .findOne(
+        { name },
+        {
+          projection: {
+            _id: 1,
+            'builds.android.packageId': 1,
+            'builds.android.name': 1,
+            'builds.ios.iosAppId': 1,
+            'builds.ios.name': 1,
+            'credentials.facebook.appId': 1,
+            'settings.webAppUrl': 1,
+            protocol: 1,
+          },
+        }
+      );
+    if (!app) {
+      throw new Error('app_not_found');
+    }
+    return app;
+  } finally {
+    client.close();
+  }
+}
 
 export default async (event) => {
   try {
     const redirectUrl = (event.queryStringParameters || {}).redirect_url;
     const { appName } = event.queryStringParameters || {};
     const userAgent = event.headers['User-Agent'];
-    const { _id: appId, builds, credentials } = await getAppFromName(appName);
-    const redirectResponse = await redirect(userAgent, redirectUrl, appId);
+    const {
+      _id: appId,
+      builds,
+      credentials,
+      settings,
+      protocol,
+    } = await getApp(appName);
+    const redirectResponse = redirect(
+      userAgent,
+      redirectUrl,
+      appId,
+      protocol,
+      settings.webAppUrl
+    );
     if (redirectResponse) {
       return redirectResponse;
     }
